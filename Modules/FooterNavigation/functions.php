28c9d0ca1e0aaa1060941c0fe3b949e1814d8657
Wahiba Khadri <wahiba@wahiba.fritz.box>
2016-11-16 12:14:43 +0100

    feat(footer): Footer Navigation

--- Modules/FooterNavigation/functions.php
+++ Modules/FooterNavigation/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+use WPStarterTheme\Helpers\Navigation;
+
 add_filter('WPStarter/modifyModuleData?name=FooterNavigation', function($data) {
+  $data['menuItems'] = Navigation::getMenuLinks('footer_navigation');
   return $data;
 });
