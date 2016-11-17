15e108826096fa39213e654376f4ad07d97e9e19
Doğa Gürdal <doga@bleech.de>
2016-11-17 14:55:30 +0100

    fix(checkPlugin): moved setTemplateDirectory hooks to Core to enable updating template path even if
    added comments in functions.php for some clarity

--- functions.php
+++ functions.php
@@ -6,6 +6,15 @@ use WPStarterTheme\Core;
 
 require_once __DIR__ . '/lib/Core.php';
 
+// This needs to happen first.
+// Reason:  In case the theme was just activated and the plugin is not active,
+//          we still need to run the after_switch_theme action, which is
+//          defined here.
+Core::setTemplateDirectory();
+
+// Check if the plugin is installed and activated.
+// If it isn't, this function redirects the template rendering to use
+// plugin-inactive.php instead
 $pluginActive = Core::checkPlugin();
 
 if ($pluginActive) {
--- lib/Core.php
+++ lib/Core.php
@@ -5,17 +5,29 @@ namespace WPStarterTheme;
 use RecursiveDirectoryIterator;
 
 class Core {
-  public static function checkPlugin() {
-    // TODO check if this is a problem
-    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
+  public static function setTemplateDirectory() {
+
+    add_action('after_switch_theme', function () {
+      $stylesheet = get_option('stylesheet');
+
+      if (basename($stylesheet) !== 'templates') {
+        update_option('stylesheet', $stylesheet . '/templates');
+      }
+    });
+
+    add_filter('stylesheet', function ($stylesheet) {
+      return dirname($stylesheet);
+    });
+
+  }
+
+  public static function checkPlugin() {
+
+    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
     $pluginActive = is_plugin_active('wp-starter-plugin/wp-starter-plugin.php');
 
-    // TODO or use this instead?
-    // $pluginActive = class_exists('WPStarter\Defaults');
-
     if (!$pluginActive) {
-
       add_filter('template_include', function () {
         $newTemplate = locate_template(array('plugin-inactive.php'));
         if ('' != $newTemplate) {
@@ -24,7 +36,6 @@ class Core {
           return 'WP Starter Plugin not activated! Please activate the plugin and reload the page.';
         }
       });
-
     }
 
     return $pluginActive;
@@ -32,9 +43,11 @@ class Core {
   }
 
   public static function loadFiles($dir = '', $files = []) {
+
     if (count($files) === 0) {
       $directory = get_template_directory() . DIRECTORY_SEPARATOR . $dir;
       $directoryIterator = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
+
       foreach ($directoryIterator as $name => $file) {
         if ($file->isFile() && $file->getExtension() === 'php') {
           $filePath = $file->getPathname();
@@ -44,6 +57,7 @@ class Core {
     } else {
       array_walk($files, function ($file) use ($dir) {
         $filePath = $dir . $file;
+
         if (!locate_template($filePath, true, true)) {
           trigger_error(
             sprintf(__('Error locating %s for inclusion', 'wp-starter-boilerplate'), $filePath),
