be70c4f3aa15a70f5620eceeba7a7a6110a42b79
Doğa Gürdal <doga@bleech.de>
2016-11-17 12:13:34 +0100

    feat(checkPlugin): doesn't break anymore when plugin is inactive or not installed

--- functions.php
+++ functions.php
@@ -4,15 +4,31 @@ namespace WPStarterTheme;
 
 use WPStarterTheme\Core;
 
-require_once __DIR__ . '/lib/Core.php';
+// TODO check if this is a problem
+include_once ABSPATH . 'wp-admin/includes/plugin.php';
 
-Core::loadFiles(
-  'lib/',
-  [
-    'Config.php',
-    'Helpers.php',
-    'Hooks.php',
-    'DataFilters.php',
-    'Init.php'
-  ]
-);
+if (!is_plugin_active('wp-starter-plugin')) {
+  add_filter('template_include', function() {
+    $newTemplate = locate_template( array( 'plugin-inactive.php' ) );
+		if ( '' != $newTemplate ) {
+			return $newTemplate;
+		} else {
+      return 'WP Starter Plugin not activated! Please activate the plugin and reload the page.';
+    }
+  });
+} else {
+
+  require_once __DIR__ . '/lib/Core.php';
+
+  Core::loadFiles(
+    'lib/',
+    [
+      'Config.php',
+      'Helpers.php',
+      'Hooks.php',
+      'DataFilters.php',
+      'Init.php'
+    ]
+  );
+
+}
