c4c019952b90cda8d0716a9f7f76ff037b641c40
Doğa Gürdal <doga@bleech.de>
2016-11-17 10:28:01 +0100

    feat(postList and postTeasers): got modules working with acf and the Posts data filter
    also, removed unused script and style files

--- Modules/PageHeader/functions.php
+++ Modules/PageHeader/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 add_filter('WPStarter/modifyModuleData?name=PageHeader', function($data, $parentData) {
-  $data['image'] = $parentData['post_thumbnail']['url'];
+  if (!empty($parentData['post_thumbnail']) && array_key_exists('url', $parentData['post_thumbnail'])) {
+    $data['image'] = $parentData['post_thumbnail']['url'];
+  }
   return $data;
 }, 10, 2);
--- Modules/PostList/functions.php
+++ Modules/PostList/functions.php
@@ -1,5 +1,6 @@
 <?php
 
 add_filter('WPStarter/modifyModuleData?name=PostList', function($data) {
+  $data['posts'] = apply_filters('WPStarterTheme/DataFilters/Posts', [], 5, 'long');
   return $data;
 });
