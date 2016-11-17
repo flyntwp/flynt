a4c55722dc005620f10ff263631970703b6d1f41
Doğa Gürdal <doga@bleech.de>
2016-11-17 12:30:50 +0100

    refactor(checkPlugin): moved to Core Class
    also made it actually work

--- functions.php
+++ functions.php
@@ -4,21 +4,11 @@ namespace WPStarterTheme;
 
 use WPStarterTheme\Core;
 
-// TODO check if this is a problem
-include_once ABSPATH . 'wp-admin/includes/plugin.php';
+require_once __DIR__ . '/lib/Core.php';
 
-if (!is_plugin_active('wp-starter-plugin')) {
-  add_filter('template_include', function() {
-    $newTemplate = locate_template( array( 'plugin-inactive.php' ) );
-		if ( '' != $newTemplate ) {
-			return $newTemplate;
-		} else {
-      return 'WP Starter Plugin not activated! Please activate the plugin and reload the page.';
-    }
-  });
-} else {
+$pluginActive = Core::checkPlugin();
 
-  require_once __DIR__ . '/lib/Core.php';
+if ($pluginActive) {
 
   Core::loadFiles(
     'lib/',
--- lib/Core.php
+++ lib/Core.php
@@ -5,12 +5,38 @@ namespace WPStarterTheme;
 use RecursiveDirectoryIterator;
 
 class Core {
+  public static function checkPlugin() {
+    // TODO check if this is a problem
+    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+
+    $pluginActive = is_plugin_active('wp-starter-plugin/wp-starter-plugin.php');
+
+    // TODO or use this instead?
+    // $pluginActive = class_exists('WPStarter\Defaults');
+
+    if (!$pluginActive) {
+
+      add_filter('template_include', function () {
+        $newTemplate = locate_template(array('plugin-inactive.php'));
+        if ('' != $newTemplate) {
+          return $newTemplate;
+        } else {
+          return 'WP Starter Plugin not activated! Please activate the plugin and reload the page.';
+        }
+      });
+
+    }
+
+    return $pluginActive;
+
+  }
+
   public static function loadFiles($dir = '', $files = []) {
-    if(count($files) === 0) {
+    if (count($files) === 0) {
       $directory = get_template_directory() . DIRECTORY_SEPARATOR . $dir;
-      $Directory = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
-      foreach($Directory as $name => $file){
-        if($file->isFile() && $file->getExtension() === 'php') {
+      $directoryIterator = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
+      foreach ($directoryIterator as $name => $file) {
+        if ($file->isFile() && $file->getExtension() === 'php') {
           $filePath = $file->getPathname();
           require_once $filePath;
         }
@@ -19,7 +45,10 @@ class Core {
       array_walk($files, function ($file) use ($dir) {
         $filePath = $dir . $file;
         if (!locate_template($filePath, true, true)) {
-          trigger_error(sprintf(__('Error locating %s for inclusion', 'wp-starter-boilerplate'), $filePath), E_USER_ERROR);
+          trigger_error(
+            sprintf(__('Error locating %s for inclusion', 'wp-starter-boilerplate'), $filePath),
+            E_USER_ERROR
+          );
         }
       });
     }
