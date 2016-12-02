6d9bd0a2472f3fb614d8f31c98243c5b8ef90e9e
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-12-02 13:44:46 +0100

    refactor(Refactored stylings and naming for image sizes):

--- Modules/Image/functions.php
+++ Modules/Image/functions.php
@@ -4,8 +4,8 @@ namespace WPStarterTheme\Modules\Image;
 use WPStarterTheme\Helpers\Utils;
 use WPStarterTheme\Helpers\Module;
 
-add_image_size('wps-Image-desktop', 1140, 700);
-add_image_size('wps-Image-mobile', 768, 500);
+add_image_size('wps-Image-lg', 1140, 700);
+add_image_size('wps-Image-sm', 768, 500);
 
 add_filter('WPStarter/modifyModuleData?name=Image', function ($data) {
   Module::enqueueAssets('Image', [
