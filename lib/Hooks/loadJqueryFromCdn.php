2aeca598c04b2030dec4ce2d5a26160db7d1d52b
Michael <michaelcarruthers31@gmail.com>
2016-12-02 10:20:48 +0100

    refactor(lib/hooks): cleanup helpers + linting errors

--- lib/Hooks/loadJqueryFromCdn.php
+++ lib/Hooks/loadJqueryFromCdn.php
@@ -2,42 +2,22 @@
 
 namespace WPStarterTheme\Hooks;
 
-// Taken from roots/soil
-
 /**
- * Load jQuery from jQuery's CDN with a local fallback
+ * registerJquery
  *
- * You can enable/disable this feature in functions.php (or lib/setup.php if you're using Sage):
- * add_theme_support('soil-jquery-cdn');
+ * Load jQuery from jQuery's CDN
+ *
+ * TODO add local fallback
+ *
  */
-function register_jquery() {
-  $jquery_version = wp_scripts()->registered['jquery']->ver;
+add_action('wp_enqueue_scripts', function () {
+  $jqueryVersion = wp_scripts()->registered['jquery']->ver;
   wp_deregister_script('jquery');
   wp_register_script(
     'jquery',
-    '//code.jquery.com/jquery-' . $jquery_version . '.min.js',
+    '//code.jquery.com/jquery-' . $jqueryVersion . '.min.js',
     [],
     null,
     true
   );
-  // add_filter('script_loader_src', __NAMESPACE__ . '\\jquery_local_fallback', 10, 2);
-}
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\register_jquery', 101);
-
-/**
- * Output the local fallback immediately after jQuery's <script>
- *
- * @link http://wordpress.stackexchange.com/a/12450
- */
-function jquery_local_fallback($src, $handle = null) {
-  static $add_jquery_fallback = false;
-  if ($add_jquery_fallback) {
-    echo '<script>(window.jQuery && jQuery.noConflict()) || document.write(\'<script src="' . $add_jquery_fallback .'"><\/script>\')</script>' . "\n";
-    $add_jquery_fallback = false;
-  }
-  if ($handle === 'jquery') {
-    $add_jquery_fallback = apply_filters('script_loader_src', \includes_url('/js/jquery/jquery.js'), 'jquery-fallback');
-  }
-  return $src;
-}
-// add_action('wp_head', __NAMESPACE__ . '\\jquery_local_fallback');
+}, 100);
