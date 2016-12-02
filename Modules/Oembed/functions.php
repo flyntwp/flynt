fdf62929a86fa277165ce7db14493b22f747b53b
Wahiba Khadri <wahiba@wahiba.local>
2016-12-02 12:50:35 +0100

    refactor(oembed-module): Added instructions and comments for images sizes and format

--- Modules/Oembed/functions.php
+++ Modules/Oembed/functions.php
@@ -2,8 +2,13 @@
 use WPStarterTheme\Helpers\Log;
 
 add_filter('WPStarter/modifyModuleData?name=Oembed', function ($data) {
+  
   if (empty($data['posterImage'])) {
     $data['posterImage'] = get_field('defaultPosterImage', 'options');
   }
+
+  // Must have good pre-set image sizes. (add_image_size needs to live here)
+  // Add you min/max image sizes in the fields.json config
+
   return $data;
 });
