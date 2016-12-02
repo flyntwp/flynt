d07ad022e9beff9c3f474d079ed0f4e62d6a802a
Markus Müller <markus.mueller@live.de>
2016-12-02 11:23:56 +0100

    feat(AcfOptionPages): implemented first draft

---
+++ lib/Helpers/AcfOptionPages.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace WPStarterTheme\Helpers;
+
+use WPStarterTheme\Helpers\StringHelpers;
+use ACFComposer;
+
+class AcfOptionPages {
+  const FILTER_NAMESPACE = 'WPStarterTheme/Modules';
+  const FIELD_GROUPS_DIR = '/config/fieldGroups';
+
+  const OPTION_TYPES = [
+    'options',
+    'localeOptions'
+  ];
+
+  protected static $optionPages = [];
+
+  public static function init() {
+    if (class_exists('acf') && function_exists('acf_add_options_page') && function_exists('acf_add_options_sub_page')) {
+      self::createOptionPages();
+
+      add_action(
+        'WPStarter/registerModule',
+        ['WPStarterTheme\Helpers\AcfOptionPages', 'addAllModuleOptionSubpages'],
+        12,
+        2
+      );
+    }
+  }
+
+  public static function createOptionPages() {
+    foreach (self::OPTION_TYPES as $index => $optionType) {
+      $title = ucfirst($optionType);
+      $slug = 'module' . ucfirst($optionType);
+
+      $generalSettings = acf_add_options_page(array(
+        'page_title'  => 'Module ' . $title,
+        'menu_title'  => $title,
+        'redirect'    => false,
+        'menu_slug'   => $slug
+      ));
+
+      self::$optionPages[$optionType] = [
+        'menu_slug' => $slug
+      ];
+    }
+  }
+
+  public static function setOptionSubPageFieldGroupConfig($parentMenuSlug, $menuSlug, $prettyModuleName, $fields) {
+    $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup(
+      [
+        'name' => 'options' . ucfirst($parentMenuSlug) . $menuSlug,
+        'title' => $prettyModuleName,
+        'fields' => $fields,
+        'location' => [
+          [
+            [
+              'param' => 'options_page',
+              'operator' => '==',
+              'value' => $menuSlug
+            ]
+          ]
+        ]
+      ]
+    );
+
+    acf_add_local_field_group($fieldGroup);
+  }
+
+  public static function addModuleOptionSubpages($modulePath, $moduleName, $optionType, $fields) {
+    $prettyModuleName = StringHelpers::splitCamelCase($moduleName);
+
+    $moduleConfig = array(
+      'page_title'  =>  $prettyModuleName. ' Module',
+      'menu_title'  => $prettyModuleName,
+      'parent_slug' => self::$optionPages[$optionType]['menu_slug'],
+      'menu_slug'   => $optionType. 'Module' . $moduleName
+    );
+
+    acf_add_options_sub_page($moduleConfig);
+
+    self::setOptionSubPageFieldGroupConfig(
+      $moduleConfig['parent_slug'],
+      $moduleConfig['menu_slug'],
+      $moduleConfig['menu_title'],
+      $fields
+    );
+  }
+
+  public static function addAllModuleOptionSubpages($modulePath, $moduleName) {
+    // load fields.json if it exists
+    $filePath = $modulePath . '/fields.json';
+    if (!is_file($filePath)) return;
+    $fields = json_decode(file_get_contents($filePath), true);
+
+    foreach (self::OPTION_TYPES as $index => $optionType) {
+      if (array_key_exists($optionType, $fields)) {
+        self::addModuleOptionSubpages($modulePath, $moduleName, $optionType, $fields[$optionType]);
+      }
+    }
+  }
+}
+
+AcfOptionPages::init();
--- lib/Helpers/StringHelpers.php
+++ lib/Helpers/StringHelpers.php
@@ -12,6 +12,17 @@ class StringHelpers {
       return wp_trim_words(wp_strip_all_tags($str), $length, '...');
   }
 
+  public static function splitCamelCase($input) {
+    $a = preg_split(
+      '/(^[^A-Z]+|[A-Z][^A-Z]+)/',
+      $input,
+      -1, /* no limit for replacement count */
+      PREG_SPLIT_NO_EMPTY /*don't return empty elements*/
+      | PREG_SPLIT_DELIM_CAPTURE /*don't strip anything from output array*/
+    );
+    return join($a, ' ');
+  }
+
   public static function strstartswith($haystack, $needle) {
     if (!$needle) return false;
     return strpos($haystack, $needle) === 0;
