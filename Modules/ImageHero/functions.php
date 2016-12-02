2dd4d8baf719e65052d08aaf7667b5bf37c3c9c5
Markus Müller <markus.mueller@live.de>
2016-12-02 17:30:46 +0100

    refactor(imageHero): put enqueueAssets in the action hook wp_enqueue_scripts

--- Modules/ImageHero/functions.php
+++ Modules/ImageHero/functions.php
@@ -8,7 +8,7 @@ use WPStarterTheme\Helpers\Log;
 add_image_size('wpsImageHeroLg', 1140, 700, true);
 add_image_size('wpsImageHeroSm', 768, 500, true);
 
-add_filter('WPStarter/modifyModuleData?name=ImageHero', function ($data) {
+add_action('wp_enqueue_scripts', function () {
   Module::enqueueAssets('ImageHero', [
     [
       'name' => 'objectfit-polyfill',
@@ -16,7 +16,9 @@ add_filter('WPStarter/modifyModuleData?name=ImageHero', function ($data) {
       'type' => 'script'
     ]
   ]);
+});
 
+add_filter('WPStarter/modifyModuleData?name=ImageHero', function ($data) {
   $imageConfig = [
     'default' => 'wpsImageHeroLg',
     'sizes' => [
