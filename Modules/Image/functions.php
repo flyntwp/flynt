c774617ecb3952072d2666ac58d888a601e660d0
Markus Müller <markus.mueller@live.de>
2016-12-02 14:01:12 +0100

    feat(image): inserted picture mixin and added config logic to the image module in the functions.php

--- Modules/Image/functions.php
+++ Modules/Image/functions.php
@@ -3,6 +3,7 @@ namespace WPStarterTheme\Modules\Image;
 
 use WPStarterTheme\Helpers\Utils;
 use WPStarterTheme\Helpers\Module;
+use WPStarterTheme\Helpers\Log;
 
 add_image_size('wps-Image-lg', 1140, 700);
 add_image_size('wps-Image-sm', 768, 500);
@@ -15,4 +16,17 @@ add_filter('WPStarter/modifyModuleData?name=Image', function ($data) {
       'type' => 'script'
     ]
   ]);
+
+  $imageConfig = [
+    'default' => 'medium',
+    'sizes' => [
+      'thumbnail' => '(max-width: 767px)',
+      'medium' => '(max-width: 1023px)',
+      'large' => '(max-width: 1199px)'
+    ]
+  ];
+
+  $data['image']['imageConfig'] = $imageConfig;
+
+  return $data;
 });
