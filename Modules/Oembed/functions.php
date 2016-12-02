1487816689edd6f950f16604ab53e1cb8b0ae398
lukashfmn <lukas@bleech.de>
2016-12-02 18:12:20 +0100

    feat(oembed): implemented oembed basemodule.

--- Modules/Oembed/functions.php
+++ Modules/Oembed/functions.php
@@ -1,8 +1,13 @@
 <?php
-use WPStarterTheme\Helpers\Log;
+namespace WPStarterTheme\Modules\Oembed;
+
+use WPStarterTheme\Helpers\Module;
+
+add_image_size('wpsOembedLg', 1140, 700, true);
+add_image_size('wpsOembedSm', 768, 500, true);
 
 add_filter('WPStarter/modifyModuleData?name=Oembed', function ($data) {
-  
+
   if (empty($data['posterImage'])) {
     $data['posterImage'] = get_field('defaultPosterImage', 'options');
   }
@@ -10,5 +15,24 @@ add_filter('WPStarter/modifyModuleData?name=Oembed', function ($data) {
   // Must have good pre-set image sizes. (add_image_size needs to live here)
   // Add you min/max image sizes in the fields.json config
 
+  $imageConfig = [
+    'default' => 'wpsOembedLg',
+    'sizes' => [
+      'wpsOembedSm' => '(max-width: 767px)'
+    ]
+  ];
+
+  $data['posterImage']['imageConfig'] = $imageConfig;
+
   return $data;
+});
+
+add_action('wp_enqueue_scripts', function () {
+  Module::enqueueAssets('Oembed', [
+    [
+      'name' => 'fitvids',
+      'path' => 'vendor/jquery.fitvids.js',
+      'type' => 'script'
+    ]
+  ]);
 });
---
+++ Modules/Oembed/script.js
@@ -0,0 +1,32 @@
+import 'file-loader?name=vendor/jquery.fitvids.js!fitvids/jquery.fitvids.js'
+
+class Oembed extends window.HTMLDivElement {
+  constructor (self) {
+    self = super(self)
+    self.$ = $(self)
+    self.resolveElements()
+    return self
+  }
+
+  resolveElements () {
+    this.$posterImage = $('.oembed-poster-image', this)
+    this.$video = $('.oembed-video', this)
+    this.$iframe = $('iframe', this)
+  }
+
+  connectedCallback () {
+    this.$.fitVids()
+    this.$posterImage.on('click', (e) => {
+      this.startVideo()
+    })
+  }
+
+  startVideo = () => {
+    const $autoplay = this.$iframe.attr('src') + '&autoplay=1'
+    this.$posterImage.addClass('isHidden')
+    this.$video.addClass('isActive')
+    this.$iframe.attr('src', $autoplay)
+  }
+}
+
+window.customElements.define('wps-oembed', Oembed, {extends: 'div'})
