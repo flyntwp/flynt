6ed82647dc0b73431bebc82523a0324b454a4abe
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-12-02 13:33:42 +0100

    feat(Image): added image sizes

--- Modules/Image/functions.php
+++ Modules/Image/functions.php
@@ -4,6 +4,9 @@ namespace WPStarterTheme\Modules\Image;
 use WPStarterTheme\Helpers\Utils;
 use WPStarterTheme\Helpers\Module;
 
+add_image_size('wps-Image-desktop', 1140, 700);
+add_image_size('wps-Image-mobile', 768, 500);
+
 add_filter('WPStarter/modifyModuleData?name=Image', function ($data) {
   Module::enqueueAssets('Image', [
     [
