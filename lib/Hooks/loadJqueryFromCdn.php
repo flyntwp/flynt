4139d9d709b883539b599f52d29edccc11928ede
Markus Müller <markus.mueller@live.de>
2016-12-03 17:03:41 +0100

    refactor(modules and jquery): refactored some coding style and removed tmp jquery cdn
    +

--- lib/Hooks/loadJqueryFromCdn.php
+++ lib/Hooks/loadJqueryFromCdn.php
@@ -8,16 +8,16 @@ namespace WPStarterTheme\Hooks;
  * Load jQuery from jQuery's CDN
  *
  * TODO add local fallback
- *
+ * TODO add cdn again just removed it because internet so slow
  */
 add_action('wp_enqueue_scripts', function () {
-  $jqueryVersion = wp_scripts()->registered['jquery']->ver;
-  wp_deregister_script('jquery');
-  wp_register_script(
-    'jquery',
-    '//code.jquery.com/jquery-' . $jqueryVersion . '.min.js',
-    [],
-    null,
-    true
-  );
+  // $jqueryVersion = wp_scripts()->registered['jquery']->ver;
+  // wp_deregister_script('jquery');
+  // wp_register_script(
+  //   'jquery',
+  //   '//code.jquery.com/jquery-' . $jqueryVersion . '.min.js',
+  //   [],
+  //   null,
+  //   true
+  // );
 }, 100);
