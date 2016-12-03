0828c4fec9acae0d42b030712100bfb3c4befb15
Doğa Gürdal <doga@bleech.de>
2016-12-03 17:57:45 +0100

    build(gulpfile.js): fixed watch task breaking sometimes
    now using runSequence instead of (apparently) buggy gulp params

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,14 +1,22 @@
 const browserSync = require('browser-sync')
 const gulp = require('gulp')
 const watch = require('gulp-watch')
+const runSequence = require('run-sequence')
 
 module.exports = function (config) {
-  gulp.task('watch', ['webpack:watch', 'browserSync'], function (cb) {
+  gulp.task('watch:files', function () {
     watch(config.copy, function () { gulp.start('copy') })
     watch(config.stylus, function () { gulp.start('stylus') })
     watch(config.php, function () { }).on('change', browserSync.reload)
     watch(config.lint.stylus, function () { gulp.start('lint:stylus') })
     watch(config.lint.js, function () { gulp.start('lint:js') })
     watch(config.lint.php, function () { gulp.start('lint:php') })
+  })
+  gulp.task('watch', function (cb) {
+    runSequence(
+      ['webpack:watch', 'browserSync'],
+      'watch:files',
+      cb
+    )
   })
 }
