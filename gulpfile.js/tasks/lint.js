23e9ee2799b860ee0698d2fed27764a8cbcd6f7a
Doğa Gürdal <doga@bleech.de>
2016-12-05 20:04:18 +0100

    build(phpcs): added checks for php linting in gulpfile
    runs using boilerplate composer vendor bin if installed, falls back to global phpcs. If not phpcs is
    
    found fails gracefully and shows a warning in the console.

--- gulpfile.js/tasks/lint.js
+++ gulpfile.js/tasks/lint.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp')
+const gutil = require('gulp-util')
 const changedInPlace = require('gulp-changed-in-place')
 const stylint = require('gulp-stylint')
 const standard = require('gulp-standard')
 const phpcs = require('gulp-phpcs')
+const hasbin = require('hasbin')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = function (config) {
   gulp.task('lint', ['lint:stylus', 'lint:js', 'lint:php'])
@@ -35,6 +39,31 @@ module.exports = function (config) {
   })
 
   gulp.task('lint:php', function () {
+    // TODO should do these checks in the very beginning and then enable / disable php linting!
+    // Check if phpcs is installed in boilerplate vendor
+    const binaryPath = path.resolve(process.cwd(), '../../../../', 'vendor', 'bin', 'phpcs')
+    fs.access(binaryPath, fs.constants.F_OK | fs.constants.X_OK, function (err) {
+      // Binary Exists in vendor
+      if (!err) {
+        return runPhpCs(binaryPath)
+      }
+
+      // Check if a binary exists on process.env.PATH
+      hasbin('phpcs', function (result) {
+        if (!result) {
+          gutil.log(gutil.colors.yellow('PHPCS not found in PATH! Please install PHPCS to enable the php linter:'))
+          gutil.log(gutil.colors.yellow.underline('https://github.com/squizlabs/PHP_CodeSniffer'))
+        } else {
+          return runPhpCs()
+        }
+      })
+    })
+  })
+
+  function runPhpCs (binaryPath = '') {
+    if (binaryPath) {
+      config.lint.phpcs.bin = binaryPath
+    }
     const task = gulp.src(config.lint.php)
     .pipe(changedInPlace({firstPass: true}))
     .pipe(phpcs(config.lint.phpcs))
@@ -45,5 +74,5 @@ module.exports = function (config) {
       return task
       .pipe(phpcs.reporter('fail', {failOnFirst: false}))
     }
-  })
+  }
 }
