b6c407edba96e125325e2498735c75fbec0b06fd
Wahiba Khadri <wahiba@wahiba.fritz.box>
2016-12-06 18:39:51 +0100

    feat(front-module-previews): Added basic functionality to 'Module Previews' wp admin bar button
    Shows and hides desktop and mobile preview images

--- Modules/AdminModulePreview/script.js
+++ Modules/AdminModulePreview/script.js
@@ -33,8 +33,9 @@ if ($('body.wp-admin').length) {
     .find('[data-event="collapse-layout"]')
     .click()
   )
+
   var showAddModulePreview = function (layout, $wrapper) {
-    var moduleName = $.fn.firstToUpperCase(layout)
+    var moduleName = firstToUpperCase(layout)
     var image
     image = {
       desktop: wpData.templateDirectoryUri + '/Modules/' + moduleName + '/preview-desktop.jpg',
@@ -56,12 +57,98 @@ if ($('body.wp-admin').length) {
   var hideAddModulePreview = $wrapper =>
   $wrapper.find('.add-module-preview-image-wrapper')
   .remove()
+} else if ($('html.logged-in ').length) {
+  // front-end logged in
+  let $mpc = null
+
+  var getModuleImages = function (output = {}) {
+    $('.main-content [is]').each(function () {
+      var moduleString = $(this).attr('is')
+      var moduleName = $.camelCase(moduleString.substring(moduleString.indexOf('-') + 1))
+      moduleName = firstToUpperCase(moduleName)
+      var images
+      images = {
+        desktop: wpData.templateDirectoryUri + '/Modules/' + moduleName + '/preview-desktop.jpg',
+        mobile: wpData.templateDirectoryUri + '/Modules/' + moduleName + '/preview-mobile.jpg'
+      }
+      if (output[moduleName] == null) { output[moduleName] = images }
+      addModulePreviews($(this), images)
+    })
+    return output
+  }
+
+  var addModulePreviews = function (module, images) {
+    let $module = module
+    let offset = $module.offset()
+    $.ajax({
+      url: images.desktop,
+      type: 'HEAD',
+      error: function () {},
+      success: function () {
+        let selector = `<img class='module-preview-image module-preview-image-desktop' src='${images.desktop}'>`
+        appendImage(selector, offset)
+      }
+    })
+    $.ajax({
+      url: images.mobile,
+      type: 'HEAD',
+      error: function () {},
+      success: function () {
+        let selector = `<img class='module-preview-image module-preview-image-mobile' src='${images.mobile}'>`
+        appendImage(selector, offset)
+      }
+    })
+  }
+
+  var appendImage = function (image, offset) {
+    return $(image)
+    .appendTo($mpc)
+    .css({
+      opacity: 0.7,
+      position: 'absolute',
+      zIndex: 9999,
+      top: offset.top,
+      left: offset.left
+    })
+  }
+
+  $('body')
+  .on('click', '#wp-admin-bar-toggleModulePreviews', function (e) {
+    e.preventDefault()
+    let $target = $(e.currentTarget)
+    $target.toggleClass('active')
+    if ($target.hasClass('active')) {
+      return showImages()
+    } else {
+      return hideImages()
+    }
+  })
+
+  var showImages = function () {
+    if (($mpc = $('#module-preview-container')).length) {
+      $mpc.show()
+    } else {
+      $mpc = $('<div id="module-preview-container"></div>').appendTo('body')
+      getModuleImages()
+    }
+    return $(window).on('keydown.moveModulePreviewImage', moveModulePreviewImage)
+  }
+
+  var hideImages = function () {
+    $mpc.hide()
+    return $(window).off('keydown.moveModulePreviewImage')
+  }
+}
+
+var moveModulePreviewImage = function (e) {
+  e.preventDefault()
+}
+
+let firstToUpperCase = function (str) {
+  return str.substr(0, 1).toUpperCase() + str.substr(1)
 }
 
 ;(function ($) {
-  $.fn.firstToUpperCase = function (str) {
-    return str.substr(0, 1).toUpperCase() + str.substr(1)
-  }
   $.fn.closestChild = function (selector) {
     let $children
     let $results
