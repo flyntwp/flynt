bd14cd6a6e26bb57c7d05c21b88b0c2891db8981
Doğa Gürdal <doga@bleech.de>
2017-01-19 14:01:54 +0100

    refactor(Acf/OptionPages): added icons to subpages, set redirect to true, abstracted logic to prepar

--- Features/Acf/OptionPages.php
+++ Features/Acf/OptionPages.php
@@ -1,9 +1,13 @@
 <?php
 
+// TODO make global fields untranslatable
+// TODO add sub pages for CPTs and Features as well and prefix all of them
+
 namespace Flynt\Features\Acf;
 
 use Flynt\ComponentManager;
 use Flynt\Utils\StringHelpers;
+use Flynt\Features\Components\Component;
 use ACFComposer;
 
 class OptionPages {
@@ -11,8 +15,23 @@ class OptionPages {
   const FIELD_GROUPS_DIR = '/config/fieldGroups';
 
   const OPTION_TYPES = [
-    'options',
-    'localeOptions'
+    'flyntOptions' => [
+      'title' => 'Options',
+      'name' => 'options',
+      // 'translatable' => false
+    ],
+    'flyntLocaleOptions' => [
+      'title' => 'Locale Options',
+      'name' => 'localeOptions'
+    ]
+  ];
+
+  const OPTION_CATEGORIES = [
+    'component' => [
+      'title' => 'Component',
+      'name' => 'component',
+      'icon' => 'dashicons-editor-table'
+    ]
   ];
 
   protected static $optionPages = [];
@@ -22,20 +41,34 @@ class OptionPages {
 
     add_action(
       'Flynt/registerComponent',
-      ['Flynt\Features\Acf\OptionPages', 'addAllComponentOptionSubpages'],
+      ['Flynt\Features\Acf\OptionPages', 'addAllComponentSubPages'],
       12
     );
+
+    add_action('admin_enqueue_scripts', function () {
+      Component::addAsset('enqueue', [
+        'type' => 'style',
+        'name' => 'ACF/AdminCSS',
+        'path' => 'Features/Acf/admin.css'
+      ]);
+    });
+
+    // add_filter('Flynt/addComponentData', function ($data, $componentName) {
+    //   // $data[] =
+    // });
+
+    // self::removeEmptyOptionPages();
   }
 
   public static function createOptionPages() {
-    foreach (self::OPTION_TYPES as $index => $optionType) {
-      $title = ucfirst($optionType);
-      $slug = 'component' . ucfirst($optionType);
+    foreach (self::OPTION_TYPES as $optionType => $option) {
+      $title = $option['title'];
+      $slug = ucfirst($optionType);
 
       $generalSettings = acf_add_options_page(array(
-        'page_title'  => 'Component ' . $title,
+        'page_title'  => $title,
         'menu_title'  => $title,
-        'redirect'    => false,
+        'redirect'    => true,
         'menu_slug'   => $slug
       ));
 
@@ -45,11 +78,59 @@ class OptionPages {
     }
   }
 
-  public static function setOptionSubPageFieldGroupConfig($parentMenuSlug, $menuSlug, $prettyComponentName, $fields) {
+  public static function addAllComponentSubPages($componentName) {
+    // load fields.json if it exists
+    $componentManager = ComponentManager::getInstance();
+    $filePath = $componentManager->getComponentFilePath($componentName, 'fields.json');
+
+    if (false === $filePath) {
+      return;
+    }
+
+    self::createSubPageFromConfig($filePath, 'component', $componentName);
+  }
+
+  protected static function createSubPageFromConfig($filePath, $optionCategory, $subPageName) {
+    $fields = json_decode(file_get_contents($filePath), true);
+
+    foreach (self::OPTION_TYPES as $optionType => $option) {
+      if (array_key_exists($option['name'], $fields)) {
+        self::addOptionSubPage(
+          self::OPTION_CATEGORIES[$optionCategory],
+          $subPageName,
+          $optionType,
+          $fields[$option['name']]
+        );
+      }
+    }
+  }
+
+  protected static function addOptionSubPage($optionCategory, $subPageName, $optionType, $fields) {
+    $prettySubPageName = StringHelpers::splitCamelCase($subPageName);
+    $iconClasses = "flynt-submenu-item dashicons-before {$optionCategory['icon']}";
+
+    $subPageConfig = array(
+      'page_title'  => $prettySubPageName . ' ' . $optionCategory['title'],
+      'menu_title'  => "<span class='{$iconClasses}'>{$prettySubPageName}</span>",
+      'parent_slug' => self::$optionPages[$optionType]['menu_slug'],
+      'menu_slug'   => $optionType . ucfirst($optionCategory['name']) . $subPageName
+    );
+
+    acf_add_options_sub_page($subPageConfig);
+
+    self::addFieldGroupToSubPage(
+      $subPageConfig['parent_slug'],
+      $subPageConfig['menu_slug'],
+      $subPageConfig['menu_title'],
+      $fields
+    );
+  }
+
+  protected static function addFieldGroupToSubPage($parentMenuSlug, $menuSlug, $prettySubPageName, $fields) {
     $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup(
       [
-        'name' => 'options' . ucfirst($parentMenuSlug) . $menuSlug,
-        'title' => $prettyComponentName,
+        'name' => $menuSlug,
+        'title' => $prettySubPageName,
         'fields' => $fields,
         'location' => [
           [
@@ -64,44 +145,5 @@ class OptionPages {
     );
 
     acf_add_local_field_group($fieldGroup);
-  }
-
-  public static function addComponentOptionSubpages($componentPath, $componentName, $optionType, $fields) {
-    $prettyComponentName = StringHelpers::splitCamelCase($componentName);
-
-    $componentConfig = array(
-      'page_title'  =>  $prettyComponentName. ' Component',
-      'menu_title'  => $prettyComponentName,
-      'parent_slug' => self::$optionPages[$optionType]['menu_slug'],
-      'menu_slug'   => $optionType. 'Component' . $componentName
-    );
-
-    acf_add_options_sub_page($componentConfig);
-
-    self::setOptionSubPageFieldGroupConfig(
-      $componentConfig['parent_slug'],
-      $componentConfig['menu_slug'],
-      $componentConfig['menu_title'],
-      $fields
-    );
-  }
-
-  public static function addAllComponentOptionSubpages($componentName) {
-    // load fields.json if it exists
-    $componentManager = ComponentManager::getInstance();
-    $filePath = $componentManager->getComponentFilePath($componentName, 'fields.json');
-
-    if (false === $filePath) {
-      return;
-    }
-
-    $fields = json_decode(file_get_contents($filePath), true);
-    $componentPath = $componentManager->getComponentDirPath($componentName);
-
-    foreach (self::OPTION_TYPES as $index => $optionType) {
-      if (array_key_exists($optionType, $fields)) {
-        self::addComponentOptionSubpages($componentPath, $componentName, $optionType, $fields[$optionType]);
-      }
-    }
   }
 }
