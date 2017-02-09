a37b86d5be7abc3143192c35de9a0b4d547845a8
bertrand <bertrand@bleech.de>
2017-02-09 17:09:01 +0100

    feat(not-found): New syntax for options fields

--- Components/NotFound/functions.php
+++ Components/NotFound/functions.php
@@ -10,8 +10,6 @@ add_action('wp_enqueue_scripts', function () {
 
 add_filter('Flynt/addComponentData?name=NotFound', function ($data) {
   $data['homeUrl'] = home_url();
-  $data['text'] = get_field('notFoundText', 'options');
-  $data['linkLabel'] = get_field('notFoundLinkLabel', 'options');
   return $data;
 });
 
