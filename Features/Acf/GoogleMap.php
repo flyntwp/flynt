673a90152d0eb3dadf2fe7199cfa4d7e08d78a32
Wahiba Khadri <wahiba@bleech.de>
2017-02-13 12:51:56 +0100

    feat(acfGoogleMapKey): Google Maps Api Key for acf google_map field type
    Added to the Acf Feature

---
+++ Features/Acf/GoogleMap.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Flynt\Features\Acf;
+
+class GoogleMap {
+  public static function init() {
+    add_filter('acf/fields/google_map/api', function ($api) {
+      $apiKey = OptionPages::getOption('options', 'feature', 'Acf', 'googleMapApiKey');
+      if ($apiKey) {
+        $api['key'] = $apiKey;
+      }
+      return $api;
+    });
+  }
+}
