7f732f35c229ced224430d4993d64ebb5cab0774
Wahiba Khadri <wahiba@bleech.de>
2017-02-14 11:21:12 +0100

    refactor(renaming): Renamed googleMap to googleMaps

--- Features/Acf/GoogleMaps.php
+++ Features/Acf/GoogleMaps.php
@@ -2,7 +2,7 @@
 
 namespace Flynt\Features\Acf;
 
-class GoogleMap {
+class GoogleMaps {
   public static function init() {
     add_filter('acf/fields/google_map/api', function ($api) {
       $apiKey = OptionPages::getOption('options', 'feature', 'Acf', 'googleMapsApiKey');
--- Features/Acf/functions.php
+++ Features/Acf/functions.php
@@ -5,7 +5,7 @@ namespace Flynt\Features\Acf;
 require_once __DIR__ . '/FieldGroupComposer.php';
 require_once __DIR__ . '/OptionPages.php';
 require_once __DIR__ . '/Loader.php';
-require_once __DIR__ . '/GoogleMap.php';
+require_once __DIR__ . '/GoogleMaps.php';
 
 use Flynt\Features\Acf\Loader;
 use Flynt\Utils\Feature;
@@ -13,4 +13,4 @@ use Flynt\Utils\Feature;
 Loader::setup(Feature::getOption('flynt-acf', 0));
 
 add_action('Flynt/afterRegisterFeatures', 'Flynt\Features\Acf\Loader::init');
-add_action('Flynt/afterRegisterFeatures', 'Flynt\Features\Acf\GoogleMap::init');
+add_action('Flynt/afterRegisterFeatures', 'Flynt\Features\Acf\GoogleMaps::init');
