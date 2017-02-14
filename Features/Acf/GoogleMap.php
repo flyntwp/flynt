3a739ca6c1c04580839fa42132698accb359ec56
Wahiba Khadri <wahiba@bleech.de>
2017-02-14 10:45:08 +0100

    refactor(renaming): Renamed googleMapApiKey to googleMapsApiKey

--- Features/Acf/GoogleMap.php
+++ Features/Acf/GoogleMap.php
@@ -5,7 +5,7 @@ namespace Flynt\Features\Acf;
 class GoogleMap {
   public static function init() {
     add_filter('acf/fields/google_map/api', function ($api) {
-      $apiKey = OptionPages::getOption('options', 'feature', 'Acf', 'googleMapApiKey');
+      $apiKey = OptionPages::getOption('options', 'feature', 'Acf', 'googleMapsApiKey');
       if ($apiKey) {
         $api['key'] = $apiKey;
       }
