4a0f056918928ec59ea901c7f7bf477e1de32459
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-02-16 16:29:00 +0100

    refactor(MainTemplate): Moved related Styles from MainLayout to MainTemplate

--- Components/MainTemplate/functions.php
+++ Components/MainTemplate/functions.php
@@ -3,6 +3,37 @@
 namespace Flynt\Components\MainTemplate;
 
 use Timber\Timber;
+use Flynt\Features\Components\Component;
+
+add_action('wp_enqueue_scripts', function () {
+  Component::enqueueAssets('MainTemplate', [
+    [
+      'name' => 'console-polyfill',
+      'type' => 'script',
+      'path' => 'vendor/console.js'
+    ],
+    [
+      'name' => 'babel-polyfill',
+      'type' => 'script',
+      'path' => 'vendor/babel-polyfill.js'
+    ],
+    [
+      'name' => 'document-register-element',
+      'type' => 'script',
+      'path' => 'vendor/document-register-element.js'
+    ],
+    [
+      'name' => 'picturefill',
+      'path' => 'vendor/picturefill.js',
+      'type' => 'script'
+    ],
+    [
+      'name' => 'normalize',
+      'path' => 'vendor/normalize.css',
+      'type' => 'style'
+    ]
+  ]);
+});
 
 add_filter('Flynt/addComponentData?name=MainTemplate', function ($data) {
   $query = !empty($data['query']) ? $data['query'] : false;
