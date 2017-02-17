73ec3cd50e23e4e95512134321ede890ab08e6cb
Markus Müller <markus.mueller@live.de>
2017-02-17 13:37:12 +0100

    docs(Feature/TinyMCE): removed linting error and removed commented code

--- Features/TinyMce/functions.php
+++ Features/TinyMce/functions.php
@@ -50,11 +50,8 @@ add_filter('mce_buttons_2', function ($buttons) {
   return [];
 });
 
-
-add_filter('tiny_mce_before_init', function($init) {
+add_filter('tiny_mce_before_init', function ($init) {
   // Add block format elements you want to show in dropdown
-  // All Block Formats
-  // $init['block_formats'] = 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Address=address;Pre=pre';
   $init['block_formats'] = 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6';
   return $init;
 });
