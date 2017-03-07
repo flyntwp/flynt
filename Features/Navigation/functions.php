18a0c8d8e51974bf075690dc0886d611565ee40d
Doğa Gürdal <doga@bleech.de>
2017-03-07 11:31:08 +0100

    fix(Features/Navigation): change hook to init
    This somehow fixes the menu sub-page showing up. Not sure why it doesn't work with
    after_setup_theme.
    
    #59

--- Features/Navigation/functions.php
+++ Features/Navigation/functions.php
@@ -2,9 +2,9 @@
 
 namespace Flynt\Features\Navigation;
 
-add_action('after_setup_theme', function () {
-  register_nav_menus(array(
+add_action('init', function () {
+  register_nav_menus([
     'main_navigation' => __('Main Navigation', 'flynt-theme'),
     'footer_navigation' => __('Footer Navigation', 'flynt-theme')
-  ));
+  ]);
 });
