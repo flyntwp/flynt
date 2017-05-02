bf5d735037686fb684a2498807a2d1aa78757c2e
Doğa Gürdal <Qakulukiam@users.noreply.github.com>
2017-05-02 10:31:29 +0200

    style(lib/Bootstrap): fix code style
    * style(lib/Bootstrap): remove whitespace, remove unused use statement
    
    * style(lib/Bootstrap): use shorthand array notation

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -2,14 +2,10 @@
 
 namespace Flynt;
 
-use RecursiveDirectoryIterator;
-
 class Bootstrap
 {
-
     public static function setTemplateDirectory()
     {
-
         add_action('after_switch_theme', function () {
             $stylesheet = get_option('stylesheet');
 
@@ -25,24 +21,23 @@ class Bootstrap
 
     public static function checkPlugin()
     {
-
         $pluginActive = class_exists('\\Flynt\\Render');
 
         if (!$pluginActive) {
             add_action('admin_notices', function () {
                 echo '<div class="error"><p>Flynt Core Plugin not activated. Make sure you activate the plugin in <a href="'
-                . esc_url(admin_url('plugins.php#flynt')) . '">'
-                . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+                    . esc_url(admin_url('plugins.php#flynt')) . '">'
+                    . esc_url(admin_url('plugins.php')) . '</a></p></div>';
             });
 
             add_filter('template_include', function () {
-                $newTemplate = locate_template(array('plugin-inactive.php'));
+                $newTemplate = locate_template(['plugin-inactive.php']);
                 if ('' != $newTemplate) {
                     return $newTemplate;
                 } else {
                     return 'Flynt Core Plugin not activated! Please <a href="'
-                    . esc_url(admin_url('plugins.php'))
-                    . '">activate the plugin</a> and reload the page.';
+                        . esc_url(admin_url('plugins.php'))
+                        . '">activate the plugin</a> and reload the page.';
                 }
             });
         }
