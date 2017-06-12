7a0d48901759a14258733dea48d339e2a1bc1680
Wahiba <wahiba@bleech.de>
2017-06-12 10:42:30 +0200

    docs(lib/Utils): Add DocBlocks for Log Util (#212)
    * feat(lib/Utils/Log.php): Added DocBlocks
    
    * refactor(lib/Utils/Log.php): Fixed indentations in DocBlocks
    
    * refactor(lib/Utils/Log.php): Refactored DockBlocks
    
    Added an uppercase to each sentence first word
    
    * refactor(lib/Utils/Log.php): DocBlocks
    
    Changed number type to integer
    
    * refactor(DocBlock): Added a return tag to the getCallerFile method
    
    * fix(lib/Utils/Log.php): Fixed bad copy paste in error function docblock
    
    * refactor(lib/Utils/Log.php): Reindented Docblocks. Added @since %%NEXT_VERSION%%

--- lib/Utils/Log.php
+++ lib/Utils/Log.php
@@ -4,21 +4,65 @@ namespace Flynt\Utils;
 
 class Log
 {
+    /**
+     * Outputs a message to the Web Console.
+     *
+     * Outputs a JavaScript `console.log`.
+     *
+     * @since 0.1.0
+     *
+     * @param mixed    $data     Data to be logged.
+     * @param boolean  $postpone Postpone logging to the wp_footer action.
+     */
     public static function console($data, $postpone = true)
     {
         self::consoleDebug($data, $postpone);
     }
 
+    /**
+     * Outputs an error message to the Web Console.
+     *
+     * Outputs a JavaScript `console.error`.
+     *
+     * @since 0.1.0
+     *
+     * @param mixed    $data     Data to be logged.
+     * @param boolean  $postpone Postpone logging to the wp_footer action.
+     */
     public static function error($data, $postpone = true)
     {
         self::consoleDebug($data, $postpone, 'PHP', 'error');
     }
 
+    /**
+     * Pretty prints a variable.
+     *
+     * Prints human-readable information about a variable.
+     *
+     * @since 0.1.0
+     *
+     * @param mixed    $data     Data to be printed.
+     * @param boolean  $postpone Postpone printing to the wp_footer action.
+     */
     public static function pp($data, $postpone = true)
     {
         self::printDebug($data, $postpone);
     }
 
+    /**
+     * Outputs a message to the Web Console.
+     *
+     * Outputs a JavaScript log.
+     * Accepts log, error and trace. Can specify a title.
+     * Will also output the data type, the filename and line number.
+     *
+     * @since 0.1.0
+     *
+     * @param mixed    $data     Data to be logged.
+     * @param boolean  $postpone Postpone logging to the wp_footer action.
+     * @param string   $title    A title.
+     * @param string   $logType  The type of log (accepts log|error|trace).
+     */
     public static function consoleDebug($data, $postpone = true, $title = 'PHP', $logType = 'log')
     {
         if (in_array($logType, ['log', 'error', 'trace'])) {
@@ -30,6 +74,18 @@ class Log
         }
     }
 
+    /**
+     * Pretty prints a variable.
+     *
+     * Prints human-readable information about a variable.
+     * Print will be wrapped between <pre> tags.
+     * Will also output the data type, the filename and line number.
+     *
+     * @since %%NEXT_VERSION%%
+     *
+     * @param mixed    $data     Data to be printed.
+     * @param boolean  $postpone Postpone printing to the wp_footer action.
+     */
     public static function printDebug($data, $postpone = true)
     {
         $type = gettype($data);
@@ -43,6 +99,14 @@ class Log
         self::echoDebug($output, $postpone);
     }
 
+    /**
+     * Echoes a variable.
+     *
+     * @since %%NEXT_VERSION%%
+     *
+     * @param mixed    $data     Data to be printed.
+     * @param boolean  $postpone Postpone printing to the wp_footer action.
+     */
     protected static function echoDebug($data, $postpone)
     {
         if ($postpone) {
@@ -54,6 +118,15 @@ class Log
         }
     }
 
+    /**
+     * Returns a filename and line number by performing a debug_backtrace.
+     *
+     * @since 0.1.0
+     *
+     * @param integer  $depth  Depth to return.
+     *
+     * @return string  The filename and line number.
+     */
     protected static function getCallerFile($depth = 1)
     {
         $debug = debug_backtrace();
