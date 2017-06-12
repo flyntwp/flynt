d4131eb5ae53eb529550fa80d46101253632af45
Wahiba <wahiba@bleech.de>
2017-06-12 10:44:47 +0200

    docs(lib/Utils): Add DocBlocks for StringHelpers Util (#213)
    * feat(lib/Utils/StringHelpers.php): Added DocBlocks
    
    * refactor(lib/Utils/StringHelpers.php): Small docblocks spelling fixes
    
    * refactor(lib/Utils/StringHelpers.php): Docblocks refactoring: small changes and @return added
    
    * refactor(lib/Utils/StringHelpers.php): Reindented Docblocks.

--- lib/Utils/StringHelpers.php
+++ lib/Utils/StringHelpers.php
@@ -4,11 +4,31 @@ namespace Flynt\Utils;
 
 class StringHelpers
 {
+    /**
+     * Converts a string from camel case to kebap case.
+     *
+     * @since 0.1.0
+     *
+     * @param string $str The string to convert.
+     *
+     * @return string
+     */
     public static function camelCaseToKebap($str)
     {
         return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $str));
     }
 
+    /**
+     * Strips all HTML tags including script and style,
+     * and trims text to a certain number of words.
+     *
+     * @since 0.1.0
+     *
+     * @param string $str    The string to trim and strip.
+     * @param number $length The string length to return.
+     *
+     * @return string
+     */
     public static function trimStrip($str, $length = 25)
     {
         if (isset($str)) {
@@ -17,6 +37,15 @@ class StringHelpers
         return $str;
     }
 
+    /**
+     * Splits a camel case string.
+     *
+     * @since 0.1.0
+     *
+     * @param string $str The string to split.
+     *
+     * @return string
+     */
     public static function splitCamelCase($str)
     {
         $a = preg_split(
@@ -29,6 +58,16 @@ class StringHelpers
         return implode($a, ' ');
     }
 
+    /**
+     * Converts a string from kebap case to camel case.
+     *
+     * @since 0.1.0
+     *
+     * @param string $str                        The string to convert.
+     * @param boolean $capitalizeFirstCharacter  Sets if the first character should be capitalized.
+     *
+     * @return string
+     */
     public static function kebapCaseToCamelCase($str, $capitalizeFirstCharacter = false)
     {
         $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $str)));
@@ -38,6 +77,16 @@ class StringHelpers
         return $str;
     }
 
+    /**
+     * Removes a prefix from a string.
+     *
+     * @since 0.1.0
+     *
+     * @param string $prefix The prefix to be removed.
+     * @param string $str    The string to manipulate.
+     *
+     * @return string
+     */
     public static function removePrefix($prefix, $str)
     {
         if (substr($str, 0, strlen($prefix)) == $prefix) {
@@ -46,11 +95,31 @@ class StringHelpers
         return $str;
     }
 
+    /**
+     * Checks if a string starts with a certain string.
+     *
+     * @since 0.1.0
+     *
+     * @param string $search   The string to search for.
+     * @param string $subject  The string to look into.
+     *
+     * @return boolean Returns true if the subject string starts with the search string.
+     */
     public static function startsWith($search, $subject)
     {
         return substr($subject, 0, strlen($search)) === $search;
     }
 
+    /**
+     * Checks if a string ends with a certain string.
+     *
+     * @since 0.1.0
+     *
+     * @param string $search   The string to search for.
+     * @param string $subject  The string to look into.
+     *
+     * @return boolean Returns true if the subject string ends with the search string.
+     */
     public static function endsWith($search, $subject)
     {
         $searchLength = strlen($search);
