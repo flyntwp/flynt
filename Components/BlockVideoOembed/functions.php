ff59d048e234235d6dd2722da7f2e1f7b571586a
Markus Müller <markus.mueller@live.de>
2018-11-20 18:52:13 +0100

    feat(BlockVideoOembed): add Component draft

--- Components/BlockVideoOembed/functions.php
+++ Components/BlockVideoOembed/functions.php
@@ -5,12 +5,18 @@ namespace Flynt\Components\BlockVideoOembed;
 use Flynt\Features\Components\Component;
 use Flynt\Utils\Oembed;
 
-add_action('wp_enqueue_scripts', function () {
-    Component::enqueueAssets('BlockVideoOembed');
-});
-
 add_filter('Flynt/addComponentData?name=BlockVideoOembed', function ($data) {
-    $data['oembedLazyLoad'] = Oembed::setSrcAsDataAttribute(
+    add_action('wp_enqueue_scripts', function () {
+        Component::enqueueAssets('BlockVideoOembed', [
+            [
+                'name' => 'lazysizes',
+                'type' => 'script',
+                'path' => 'vendor/lazysizes.js'
+            ]
+        ]);
+    });
+
+    $data['video'] = Oembed::setSrcAsDataAttribute(
         $data['oembed'],
         [
             'autoplay' => 'true'
--- Components/BlockVideoOembed/script.js
+++ Components/BlockVideoOembed/script.js
@@ -1,3 +1,4 @@
+import 'file-loader?name=vendor/lazysizes.js!uglify-loader!lazysizes'
 import $ from 'jquery'
 
 class BlockVideoOembed extends window.HTMLDivElement {
@@ -9,23 +10,45 @@ class BlockVideoOembed extends window.HTMLDivElement {
   }
 
   resolveElements () {
-    this.$posterImage = $('.oembedVideo-posterImage', this)
-    this.$video = $('.oembedVideo-video', this)
+    this.$posterImage = $('.video-posterImage', this)
+    this.$videoContent = $('.video-content', this)
     this.$iframe = $('iframe', this)
+    this.$play = $('.play', this)
+    this.$loader = $('.loader', this)
   }
 
   connectedCallback () {
-    this.$iframe.on('load', this.onIframeLoad)
-    this.$.on('click', this.$posterImage.selector, this.setIframeSrc)
+    this.$.on('click', this.$posterImage.selector, this.setIframeSrc.bind(this))
+    this.$iframe.on('load', this.onIframeLoad.bind(this))
+    this.videoIsStopped()
   }
 
   setIframeSrc = () => {
     this.$iframe.attr('src', this.$iframe.data('src'))
+    this.videoIsLoading()
   }
 
   onIframeLoad = () => {
-    this.$video.addClass('oembedVideo-video-isVisible')
-    this.$posterImage.addClass('oembedVideo-posterImage-isHidden')
+    if (this.$iframe.attr('src') !== undefined && this.$iframe.attr('src') !== '') {
+      this.$videoContent.addClass('video-content-isVisible')
+      this.$posterImage.addClass('video-posterImage-isHidden')
+      this.videoIsPlaying()
+    }
+  }
+
+  videoIsStopped () {
+    this.$play.addClass('play--isVisible')
+    this.$loader.removeClass('loader--isVisible')
+  }
+
+  videoIsLoading () {
+    this.$play.removeClass('play--isVisible')
+    this.$loader.addClass('loader--isVisible')
+  }
+
+  videoIsPlaying () {
+    this.$play.removeClass('play--isVisible')
+    this.$loader.removeClass('loader--isVisible')
   }
 }
 
