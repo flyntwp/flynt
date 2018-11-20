b8c3b654a6cd7b1d2a2cbc7bc7a62b405540e8d6
Markus Müller <markus.mueller@live.de>
2018-11-20 18:53:50 +0100

    feat(Acf): add youtube oembed acf caching

--- Features/Acf/functions.php
+++ Features/Acf/functions.php
@@ -14,3 +14,20 @@ use Flynt\Utils\Feature;
 Loader::setup(Feature::getOption('flynt-acf', 0));
 
 add_action('Flynt/afterRegisterFeatures', 'Flynt\Features\Acf\Loader::init');
+
+add_filter('pre_http_request', function ($preempt, $args, $url) {
+    if (strpos($url, 'https://www.youtube.com/oembed') !== false || strpos($url, 'https://vimeo.com/api/oembed') !== false) {
+        $response = wp_cache_get($url, 'oembedCache');
+        if (!empty($response)) {
+            return $response;
+        }
+    }
+    return false;
+}, 10, 3);
+
+add_filter('http_response', function ($response, $args, $url) {
+    if (strpos($url, 'https://www.youtube.com/oembed') !== false || strpos($url, 'https://vimeo.com/api/oembed') !== false) {
+        wp_cache_set($url, $response, 'oembedCache');
+    }
+    return $response;
+}, 10, 3);
