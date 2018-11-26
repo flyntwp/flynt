6cb8a2bebf0f66ecebe23f42eb2cd3c2dee4a134
Phil Bennett <phil@bleech.de>
2018-11-26 11:32:33 +0100

    feat(NavigationMain): fixing navigation main

---
+++ Components/NavigationMain/README.md
@@ -0,0 +1,14 @@
+# NavigationMain
+
+## Category
+Navigation
+
+## Tags
+navigation, sticky
+
+## Description
+An absolutely positioned navigation with a brand logo and a WordPress menu.
+
+## Fields
+* brandLogo
+* menuItems
---
+++ Components/NavigationMain/script.js
@@ -0,0 +1,43 @@
+import $ from 'jquery'
+
+class NavigationMain extends window.HTMLElement {
+  constructor (self) {
+    self = super(self)
+    self.$ = $(self)
+    self.resolveElements()
+    return self
+  }
+
+  resolveElements () {
+    this.$hamburger = $('.hamburger', this)
+    this.$navigation = $('.navigationMain', this)
+    this.$body = $('body')
+    this.$menu = $('.menu', this)
+    this.noTransitionClass = 'menu-hasNoTransition'
+  }
+
+  connectedCallback () {
+    this.$.on('click', '.hamburger', this.triggerMenu)
+    $(window).on('resize', () => {
+      this.disableTransition()
+      clearTimeout(this.enableTransitionTimeout)
+      this.enableTransitionTimeout = setTimeout(this.enableTransition, 150)
+    })
+  }
+
+  disableTransition () {
+    this.$menu.addClass(this.noTransitionClass)
+  }
+
+  enableTransition = () => {
+    this.$menu.removeClass(this.noTransitionClass)
+  }
+
+  triggerMenu = (e) => {
+    e.preventDefault()
+    this.$navigation.toggleClass('navigationMain-isActive')
+    this.$body.toggleClass('navigationMain-isActive')
+  }
+}
+
+window.customElements.define('flynt-navigation-main', NavigationMain, {extends: 'nav'})
