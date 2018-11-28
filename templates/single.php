0f6822f218b1a91fa35cf337d951dd03d2804246
Dominik Tränklein <dominik@bleech.de>
2018-11-28 16:10:39 +0100

    feat(Features/Templates): remove template configs (#295)
    (cherry picked from commit 09a550f53238d6368a28cee338c03f71550a45de)
    BREAKING CHANGE

--- templates/single.php
+++ templates/single.php
@@ -1,3 +1,38 @@
 <?php
 
-Flynt\echoHtmlFromConfigFile('single.json');
+Flynt\echoHtmlFromConfig([
+    'name' => 'DocumentDefault',
+    'areas' => [
+        'layout' => [
+            [
+                'name' => 'LayoutSinglePost',
+                'areas' => [
+                    'mainHeader' => [
+                        [
+                            'name' => 'NavigationMain',
+                            'customData' => [
+                                'menuSlug' => 'navigation_main'
+                            ]
+                        ]
+                    ],
+                    'pageComponents' => [
+                        [
+                            'name' => 'ComponentLoaderFlexible',
+                            'customData' => [
+                                'fieldGroup' => 'postComponents'
+                            ]
+                        ]
+                    ],
+                    'mainFooter' => [
+                        [
+                            'name' => 'NavigationFooter',
+                            'customData' => [
+                                'menuSlug' => 'navigation_footer'
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+]);
