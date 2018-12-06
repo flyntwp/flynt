9f39a78f655a78fd522db99b0e5e78e82e2b226e
koukouchris <christina@bleech.de>
2018-12-06 10:32:06 +0100

    component(HeroCTA): initial setup

---
+++ Components/HeroCta/script.js
@@ -0,0 +1,18 @@
+import $ from 'jquery'
+
+class HeroCta extends window.HTMLDivElement {
+  constructor (self) {
+    self = super(self)
+    self.$ = $(self)
+    self.resolveElements()
+    return self
+  }
+
+  resolveElements () {
+  }
+
+  connectedCallback () {
+  }
+}
+
+window.customElements.define('flynt-hero-cta', HeroCta, {extends: 'div'})
