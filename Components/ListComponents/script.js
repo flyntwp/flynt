af9e35cdff60230dd2ed19e7645fe09e59ed2271
Steffen Bewersdorff <steffen@bleech.de>
2018-12-09 23:53:56 +0100

    refactor(ListComponents): implemented parallax scroll animation on preview images as alternative to hover scroll

--- Components/ListComponents/script.js
+++ Components/ListComponents/script.js
@@ -9,35 +9,74 @@ class ListComponents extends window.HTMLDivElement {
   }
 
   resolveElements () {
-    this.animating = []
+    this.$window = $(window)
     this.$componentImageWrappers = $('.component-previewImage', this)
+    this.$componentImageMobile = $('.component-previewImage img', this)
   }
 
   connectedCallback () {
-    this.$.on('mouseover', this.$componentImageWrappers.selector, this.startPreviewScroll.bind(this))
-    this.$.on('mouseout', this.$componentImageWrappers.selector, this.stopPreviewScroll.bind(this))
+    // this.$.on('mouseenter mouseleave', this.$componentImageWrappers.selector, this.toggleHoverScroll.bind(this))
+    this.$window.on('resize', this.setParallaxConfig.bind(this))
+    $(document).on('lazyloaded', this.setParallaxConfig.bind(this))
+    this.$window.on('scroll', this.startParallaxScroll.bind(this))
   }
 
-  startPreviewScroll (e) {
+  toggleHoverScroll (e) {
     let $imageWrapper = $(e.currentTarget)
+    let $imageWrapperHeight = $imageWrapper.height()
     let $image = $imageWrapper.find('img')
-    if ($image.height() > $imageWrapper.height()) {
-      $image.css('transition', 'transform ' + this.easeOutCubic(($image.height() - $imageWrapper.height()) / 200) + 's cubic-bezier(0.215, 0.61, 0.355, 1)')
-      $image.css('transform', 'translateY(-' + ($image.height() - $imageWrapper.height()) + 'px)')
+    let $imageHeight = $image.height()
+
+    if ($imageHeight > $imageWrapperHeight) {
+      if (e.type === 'mouseenter') {
+        $image.css('transition', 'transform ' + (($imageHeight - $imageWrapperHeight) / $imageWrapperHeight) + 's cubic-bezier(0.215, 0.61, 0.355, 1)')
+        $image.css('transform', 'translateY(-' + ($imageHeight - $imageWrapperHeight) + 'px)')
+      } else {
+        $image.css('transition', 'transform ' + (($imageHeight - $imageWrapperHeight) / $imageWrapperHeight) + 's cubic-bezier(0.23, 1, 0.32, 1)')
+        $image.css('transform', 'translateY(0)')
+      }
     }
   }
 
-  stopPreviewScroll (e) {
-    let $imageWrapper = $(e.currentTarget)
-    let $image = $imageWrapper.find('img')
-    if ($image.height() > $imageWrapper.height()) {
-      $image.css('transition', 'transform ' + this.easeOutCubic(($image.height() - $imageWrapper.height()) / 200) + 's cubic-bezier(0.23, 1, 0.32, 1)')
-      $image.css('transform', 'translateY(0)')
-    }
+  setParallaxConfig () {
+    let windowHeight = this.$window.height()
+
+    this.$componentImageMobile.each((index, el) => {
+      let $image = $(el)
+      let $imageWrapper = $image.parent()
+      let $previewWrapper = $image.closest('.component-previews')
+
+      let imageOverflow = $image.height() - $imageWrapper.height()
+      let topOffset = $previewWrapper.offset().top
+      let startOffset = topOffset - windowHeight * 0.4
+      let endOffset = topOffset + $previewWrapper.height() - windowHeight * 0.6
+
+      $image.data('parallaxConfig', {
+        imageOverflow: imageOverflow,
+        startOffset: startOffset,
+        endOffset: endOffset
+      })
+    })
+
+    this.startParallaxScroll()
   }
 
-  easeOutCubic (t) {
-    return (--t) * t * t + 1
+  startParallaxScroll (e) {
+    let scrollTop = this.$window.scrollTop()
+
+    this.$componentImageMobile.each((index, el) => {
+      let $image = $(el)
+      let parallaxConfig = $image.data('parallaxConfig')
+
+      if (parallaxConfig && parallaxConfig.imageOverflow > 0) {
+        let scrollPercentage = (scrollTop - parallaxConfig.startOffset) / (parallaxConfig.endOffset - parallaxConfig.startOffset)
+        scrollPercentage = Math.min(Math.max(scrollPercentage, 0), 1)
+        let move = (scrollPercentage * parallaxConfig.imageOverflow) * -1
+        if (!e || (scrollTop >= parallaxConfig.startOffset && scrollTop < parallaxConfig.endOffset)) {
+          $image.css('transform', 'translateY(' + move + 'px)')
+        }
+      }
+    })
   }
 }
 
