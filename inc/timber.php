90876de9d7b90928ea223edb9d5c643f9aa8bdc8
Dominik Tränklein <dominik@bleech.de>
2018-12-12 21:53:59 +0100

    feat(NoCP): enqueue theme assets early

--- inc/timber.php
+++ inc/timber.php
@@ -42,53 +42,52 @@ add_action('after_setup_theme', function () {
     new Timber\Timber();
 });
 
-add_action('Flynt/afterRegisterFeatures', function () {
-    add_action('wp_enqueue_scripts', function () {
-        Asset::register([
-            'name' => 'console-polyfill',
-            'type' => 'script',
-            'path' => 'vendor/console.js'
-        ]);
-        Asset::register([
-            'name' => 'babel-polyfill',
-            'type' => 'script',
-            'path' => 'vendor/babel-polyfill.js'
-        ]);
-        Asset::register([
-            'name' => 'document-register-element',
-            'type' => 'script',
-            'path' => 'vendor/document-register-element.js'
-        ]);
-        Asset::register([
-            'name' => 'normalize',
-            'path' => 'vendor/normalize.css',
-            'type' => 'style'
-        ]);
-        Asset::enqueue([
-            'name' => 'Flynt/assets',
-            'path' => 'assets/script.js',
-            'type' => 'script',
-            'dependencies' => [
-                'console-polyfill',
-                'babel-polyfill',
-                'document-register-element',
-            ],
-        ]);
-        Asset::enqueue([
-            'name' => 'Flynt/assets',
-            'path' => 'assets/style.css',
-            'type' => 'style',
-            'dependencies' => [
-                'normalize',
-            ],
-        ]);
+add_action('wp_enqueue_scripts', function () {
+    Asset::register([
+        'name' => 'console-polyfill',
+        'type' => 'script',
+        'path' => 'vendor/console.js'
+    ]);
+    Asset::register([
+        'name' => 'babel-polyfill',
+        'type' => 'script',
+        'path' => 'vendor/babel-polyfill.js'
+    ]);
+    Asset::register([
+        'name' => 'document-register-element',
+        'type' => 'script',
+        'path' => 'vendor/document-register-element.js'
+    ]);
+    Asset::register([
+        'name' => 'normalize',
+        'path' => 'vendor/normalize.css',
+        'type' => 'style'
+    ]);
+    Asset::enqueue([
+        'name' => 'Flynt/assets',
+        'path' => 'assets/script.js',
+        'type' => 'script',
+        'dependencies' => [
+            'jquery',
+            'console-polyfill',
+            'babel-polyfill',
+            'document-register-element',
+        ],
+    ]);
+    Asset::enqueue([
+        'name' => 'Flynt/assets',
+        'path' => 'assets/style.css',
+        'type' => 'style',
+        'dependencies' => [
+            'normalize',
+        ],
+    ]);
 
-        // separately enqueued after components script.js to being able
-        // to set global config variables before lazysizes is loaded
-        Asset::enqueue([
-            'name' => 'lazysizes',
-            'type' => 'script',
-            'path' => 'vendor/lazysizes.js'
-        ]);
-    });
+    // separately enqueued after components script.js to being able
+    // to set global config variables before lazysizes is loaded
+    Asset::enqueue([
+        'name' => 'lazysizes',
+        'type' => 'script',
+        'path' => 'vendor/lazysizes.js'
+    ]);
 });
