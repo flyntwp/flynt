a307ed388c2f819225af6b4423e10e1af4b25a7d
Dominik Tränklein <dominik@bleech.de>
2018-12-13 23:23:19 +0100

    refactor(ListFacts): convert to new format

--- Components/ListFacts/functions.php
+++ Components/ListFacts/functions.php
@@ -5,20 +5,18 @@ namespace Flynt\Components\ListFacts;
 use Flynt\Features\Components\Component;
 
 add_filter('Flynt/addComponentData?name=ListFacts', function ($data) {
-    add_action('wp_enqueue_scripts', function () {
-        Component::enqueueAssets('ListFacts', [
-            [
-                'name' => 'countup',
-                'path' => 'vendor/countup.js',
-                'type' => 'script'
-            ],
-            [
-                'name' => 'intersection-observer',
-                'type' => 'script',
-                'path' => 'vendor/intersection-observer.js'
-            ],
-        ]);
-    });
+    Component::enqueueAssets('ListFacts', [
+        [
+            'name' => 'countup',
+            'path' => 'vendor/countup.js',
+            'type' => 'script'
+        ],
+        [
+            'name' => 'intersection-observer',
+            'type' => 'script',
+            'path' => 'vendor/intersection-observer.js'
+        ],
+    ]);
 
     return $data;
 });
