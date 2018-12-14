e69f34b8aa13ae1f9fb67aa912c575941ffc34d2
Dominik Tränklein <dominik@bleech.de>
2018-12-14 17:29:57 +0100

    refactor(gulp): extract setWatchMode task

--- gulpfile.js/tasks/zz_default.js
+++ gulpfile.js/tasks/zz_default.js
@@ -1,11 +1,12 @@
 const gulp = require('gulp')
 
 module.exports = function (config) {
+  gulp.task('setWatchMode', function (done) {
+    global.watchMode = true
+    done()
+  })
   gulp.task('default', gulp.series(
-    function (done) {
-      global.watchMode = true
-      done()
-    },
+    'setWatchMode',
     'clean',
     gulp.parallel(['copy', 'stylus']),
     'watch'
