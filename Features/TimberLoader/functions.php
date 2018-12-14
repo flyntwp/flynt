0b103dd64fc2081fb0fadce06196f2644ee4f3ed
Steffen Bewersdorff <steffen@bleech.de>
2018-12-14 18:39:50 +0100

    fix(TimberLoader): rounded placeHolder values and changed passed image dimensions to resized image w

--- Features/TimberLoader/functions.php
+++ Features/TimberLoader/functions.php
@@ -106,6 +106,8 @@ function convertToTimberPost($value)
 
 add_action('timber/twig/filters', function ($twig) {
     $twig->addFunction(new \Twig_SimpleFunction('placeholderImage', function ($width, $height, $color = null) {
+        $width = round($width);
+        $height = round($height);
         $colorRect = $color ? "<rect width='{$width}' height='{$height}' style='fill:$color' />" : '';
         $svg = "<svg width='{$width}' height='{$height}' xmlns='http://www.w3.org/2000/svg'>{$colorRect}</svg>";
         return "data:image/svg+xml;base64," . base64_encode($svg);
@@ -113,31 +115,6 @@ add_action('timber/twig/filters', function ($twig) {
 
     return $twig;
 });
-
-function aspectToRatio($n, $tolerance = 1.e-3)
-{
-    $h1 = 1;
-    $h2 = 0;
-    $k1 = 0;
-    $k2 = 1;
-    $b = 1 / $n;
-    do {
-        $b = 1/$b;
-        $a = floor($b);
-        $aux = $h1;
-        $h1 = $a * $h1 + $h2;
-        $h2 = $aux;
-        $aux = $k1;
-        $k1 = $a * $k1 + $k2;
-        $k2 = $aux;
-        $b = $b - $a;
-    } while (abs($n - $h1 / $k1) > $n * $tolerance);
-
-    return [
-        'width' => $h1,
-        'height' => $k1
-    ];
-}
 
 function generateToken($args)
 {
