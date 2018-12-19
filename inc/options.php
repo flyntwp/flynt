d75d331c387a2c192cebbda2a7cf3316127d529c
Dominik Tränklein <dominik@bleech.de>
2018-12-19 11:40:08 +0100

    fix(Options): add automatic option loading again

---
+++ inc/options.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flynt;
+
+use Flynt\Utils\Options;
+
+add_filter(
+    'Flynt/addComponentData',
+    'Flynt\\addOptionsToComponent',
+    9,
+    2
+);
+
+function addOptionsToComponent($data, $componentName)
+{
+    // get fields for this component
+    $options = array_reduce(array_keys(Options::OPTION_TYPES), function ($carry, $optionType) use ($componentName) {
+        return array_merge($carry, Options::get($optionType, 'Component', $componentName));
+    }, []);
+    // don't overwrite existing data
+    return array_merge($options, $data);
+}
