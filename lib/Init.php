93b7bca428f2d818cee21691a72744188013629d
Dominik Tränklein <dominik@bleech.de>
2018-12-19 14:53:51 +0100

    refactor(php): move 'core' files into lib folder

---
+++ lib/Init.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Flynt;
+
+use Flynt;
+use Flynt\Utils\Asset;
+use Flynt\Utils\Feature;
+use Flynt\Utils\FileLoader;
+use Flynt\Utils\StringHelpers;
+use Timber\Timber;
+
+class Init
+{
+    public static function initTheme()
+    {
+        // initialize plugin defaults
+        Flynt\initDefaults();
+
+        // Set to true to load all assets from a CDN if there is one specified
+        Asset::loadFromCdn(false);
+
+        new Timber();
+    }
+
+    public static function loadFeatures()
+    {
+        $basePath = get_template_directory() . '/dist/Features';
+
+        global $flyntCurrentOptionCategory;
+        $flyntCurrentOptionCategory = 'feature';
+
+        Feature::register('YoutubeNoCookieEmbed', $basePath);
+
+        // initialize ACF Field Groups and Option Pages
+        Feature::register('Acf', $basePath, [[
+            'FlexibleContentToggle',
+            'GoogleMaps'
+        ]]);
+
+        // enable admin notices
+        Feature::register('AdminNotices', $basePath);
+
+        // use timber rendering
+        Feature::register('TimberLoader', $basePath);
+
+        // load jQuery in footer by default
+        Feature::register('Jquery', $basePath);
+
+        // clean up some things
+        Feature::register('CleanHead', $basePath);
+        Feature::register('CleanRss', $basePath);
+        Feature::register('MimeTypes', $basePath);
+        Feature::register('RemoveEditor', $basePath);
+        Feature::register('TinyMce', $basePath);
+        Feature::register('BaseStyle', $basePath);
+
+        // add components previews
+        Feature::register('AdminComponentPreview', $basePath);
+
+        // google analytics
+        Feature::register('GoogleAnalytics', $basePath);
+
+        // hide protected posts
+        Feature::register('HideProtectedPosts', $basePath);
+
+        // move yoast seo plugin box to the bottom of the backend interface
+        Feature::register('YoastToBottom', $basePath);
+
+        Feature::register('PasswordForm', $basePath);
+        Feature::register('ExternalScriptLoader', $basePath);
+        Feature::register('Lodash', $basePath);
+        Feature::register('ComponentLogServer', $basePath);
+
+        do_action('Flynt/afterRegisterFeatures');
+    }
+
+    public static function loadComponents()
+    {
+        $basePath = get_template_directory() . '/dist/Components';
+        global $flyntCurrentOptionCategory;
+        $flyntCurrentOptionCategory = 'component';
+        Flynt\registerComponentsFromPath($basePath);
+        do_action('Flynt/afterRegisterComponents');
+    }
+
+    public static function setTemplateDirectory()
+    {
+        add_action('after_switch_theme', function () {
+            $stylesheet = get_option('stylesheet');
+
+            if (basename($stylesheet) !== 'templates') {
+                update_option('stylesheet', $stylesheet . '/templates');
+            }
+        });
+
+        add_filter('stylesheet', function ($stylesheet) {
+            return dirname($stylesheet);
+        });
+    }
+
+    public static function checkRequiredPlugins()
+    {
+        $flyntCoreActive = function_exists('\\Flynt\\renderComponent');
+        $acfActive = class_exists('acf');
+
+        if (!$flyntCoreActive) {
+            self::notifyRequiredPluginIsMissing('Flynt Core');
+        }
+
+        if (!$acfActive) {
+            self::notifyRequiredPluginIsMissing('ACF');
+        }
+
+        if (!$acfActive || !$flyntCoreActive) {
+            add_filter('template_include', function () {
+                die(
+                    'One or more required plugins are not activated! Please <a href="'
+                    . esc_url(admin_url('plugins.php'))
+                    . '">activate or install the required plugin(s)</a> and reload the page.'
+                );
+            });
+        }
+
+        return $acfActive && $flyntCoreActive;
+    }
+
+    protected static function notifyRequiredPluginIsMissing($pluginName)
+    {
+        add_action('admin_notices', function () use ($pluginName) {
+            echo "<div class=\"error\"><p>${pluginName} Plugin not activated. Make sure you activate the plugin on the <a href=\""
+                . esc_url(admin_url('plugins.php')) . "\">plugin page</a>.</p></div>";
+        });
+    }
+}
