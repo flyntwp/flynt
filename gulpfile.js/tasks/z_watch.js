6dc27be8a32d0f5b233594fd51ce2e4804df6556
Dominik Tränklein <dominik@bleech.de>
2018-12-19 14:36:46 +0100

    fix(gulp): clean watch task

--- gulpfile.js/tasks/z_watch.js
+++ gulpfile.js/tasks/z_watch.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp')
 const path = require('path')
 const fs = require('fs')
-let touch, globby
 
 const extensionMappings = {
   '.styl': '.css'
@@ -36,64 +35,11 @@ function watchAndDelete (src, callback, dest) {
     })
 }
 
-function findFileInParentDirectory (filename, directory, stopSearchDirnames = []) {
-  if (stopSearchDirnames.includes(directory)) { return null }
-  if (!directory) {
-    directory = path.dirname(module.parent.filename)
-  }
-  const file = path.resolve(directory, filename)
-  if (fs.existsSync(file) && fs.statSync(file).isFile()) {
-    return file
-  }
-  const parent = path.resolve(directory, '..')
-  if (parent === directory) {
-    return null
-  }
-  return findFileInParentDirectory(filename, parent, stopSearchDirnames)
-}
-
-function checkForSassPartial (relativePath, config) {
-  if (path.basename(relativePath)[0] === config.watch.sassPartials.partialCssFilenamePrefix) {
-    const fileDir = path.dirname(path.join(process.cwd(), relativePath))
-    const styleCssFilepath = findFileInParentDirectory(
-      config.watch.sassPartials.rootCssFilename,
-      fileDir,
-      config.watch.sassPartials.stopSearchDirnames
-    )
-    if (styleCssFilepath !== null && fs.existsSync(styleCssFilepath)) {
-      touch.sync(styleCssFilepath)
-    }
-  }
-}
-
-function checkForCssVariablesSass (relativePath, config) {
-  if (config.watch.hardReloadOnSassFiles.includes(relativePath)) {
-    globby(config.sass, {}).then((files) => {
-      Promise.all(
-        files.map(function (file) {
-          return new Promise((resolve, reject) => {
-            touch(file, {}, (err) => {
-              if (err) reject(err)
-              resolve(file)
-            })
-          })
-        }, [])
-      )
-    })
-  }
-}
-
 module.exports = function (config) {
   gulp.task('watch:files', function () {
     const browserSync = require('browser-sync')
-    globby = require('globby')
-    touch = require('touch')
     watchAndDelete(config.copy, gulp.series('copy'), config.dest)
-    watchAndDelete(config.watch.sass, gulp.series('sass'), config.dest)
-      .on('all', function (event, relativePath) {
-        checkForSassPartial(relativePath, config)
-        checkForCssVariablesSass(relativePath, config)
-      })
+    gulp.watch(config.watch.sass, gulp.series('sass'))
     gulp.watch(config.watch.php, function () { browserSync.reload() })
   })
   gulp.task(
