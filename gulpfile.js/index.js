89c8aebcfb9ab14af4ac072225c5d37c06ae225b
Dominik Tränklein <dominik@bleech.de>
2019-03-10 11:12:01 +0100

    build(gulp): flatten gulpfile.js dir into file

--- gulpfile.js/index.js
+++
@@ -1,8 +0,0 @@
-const config = require('../build-config').gulp
-
-require('./tasks/replaceVersion')(config)
-require('./tasks/revAssets')(config)
-require('./tasks/revRevvedFiles')(config)
-require('./tasks/revStaticFiles')(config)
-require('./tasks/revUpdateReferences')(config)
-require('./tasks/rev')(config)
