a3774e05111bdaf765cc7430be0f2e0e61226032
Dominik Tränklein <dominik@bleech.de>
2019-03-13 11:11:06 +0100

    refactor(HideProtectedPosts): move into inc

--- inc/hideProtectedPosts.php
+++ inc/hideProtectedPosts.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace Flynt\Features\HideProtectedPosts;
+/**
+ * Hide password protected posts from listings when using the loop or `get_posts`.
+ */
+namespace Flynt\HideProtectedPosts;
 
 add_action('pre_get_posts', function ($query) {
     if (!$query->is_singular() && !is_admin()) {
