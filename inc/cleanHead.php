ced83a7d389ed393d795d356a15a20b9857346f3
Dominik Tränklein <dominik@bleech.de>
2019-03-13 11:10:18 +0100

    refactor(CleanHead): move into inc

--- inc/cleanHead.php
+++ inc/cleanHead.php
@@ -1,6 +1,22 @@
 <?php
 
-namespace Flynt\Features\CleanHead;
+/**
+ * Collection of the following clean up tasks in the HTML head element:
+ * - Remove unnecessary extra feed links
+ * - Remove RSD link
+ * - Remove WLW link
+ * - Remove Adjacent Posts link
+ * - Remove WP Emoji Scripts and Styles
+ * - Remove WP Generator Meta Data
+ * - Remove WP Shortlink
+ * - Remove WP OEmbed Links and JS
+ * - Remove REST Output Link
+ * - Disable WP hard-coded CSS
+ * - Only display media in style tags if it makes sense
+ * - Remove type='text/javascript' from script tags and replace single quotes with double quotes
+ */
+
+namespace Flynt\CleanHead;
 
 /**
  * Clean up wp_head()
@@ -62,8 +78,9 @@ add_filter('style_loader_tag', function ($input) {
  * cleanScriptTag
  *
  * Clean up output of <script> tags
+ * TODO: fix or remove (broken on wp > 5.0.0)
  */
-add_filter('script_loader_tag', function ($input) {
-    $input = str_replace("type='text/javascript' ", '', $input);
-    return str_replace("'", '"', $input);
-});
+// add_filter('script_loader_tag', function ($input) {
+//     $input = str_replace("type='text/javascript' ", '', $input);
+//     return str_replace("'", '"', $input);
+// });
