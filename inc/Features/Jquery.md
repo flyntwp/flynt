bb9240b0ac83056f1ae51993da5f86fd66d09f2e
Dominik Tränklein <dominik@bleech.de>
2019-03-13 11:11:30 +0100

    refactor(Jquery): move into inc

--- inc/Features/Jquery.md
+++
@@ -1,5 +0,0 @@
-# jQuery (Flynt Feature)
-
-Loads jQuery before the closing body tag by default. Can be overwritten if there is a script in the head with jQuery as a dependency.
-
-If the Asset utility has `loadFromCdn` set to true, it will load from Google's CDN falling back to the default WordPress script. This setting can be changed in the `lib/Init.php` file inside the `initTheme` function.
--- inc/jquery.php
+++ inc/jquery.php
@@ -1,6 +1,10 @@
 <?php
-
-namespace Flynt\Features\Jquery;
+/**
+ * Loads jQuery before the closing body tag by default. Can be overwritten if there is a script in the head with jQuery as a dependency.
+ *
+ * If the Asset utility has `loadFromCdn` set to true, it will load from Google's CDN falling back to the default WordPress script. This setting can be changed in the `lib/Init.php` file inside the `initTheme` function.
+ */
+namespace Flynt\Jquery;
 
 use Flynt\Utils\Asset;
 
