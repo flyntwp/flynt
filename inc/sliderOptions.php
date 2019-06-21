493250fbdba0af8d02eb55ccc69cd4c0d238e0ce
koukouchris <christina@bleech.de>
2019-06-21 18:10:35 +0200

    refactor(GridPostsSlider): swiper

---
+++ inc/sliderOptions.php
@@ -0,0 +1,52 @@
+<?php
+
+use Flynt\Utils\Options;
+
+Options::addTranslatable('SliderOptions', [
+    [
+        'label' => 'Accessibility',
+        'name' => 'a11y',
+        'type' => 'group',
+        'instructions' => 'Text labels for screen readers.',
+        'sub_fields' => [
+            [
+                'label' => 'Next Slide Button Text',
+                'name' => 'nextSlideMessage',
+                'type' => 'text',
+                'default_value' => 'Next Slide',
+                'required' => 1
+            ],
+            [
+                'label' => 'Previous Slide Button Text',
+                'name' => 'prevSlideMessage',
+                'type' => 'text',
+                'default_value' => 'Previous Slide',
+                'required' => 1
+            ],
+            [
+                'label' => 'First Slide Text',
+                'name' => 'firstSlideMessage',
+                'type' => 'text',
+                'instructions' => 'Text for previous button when swiper is on first slide.',
+                'default_value' => 'This is the first slide',
+                'required' => 1
+            ],
+            [
+                'label' => 'Last Slide Text',
+                'name' => 'lastSlideMessage',
+                'type' => 'text',
+                'instructions' => 'Text for previous button when swiper is on last slide.',
+                'default_value' => 'This is the last slide',
+                'required' => 1
+            ],
+            [
+                'label' => 'Pagination Bullet Message',
+                'name' => 'paginationBulletMessage',
+                'type' => 'text',
+                'instructions' => '`{{index}}` will be replaced for the slide number.',
+                'default_value' => 'Go to slide {{index}}',
+                'required' => 1,
+            ],
+        ],
+    ],
+], 'feature');
