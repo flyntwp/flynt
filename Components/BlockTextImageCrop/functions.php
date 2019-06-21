5c104a59206a3ea54ef7cf16db5d6af4a12e4380
JobvdMeer <jacobus@bleech.de>
2019-06-21 10:14:57 +0200

    feat(BlockTextImageCrop): init setup component

---
+++ Components/BlockTextImageCrop/functions.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Flynt\Components\BlockTextImageCrop;
+
+use Flynt\Api;
+
+Api::registerFields('BlockTextImageCrop', [
+    'layout' => [
+        'name' => 'blockTextImageCrop',
+        'label' => 'Block: Text Image Crop',
+        'sub_fields' => [
+            [
+                'label' => 'Image Position',
+                'name' => 'imagePosition',
+                'type' => 'button_group',
+                'choices' => [
+                    'imageLeft' => '<i class=\'dashicons dashicons-align-left\' title=\'Image on the left\'></i>',
+                    'imageRight' => '<i class=\'dashicons dashicons-align-right\' title=\'Image on the right\'></i>'
+                ]
+            ],
+            [
+                'label' => 'Image',
+                'name' => 'image',
+                'type' => 'image',
+                'preview_size' => 'medium',
+                'instructions' => '',
+                'max_size' => 4,
+                'required' => true,
+                'mime_types' => 'gif,jpg,jpeg,png'
+            ],
+            [
+                'name' => 'contentHtml',
+                'label' => 'Content',
+                'type' => 'wysiwyg',
+                'delay' => 1,
+                'media_upload' => 0,
+                'toolbar' => 'custom',
+                'required' => true,
+                'wrapper' => [
+                    'class' => 'autosize',
+                ],
+            ],
+            [
+                'label' => 'Link',
+                'type' => 'link',
+                'instructions' => 'Here you can link to every page you like',
+                'name' => 'link',
+                'return_format' => 'array'
+            ]
+        ]
+    ]
+]);
