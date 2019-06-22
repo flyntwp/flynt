0a48d1f4d8c1d00d8789697f6823740c1cc9f73f
Mykola Chernyshevskyi <mykola@bleech.de>
2019-06-22 17:42:49 +0200

    feat(BlockCountUp): add icons and countup

---
+++ Components/BlockCountUp/script.js
@@ -0,0 +1,54 @@
+
+/* globals IntersectionObserver, CountUp */
+import $ from 'jquery'
+import 'intersection-observer'
+import { CountUp } from 'countup.js';
+
+class BlockCountUp extends window.HTMLDivElement {
+  constructor (self) {
+    self = super(self)
+    self.$ = $(self)
+    self.resolveElements()
+    return self
+  }
+
+  resolveElements () {
+    this.$items = $('.item', this)
+  }
+
+  connectedCallback () {
+    let options = {
+      threshold: 1.0
+    }
+    this.observer = new IntersectionObserver(this.triggerAnimation.bind(this), options)
+    $.each(this.$items, (i, item) => {
+      this.observer.observe(item)
+    })
+  }
+
+  triggerAnimation (entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        this.observer.unobserve(entry.target)
+
+        const $numbers = $(entry.target).find('.number')
+        $.each($numbers, (i, number) => {
+          const $number = $(number)
+          const displayNumber = $number.data('number')
+          const displaySuffix = $number.data('suffix')
+          const displayPrefix = $number.data('prefix')
+          const duration = 2
+          let count = new CountUp(number, displayNumber, {
+            duration: duration,
+            decimalPlaces: 0,
+            prefix: displayPrefix,
+            suffix: displaySuffix
+          })
+          count.start()
+        })
+      }
+    })
+  }
+}
+
+window.customElements.define('flynt-block-count-up', BlockCountUp, { extends: 'div' })
--- assets/admin.js
+++ assets/admin.js
@@ -1,5 +1,23 @@
 import './admin.scss'
 
+$(document).ready(function () {
+  setTimeout(() => {
+    window.feather.replace()
+  }, 4000)
+
+  $('.select2').on('select2:change', function(e) {
+    console.log('change')
+  })
+
+  $('body').on('click', '.select2-container', function() {
+    window.feather.replace()
+  })
+
+  $('body').on('change', '.select2-hidden-accessible', function() {
+    window.feather.replace()
+  })
+})
+
 function importAll (r) {
   r.keys().forEach(r)
 }
