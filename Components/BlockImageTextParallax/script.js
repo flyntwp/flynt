729f4999948e85d8aad29d97572c925ab4eb1a42
Nikola Jovanovic <nikola@bleech.de>
2019-06-22 14:03:06 +0200

    feat(Block: Image Text Parallax): Add parallax plugin.

--- Components/BlockImageTextParallax/script.js
+++ Components/BlockImageTextParallax/script.js
@@ -12,61 +12,14 @@ class BlockImageTextParallax extends window.HTMLDivElement {
   }
 
   resolveElements () {
-    // this.$parallax = $('[data-parallax]', this)
-    // this.$mainContent = $('.mainContent', this)
-    // this.$window = $(window)
+
   }
 
   connectedCallback () {
-    // this.setParallax()
+    var rellax = new Rellax('.rellax', {
 
-    // this.$window.on('resize', this.setParallax.bind(this))
-    // this.onElementHeightChange(this.$mainContent.get(0), () => {
-    //   if (this.rellax) {
-    //     this.rellax.refresh()
-    //   }
-    // })
+    })
   }
-
-  // onElementHeightChange (elm, callback) {
-  //   let lastHeight = elm.clientHeight
-  //   let newHeight
-
-  //   (function run () {
-  //     newHeight = elm.clientHeight
-  //     if (lastHeight !== newHeight) {
-  //       callback()
-  //     }
-  //     lastHeight = newHeight
-
-  //     if (elm.onElementHeightChangeTime) {
-  //       clearTimeout(elm.onElementHeightChangeTimer)
-  //     }
-
-  //     elm.onElementHeightChangeTimer = setTimeout(run, 200)
-  //   })()
-  // }
-
-  // setParallax () {
-  //   if (this.isMobile()) {
-  //     if (this.rellax) {
-  //       this.rellax.destroy()
-  //       this.rellax = false
-  //     }
-  //   } else {
-  //     if (!this.rellax && $('[data-parallax]', this).length) {
-  //       this.rellax = new Rellax('[data-parallax]', {
-  //         speed: 2,
-  //         center: true,
-  //         percentage: 0.5
-  //       })
-  //     }
-  //   }
-  // }
-
-  // isMobile () {
-  //   return window.matchMedia('(max-width: 1199px)').matches
-  // }
 }
 
 window.customElements.define('flynt-block-image-text-parallax', BlockImageTextParallax, { extends: 'div' })
