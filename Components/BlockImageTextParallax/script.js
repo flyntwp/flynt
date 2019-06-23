e1a7830f20db2648af43dd361a56073bf8966e58
Nikola Jovanovic <nikola@bleech.de>
2019-06-23 09:30:19 +0200

    refactor(Block: Image Text Parallax): Optimize the performance of the script.

--- Components/BlockImageTextParallax/script.js
+++ Components/BlockImageTextParallax/script.js
@@ -1,29 +1,51 @@
 import Rellax from 'rellax'
 import $ from 'jquery'
 
-let rellax
+const $window = $(window)
+
+let rellax, initialized
 
 class BlockImageTextParallax extends window.HTMLDivElement {
   constructor (self) {
     self = super(self)
     self.$ = $(self)
-    self.resolveElements()
     return self
   }
 
-  resolveElements () {
-  }
-
   connectedCallback () {
-    if (!rellax) {
+    initParallax()
+  }
+}
+
+function initParallax () {
+  if (!initialized) {
+    initialized = true
+    setParallax()
+    $window.on('resize', setParallax)
+  }
+}
+
+function setParallax () {
+  if (isMobile()) {
+    if (rellax) {
+      rellax.destroy()
+      rellax = false
+    }
+  } else {
+    if (!rellax && !isMobile()) {
       rellax = new Rellax('[data-parallax]', {
         speed: 2,
         center: true,
         percentage: 0.5
       })
+    } else if (rellax) {
+      rellax.refresh()
     }
   }
+}
 
+function isMobile () {
+  return window.matchMedia('(max-width: 767px)').matches
 }
 
 window.customElements.define('flynt-block-image-text-parallax', BlockImageTextParallax, { extends: 'div' })
