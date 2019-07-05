521230bf6f91ace0597f44eaf3d071e5c802d874
Mykola Chernyshevskyi <mykola@bleech.de>
2019-07-05 16:50:53 +0200

    feat(BlockCountUp): new clean branch:)

---
+++ Components/BlockCountUp/admin.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  var waitForEl = function (selector, callback) {
+    if ($(selector).length) {
+      callback()
+    } else {
+      setTimeout(function () {
+        waitForEl(selector, callback)
+      }, 100)
+    }
+  }
+
+  if ($('[data-layout="BlockCountUp"]').closest('.values').length) {
+    waitForEl('i[data-feather]', function () {
+      $('i[data-feather]').closest('.select2-selection__rendered').removeAttr('title')
+      window.feather.replace()
+    })
+  }
+
+  $('body').on('click', '.select2-container', function () {
+    window.feather.replace()
+  })
+
+  $('body').on('change', '.select2-hidden-accessible', function () {
+    window.feather.replace()
+  })
+
+  $('body').on('keyup', '.select2-search__field', function () {
+    window.feather.replace()
+  })
+})
