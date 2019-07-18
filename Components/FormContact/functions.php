2540cfb6df3520af0187425c19875ddcd55cccea
Nikola Jovanovic <nikola@bleech.de>
2019-07-18 10:46:39 +0200

    refactor(Form: Contact): Remove the "tel" type for the input box. Add tranlatable options. Add wysiw

--- Components/FormContact/functions.php
+++ Components/FormContact/functions.php
@@ -3,6 +3,7 @@
 namespace Flynt\Components\FormContact;
 
 use Flynt\Api;
+use Flynt\Utils\Options;
 
 Api::registerFields('FormContact', [
     'layout' => [
@@ -32,10 +33,30 @@ Api::registerFields('FormContact', [
                 'delay' => 1,
                 'media_upload' => 0,
                 'toolbar' => 'full',
-                'required' => 1,
                 'wrapper' => [
                     'class' => 'autosize',
-                ],
+                ]
+            ],
+            [
+                'label' => 'Text Alignment',
+                'name' => 'textAlignmentFooter',
+                'type' => 'button_group',
+                'choices' => [
+                    'textLeft' => '<i class=\'dashicons dashicons-editor-alignleft\' title=\'Align text left\'></i>',
+                    'textCenter' => '<i class=\'dashicons dashicons-editor-aligncenter\' title=\'Align text center\'></i>',
+                    'textRight' => '<i class=\'dashicons dashicons-editor-alignright\' title=\'Align text right\'></i>'
+                ]
+            ],
+            [
+                'name' => 'footerHtml',
+                'label' => 'Footer Content',
+                'type' => 'wysiwyg',
+                'delay' => 1,
+                'media_upload' => 0,
+                'toolbar' => 'full',
+                'wrapper' => [
+                    'class' => 'autosize',
+                ]
             ],
             [
                 'label' => 'Options',
@@ -55,4 +76,56 @@ Api::registerFields('FormContact', [
             ]
         ]
     ]
+]);
+
+Options::addTranslatable('FormContact', [
+    [
+       'label' => 'First Name',
+       'name' => 'firstName',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'First Name'
+    ],
+    [
+       'label' => 'Last Name',
+       'name' => 'lastName',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Last Name'
+    ],
+    [
+       'label' => 'Email',
+       'name' => 'email',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Email'
+    ],
+    [
+       'label' => 'Mobile Phone',
+       'name' => 'mobilePhone',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Mobile Phone'
+    ],
+    [
+       'label' => 'Company',
+       'name' => 'company',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Company'
+    ],
+    [
+       'label' => 'Message',
+       'name' => 'message',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Message'
+    ],
+    [
+       'label' => 'ButtonText',
+       'name' => 'buttonText',
+       'type' => 'text',
+       'required' => 1,
+       'default_value' => 'Send Message'
+    ],
 ]);
