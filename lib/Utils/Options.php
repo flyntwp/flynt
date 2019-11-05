1eed9c214892b8fb3b854927d35d510b476934ce
Doğa Gürdal <doga@bleech.de>
2019-11-05 09:41:43 +0100

    feat(Options): add sub-pages for option categories

--- lib/Utils/Options.php
+++ lib/Utils/Options.php
@@ -21,20 +21,13 @@ class Options
 
     const OPTION_CATEGORIES = [
         'component' => [
-            'title' => 'Component',
-            'icon' => 'dashicons-editor-table',
-            'showType' => true
-        ],
-        'customPostType' => [
-            'title' => 'Custom Post Type',
-            'icon' => 'dashicons-palmtree',
-            'showType' => true
-            // 'label' => [ 'labels', 'menu_item' ], // TODO add this functionality
+            'title' => 'Components'
         ],
         'feature' => [
-            'title' => 'Feature',
-            'icon' => 'dashicons-carrot',
-            'showType' => true
+            'title' => 'Features'
+        ],
+        'archive' => [
+            'title' => 'Archives'
         ]
     ];
 
@@ -55,22 +48,44 @@ class Options
             $title = _x($option['title'], 'title', 'flynt');
             $slug = ucfirst($optionType);
 
-            acf_add_options_page([
-                'page_title'  => $title,
-                'menu_title'  => $title,
-                'redirect'    => true,
-                'menu_slug'   => $slug,
-                'icon_url'    => $option['icon']
-            ]);
+            $pageConfig = [
+                'page_title' => $title,
+                'menu_title' => $title,
+                'redirect' => true,
+                'menu_slug' => $slug,
+                'icon_url' => $option['icon']
+            ];
+
+            acf_add_options_page($pageConfig);
 
             static::$optionPages[$optionType] = [
                 'menu_slug' => $slug,
                 'menu_title' => $title
             ];
-            $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup(
-                [
-                    'name' => $slug,
-                    'title' => $title,
+
+            // TODO: check if we can hide empty category pages
+            foreach (static::OPTION_CATEGORIES as $optionCategory => $category) {
+                $categoryTitle = _x($category['title'], 'title', 'flynt');
+                $categorySlug = $slug . ucfirst($optionCategory);
+
+                $pageConfig = [
+                    'page_title' => $title . ': ' . $categoryTitle,
+                    'menu_title' => $categoryTitle,
+                    'redirect' => true,
+                    'menu_slug' => $categorySlug,
+                    'parent_slug' => $slug
+                ];
+                acf_add_options_page($pageConfig);
+
+                static::$optionPages[$optionType]['sub_pages'][
+                    $optionCategory
+                ] = [
+                    'menu_slug' => $categorySlug,
+                    'menu_title' => $categoryTitle
+                ];
+                $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup([
+                    'name' => $categorySlug,
+                    'title' => $categoryTitle,
                     'style' => 'seamless',
                     'fields' => [],
                     'location' => [
@@ -78,28 +93,42 @@ class Options
                             [
                                 'param' => 'options_page',
                                 'operator' => '==',
-                                'value' => $slug
+                                'value' => $categorySlug
                             ]
                         ]
                     ]
-                ]
-            );
-            acf_add_local_field_group($fieldGroup);
+                ]);
+                acf_add_local_field_group($fieldGroup);
+            }
         }
 
         add_action('current_screen', function ($currentScreen) {
+            $currentScreenId = strtolower($currentScreen->id);
             foreach (static::OPTION_TYPES as $optionType => $option) {
                 $isTranslatable = $option['translatable'];
-                $toplevelPageId = 'toplevel_page_' . $optionType;
+
+                // NOTE: because the first subpage starts with toplevel instead (there is no overview page)
+                $toplevelPageId = strtolower('toplevel_page_' . $optionType);
                 $menuTitle = static::$optionPages[$optionType]['menu_title'];
-                $subPageId = sanitize_title($menuTitle) . '_page_' . $optionType;
-                $currentScreenId = strtolower($currentScreen->id);
-                $isCurrentPage = StringHelpers::startsWith(strtolower($toplevelPageId), $currentScreenId)
-                || StringHelpers::startsWith(strtolower($subPageId), $currentScreenId);
+
+                // NOTE: all other subpages have the parent menu-title in front instead
+                $subPageId = strtolower(
+                    sanitize_title($menuTitle) . '_page_' . $optionType
+                );
+                $isCurrentPage =
+                    StringHelpers::startsWith(
+                        $toplevelPageId,
+                        $currentScreenId
+                    ) ||
+                    StringHelpers::startsWith($subPageId, $currentScreenId);
 
                 if (!$isTranslatable && $isCurrentPage) {
                     // set acf field values to default language
-                    add_filter('acf/settings/current_language', 'Flynt\Utils\Options::getDefaultAcfLanguage', 101);
+                    add_filter(
+                        'acf/settings/current_language',
+                        'Flynt\Utils\Options::getDefaultAcfLanguage',
+                        101
+                    );
 
                     // hide language selector in admin bar
                     add_action('wp_before_admin_bar_render', function () {
@@ -110,7 +139,6 @@ class Options
             }
         });
     }
-
 
     // ============
     // PUBLIC API
@@ -126,36 +154,53 @@ class Options
      * @since 0.2.2 added check for required hooks to have run to alert of timing issues when used incorrectly
      *
      * @param string $optionType Type of option page. Either globalOptions or translatableOptions.
-     * @param string $optionCategory Category of option page. One of these three values: component, feature, customPostType.
-     * @param string $subPageName Name of the sub page.
+     * @param string $optionCategory Category of option sub page. One of these values: component, feature, archive.
+     * @param string $groupName Name of the group on this page.
      * @param string $fieldName (optional) Name of the field to get.
      * @return mixed The value of the option or array of options. False if subpage doesn't exist or no option was found.
      **/
-    public static function get($optionType, $optionCategory, $subPageName, $fieldName = null)
-    {
-        if (!static::checkRequiredHooks($optionType, $optionCategory, $subPageName, $fieldName)) {
+    public static function get(
+        $optionType,
+        $optionCategory,
+        $groupName,
+        $fieldName = null
+    ) {
+        if (!static::checkRequiredHooks(
+            $optionType,
+            $optionCategory,
+            $groupName,
+            $fieldName
+        )
+        ) {
             return false;
         }
 
         // convert parameters
         $optionType = lcfirst($optionType);
         $optionCategory = ucfirst($optionCategory);
-        $subPageName = ucfirst($subPageName);
+        $groupName = ucfirst($groupName);
 
         if (!isset(static::OPTION_TYPES[$optionType])) {
             return false;
         }
 
-        $prefix = implode('', [$optionType, $optionCategory, $subPageName, '_']);
+        $prefix = implode('', [$optionType, $optionCategory, $groupName, '_']);
         $isTranslatable = static::OPTION_TYPES[$optionType]['translatable'];
         if (empty($fieldName)) {
-            $optionNames = (((static::$registeredOptions[$optionType] ?? [])[lcfirst($optionCategory)] ?? [])[$subPageName] ?? []);
+            $optionNames =
+                ((static::$registeredOptions[$optionType] ?? [])[
+                    lcfirst($optionCategory)
+                ] ?? [])[$groupName] ?? [];
             return array_combine(
                 $optionNames,
-                array_map(function ($optionName) use ($prefix, $isTranslatable) {
+                array_map(function ($optionName) use (
+                    $prefix,
+                    $isTranslatable
+                ) {
                     $fieldKey = $prefix . $optionName;
                     return static::getOptionField($fieldKey, $isTranslatable);
-                }, $optionNames)
+                },
+                $optionNames)
             );
         } else {
             $fieldKey = $prefix . $fieldName;
@@ -180,56 +225,79 @@ class Options
             global $flyntCurrentOptionCategory;
             $category = $flyntCurrentOptionCategory ?? 'component';
         }
-        $optionCategorySettings = static::OPTION_CATEGORIES[$category];
-        $iconClasses = 'flynt-submenu-item dashicons-before ' . $optionCategorySettings['icon'];
-        $prettyScope = StringHelpers::splitCamelCase($scope);
-        $fieldGroupTitle = "<span class='{$iconClasses}'>{$prettyScope}</span>";
-        $optionsPageSlug = self::$optionPages[$type]['menu_slug'];
+        $fieldGroupTitle = StringHelpers::splitCamelCase($scope);
+
+        $optionsPageSlug =
+            self::$optionPages[$type]['sub_pages'][$category]['menu_slug'];
         $fieldGroupName = $type . ucfirst($category) . $scope;
-        static::addOptionsFieldGroup($fieldGroupName, $fieldGroupTitle, $optionsPageSlug, $fields);
+        static::addOptionsFieldGroup(
+            $fieldGroupName,
+            $fieldGroupTitle,
+            $optionsPageSlug,
+            $fields
+        );
         static::registerOptionNames($type, $category, $scope, $fields);
     }
 
-    protected static function registerOptionNames($type, $category, $scope, $fields)
-    {
-        static::$registeredOptions[$type] = static::$registeredOptions[$type] ?? [];
-        static::$registeredOptions[$type][$category] = static::$registeredOptions[$type][$category] ?? [];
-        static::$registeredOptions[$type][$category][$scope] = array_column($fields, 'name');
+    protected static function registerOptionNames(
+        $type,
+        $category,
+        $scope,
+        $fields
+    ) {
+        static::$registeredOptions[$type] =
+            static::$registeredOptions[$type] ?? [];
+        static::$registeredOptions[$type][$category] =
+            static::$registeredOptions[$type][$category] ?? [];
+        static::$registeredOptions[$type][$category][$scope] = array_column(
+            $fields,
+            'name'
+        );
         return static::$registeredOptions;
     }
 
-    protected static function addOptionsFieldGroup($name, $title, $optionsPageSlug, $fields)
-    {
-        $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup(
-            [
-                'name' => $name,
-                'title' => $title,
-                'fields' => array_merge([
+    protected static function addOptionsFieldGroup(
+        $name,
+        $title,
+        $optionsPageSlug,
+        $fields
+    ) {
+        $fieldGroup = ACFComposer\ResolveConfig::forFieldGroup([
+            'name' => $name,
+            'title' => $title,
+            'fields' => array_merge(
+                [
                     [
                         'label' => $title,
                         'name' => '',
                         'type' => 'accordion',
                         'placement' => 'left',
-                        'endpoint' => false,
+                        'endpoint' => false
                     ]
-                ], $fields),
-                'style' => 'seamless',
-                'location' => [
+                ],
+                $fields
+            ),
+            'style' => 'seamless',
+            'location' => [
+                [
                     [
-                        [
-                            'param' => 'options_page',
-                            'operator' => '==',
-                            'value' => $optionsPageSlug
-                        ]
+                        'param' => 'options_page',
+                        'operator' => '==',
+                        'value' => $optionsPageSlug
                     ]
                 ]
             ]
+        ]);
+        $fieldGroup['fields'] = static::prefixFields(
+            $fieldGroup['fields'],
+            $name
         );
-        $fieldGroup['fields'] = static::prefixFields($fieldGroup['fields'], $name);
         foreach ($fieldGroup['fields'] as $field) {
-            acf_add_local_field(array_merge($field, [
-                'parent' => 'group_' . $optionsPageSlug,
-            ]));
+            acf_add_local_field(
+                array_merge($field, [
+                    'parent' => 'group_' . $optionsPageSlug
+                ])
+            );
         }
     }
 
@@ -241,12 +309,19 @@ class Options
         }, $fields);
     }
 
-    protected static function checkRequiredHooks($optionType, $optionCategory, $subPageName, $fieldName)
-    {
+    protected static function checkRequiredHooks(
+        $optionType,
+        $optionCategory,
+        $groupName,
+        $fieldName
+    ) {
         if (did_action('acf/init') < 1) {
-            $parameters = "${optionType}, ${optionCategory}, ${subPageName}, ";
+            $parameters = "${optionType}, ${optionCategory}, ${groupName}, ";
             $parameters .= isset($fieldName) ? $fieldName : 'NULL';
-            trigger_error("Could not get option/s for [${parameters}]. Required hooks have not yet been executed! Please make sure to run `Options::get()` after the `acf/init` action is finished.", E_USER_WARNING);
+            trigger_error(
+                "Could not get option/s for [${parameters}]. Required hooks have not yet been executed! Please make sure to run `Options::get()` after the `acf/init` action is finished.",
+                E_USER_WARNING
+            );
             return false;
         }
         return true;
@@ -258,11 +333,19 @@ class Options
             $option = get_field('field_' . $key, 'option');
         } else {
             // switch to default language to get global options
-            add_filter('acf/settings/current_language', 'Flynt\Utils\Options::getDefaultAcfLanguage', 100);
+            add_filter(
+                'acf/settings/current_language',
+                'Flynt\Utils\Options::getDefaultAcfLanguage',
+                100
+            );
 
             $option = get_field('field_' . $key, 'option');
 
-            remove_filter('acf/settings/current_language', 'Flynt\Utils\Options::getDefaultAcfLanguage', 100);
+            remove_filter(
+                'acf/settings/current_language',
+                'Flynt\Utils\Options::getDefaultAcfLanguage',
+                100
+            );
         }
 
         return $option;
