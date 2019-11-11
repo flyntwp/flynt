2d89806b6ece52bc25d853e119ed6bb71132c953
Steffen Bewersdorff <steffen@bleech.de>
2019-11-11 17:08:59 +0100

    docs(RemoveEditor): removed outdated documentation

--- inc/removeEditor.php
+++ inc/removeEditor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Removes `the_content` area (editor) from the Wordpress backend. Since Flynt uses ACF by default, this area is not needed. If you need to use this editor in your project, simply remove the `add_theme_support('flynt-remove-editor')` line from your `lib/Init.php` file.
+ * Removes `the_content` area (editor) from the Wordpress backend, since Flynt uses ACF.
  */
 namespace Flynt\RemoveEditor;
 
