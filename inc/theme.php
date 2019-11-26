283f072e8c0db57f37417adf86fa623329d68cb0
Dominik Tränklein <dominik@bleech.de>
2019-11-26 17:01:02 +0100

    fix(theme): disable big image size threshold

--- inc/theme.php
+++ inc/theme.php
@@ -4,3 +4,5 @@ add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
 });
+
+add_filter('big_image_size_threshold', '__return_false');
