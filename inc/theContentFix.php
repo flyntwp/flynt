92b4cfc326947c5964182e7b5bf42bde765ef56a
Dominik Tränklein <dominik@bleech.de>
2019-11-26 22:40:11 +0100

    fix(theContentFix): minor issues - guard rendering for empty post id - do not apply on all built in post types - replace shortcode if id changed

--- inc/theContentFix.php
+++ inc/theContentFix.php
@@ -1,13 +1,30 @@
 <?php
 
- use Timber\Timber;
- use Timber\Post;
+namespace Flynt\TheContentFix;
+
+use Timber\Timber;
+use Timber\Post;
+
+function isShortcodeAndDoesNotMatchId($postContent, $postId)
+{
+    preg_match('/^\[flyntTheContent id=\\\"(\d*)\\\"\]$/', $postContent, $matches);
+    if (!empty($matches) && $matches[1] != $postId) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 add_filter('wp_insert_post_data', function ($data, $postArr) {
-    if ($postArr['post_type'] === 'revision') {
+    if (in_array([
+        'revision',
+        'nav_menu_item',
+        'attachment',
+        'customize_changeset'
+    ], $postArr['post_type'])) {
         return $data;
     }
-    if (empty($data['post_content'])) {
+    if (empty($data['post_content']) || isShortcodeAndDoesNotMatchId($data['post_content'], $postArr['ID'])) {
         $data['post_content'] = "[flyntTheContent id=\"{$postArr['ID']}\"]";
     }
     return $data;
@@ -15,7 +32,9 @@ add_filter('wp_insert_post_data', function ($data, $postArr) {
 
 add_shortcode('flyntTheContent', function ($attrs) {
     $postId = $attrs['id'];
-    $context = Timber::get_context();
-    $context['post'] = $post = new Post($postId);
-    return Timber::fetch('templates/theContentFix.twig', $context);
+    if (!empty($postId)) {
+        $context = Timber::get_context();
+        $context['post'] = new Post($postId);
+        return Timber::fetch('templates/theContentFix.twig', $context);
+    }
 });
