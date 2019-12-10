61debe5ecc716ab7d3473ed7d9c4dbb815d38129
Mykola <mykola@bleech.de>
2019-12-10 11:41:00 +0100

    fix(Oembed): check if $iframeTagHtml exists
    this prevents PHP warnings if the function is called 
    although no video link was provided in the backend

--- lib/Utils/Oembed.php
+++ lib/Utils/Oembed.php
@@ -20,16 +20,18 @@ class Oembed
     public static function setSrcAsDataAttribute($iframeTagHtml, $additionalGetParams)
     {
         $output = '';
-        $Dom = new DOMDocument();
-        $Dom->loadHTML($iframeTagHtml);
-        $domNodes = $Dom->getElementsByTagName('iframe');
-        foreach ($domNodes as $node) {
-            $src = $node->getAttribute('src');
-            // add additional get parameters to existing oembed url
-            $src = add_query_arg($additionalGetParams, $src);
-            $node->removeAttribute('src');
-            $node->setAttribute('data-src', $src);
-            $output .= $Dom->saveHTML($node);
+        if ($iframeTagHtml) {
+            $Dom = new DOMDocument();
+            $Dom->loadHTML($iframeTagHtml);
+            $domNodes = $Dom->getElementsByTagName('iframe');
+            foreach ($domNodes as $node) {
+                $src = $node->getAttribute('src');
+                // add additional get parameters to existing oembed url
+                $src = add_query_arg($additionalGetParams, $src);
+                $node->removeAttribute('src');
+                $node->setAttribute('data-src', $src);
+                $output .= $Dom->saveHTML($node);
+            }
         }
         return $output;
     }
