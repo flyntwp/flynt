9f08d95b8f71a4d94be053189dbf3541e7fe3f48
VA79 <vanaf1979@gmail.com>
2020-02-25 15:52:19 +0100

    feat(HTML5): remove type for script and link tags
    Link and Script type attributes (#210)
    
    * Remove type attr's from link and script tags
    
    * Remove type attr's from link and script tags
    
    * Remove type attr's from link and script tags
    
    * Corrected code style
    
    * Adjusted to suggestions
    
    - Fixed coding style.
    - Removed backslash from function calls
    - Reverted back to original style_loader_tag action
    
    * feat(HTML5): only remove type attributes with add_theme_support
    
    (cherry picked from commit 507bd7a5dc89c9619471d96ab19932641521cbce)
    
    Co-authored-by: Dominik Tränklein <dominik@bleech.de>

--- inc/theme.php
+++ inc/theme.php
@@ -3,6 +3,11 @@
 add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+
+    /**
+     * Remove type attribute from link and script tags.
+     */
+    add_theme_support('html5', ['script', 'style']);
 });
 
 add_filter('big_image_size_threshold', '__return_false');
