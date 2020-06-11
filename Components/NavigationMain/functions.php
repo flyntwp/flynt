f8098e934fba71ec4589df02e7b577ac6d884c5c
Alex Heinrich <alex@bleech.de>
2020-06-11 11:52:47 +0200

    feat9NavigationMain): add aria label

--- Components/NavigationMain/functions.php
+++ Components/NavigationMain/functions.php
@@ -4,6 +4,7 @@ namespace Flynt\Components\NavigationMain;
 
 use Timber;
 use Flynt\Utils\Asset;
+use Flynt\Utils\Options;
 
 add_action('init', function () {
     register_nav_menus([
@@ -20,3 +21,20 @@ add_filter('Flynt/addComponentData?name=NavigationMain', function ($data) {
 
     return $data;
 });
+
+Options::addTranslatable('NavigationMain', [
+    [
+        'label' => 'Accessibility',
+        'name' => 'a11y',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => 'Aria Label for Menu',
+                'name' => 'labelMenu',
+                'type' => 'text',
+                'default_value' => 'Main',
+                'required' => 1,
+            ]
+        ],
+    ],
+]);
