d397ae6639a7fb03809ca542d3df25edbf8d464b
Dominik Tränklein <dominik@bleech.de>
2020-08-12 16:30:21 +0200

    feat(Customizer): improve colors code structure

--- inc/customizer-colors.php
+++ inc/customizer-colors.php
@@ -3,261 +3,180 @@
 /**
  * Add theme options to customizer
  */
+namespace Flynt\CustomizerColors;
 
 use Flynt\Utils\Asset;
+use WP_Customize_Color_Control;
 
-$colorsDefault = [
-    'accent-default' => [
-        'label'       => 'Accent',
-        'default'     => '#4045A1',
-        'description' => ''
-    ],
-    'headline-default' => [
-        'label'       => 'Headline',
-        'default'     => '#212121',
-        'description' => ''
-    ],
-    'text-default' => [
-        'label'       => 'Text',
-        'default'     => '#383838',
-        'description' => ''
-    ],
-    'border-default' => [
-        'label'       => 'Border',
-        'default'     => '#8E8E8E',
-        'description' => ''
-    ],
-    'background-default' => [
-        'label'       => 'Background',
-        'default'     => '#ffffff',
-        'description' => ''
-    ],
-];
+function getConfig()
+{
+    $colorsByTheme = [
+        'default' => [
+            'accent-default' => [
+                'label'       => 'Accent',
+                'default'     => '#4045A1',
+                'description' => ''
+            ],
+            'headline-default' => [
+                'label'       => 'Headline',
+                'default'     => '#212121',
+                'description' => ''
+            ],
+            'text-default' => [
+                'label'       => 'Text',
+                'default'     => '#383838',
+                'description' => ''
+            ],
+            'border-default' => [
+                'label'       => 'Border',
+                'default'     => '#8E8E8E',
+                'description' => ''
+            ],
+            'background-default' => [
+                'label'       => 'Background',
+                'default'     => '#ffffff',
+                'description' => ''
+            ],
+        ],
+        'light' => [
+            'accent-light' => [
+                'label'       => 'Accent',
+                'default'     => '#4045A1',
+                'description' => ''
+            ],
+            'headline-light' => [
+                'label'       => 'Headline',
+                'default'     => '#212121',
+                'description' => ''
+            ],
+            'text-light' => [
+                'label'       => 'Text',
+                'default'     => '#383838',
+                'description' => ''
+            ],
+            'border-light' => [
+                'label'       => 'Border',
+                'default'     => '#8E8E8E',
+                'description' => ''
+            ],
+            'background-light' => [
+                'label'       => 'Background',
+                'default'     => '#fafafa',
+                'description' => ''
+            ],
+        ],
+        'dark' => [
+            'accent-dark' => [
+                'label'       => 'Accent',
+                'default'     => '#ffffff',
+                'description' => '',
+            ],
+            'headline-dark' => [
+                'label'       => 'Headline',
+                'default'     => '#FBFBFB',
+                'description' => '',
+            ],
+            'text-dark' => [
+                'label'       => 'Text',
+                'default'     => '#E9E9EC',
+                'description' => '',
+            ],
+            'border-dark' => [
+                'label'       => 'Border',
+                'default'     => '#7E7E8E',
+                'description' => '',
+            ],
+            'background-dark' => [
+                'label'       => 'Background',
+                'default'     => '#2B2C46',
+                'description' => '',
+            ],
+        ],
+        'hero' => [
+            'accent-hero' => [
+                'label'       => 'Accent',
+                'default'     => '#ffffff',
+                'description' => '',
+            ],
+            'headline-hero' => [
+                'label'       => 'Headline',
+                'default'     => '#FBFCFC',
+                'description' => '',
+            ],
+            'text-hero' => [
+                'label'       => 'Text',
+                'default'     => '#D4DFE0',
+                'description' => '',
+            ],
+            'border-hero' => [
+                'label'       => 'Border',
+                'default'     => '#9CB6B5',
+                'description' => '',
+            ],
+            'background-hero' => [
+                'label'       => 'Background',
+                'default'     => '#28615E',
+                'description' => '',
+            ],
+        ],
+    ];
+    $sectionsByTheme = [
+        'theme_colors_default' => __('Default', 'flynt'),
+        'theme_colors_light' => __('Theme Light', 'flynt'),
+        'theme_colors_dark' => __('Theme Dark', 'flynt'),
+        'theme_colors_hero' => __('Theme Hero', 'flynt'),
+    ];
 
-$colorsLight = [
-    'accent-light' => [
-        'label'       => 'Accent',
-        'default'     => '#4045A1',
-        'description' => ''
-    ],
-    'headline-light' => [
-        'label'       => 'Headline',
-        'default'     => '#212121',
-        'description' => ''
-    ],
-    'text-light' => [
-        'label'       => 'Text',
-        'default'     => '#383838',
-        'description' => ''
-    ],
-    'border-light' => [
-        'label'       => 'Border',
-        'default'     => '#8E8E8E',
-        'description' => ''
-    ],
-    'background-light' => [
-        'label'       => 'Background',
-        'default'     => '#fafafa',
-        'description' => ''
-    ],
-];
+    return [
+        'colors' => $colorsByTheme,
+        'sections' => $sectionsByTheme,
+    ];
+}
 
-$colorsDark = [
-    'accent-dark' => [
-        'label'       => 'Accent',
-        'default'     => '#ffffff',
-        'description' => '',
-    ],
-    'headline-dark' => [
-        'label'       => 'Headline',
-        'default'     => '#FBFBFB',
-        'description' => '',
-    ],
-    'text-dark' => [
-        'label'       => 'Text',
-        'default'     => '#E9E9EC',
-        'description' => '',
-    ],
-    'border-dark' => [
-        'label'       => 'Border',
-        'default'     => '#7E7E8E',
-        'description' => '',
-    ],
-    'background-dark' => [
-        'label'       => 'Background',
-        'default'     => '#2B2C46',
-        'description' => '',
-    ],
-];
-
-$colorsHero = [
-    'accent-hero' => [
-        'label'       => 'Accent',
-        'default'     => '#ffffff',
-        'description' => '',
-    ],
-    'headline-hero' => [
-        'label'       => 'Headline',
-        'default'     => '#FBFCFC',
-        'description' => '',
-    ],
-    'text-hero' => [
-        'label'       => 'Text',
-        'default'     => '#D4DFE0',
-        'description' => '',
-    ],
-    'border-hero' => [
-        'label'       => 'Border',
-        'default'     => '#9CB6B5',
-        'description' => '',
-    ],
-    'background-hero' => [
-        'label'       => 'Background',
-        'default'     => '#28615E',
-        'description' => '',
-    ],
-];
 
 add_action('customize_register', function ($wp_customize) use ($colorsDefault, $colorsLight, $colorsDark, $colorsHero) {
+    $config = getConfig();
     $wp_customize->add_panel(
         'theme_colors_panel',
         [
             'title' => __('Colors', 'flynt'),
-            'description' => 'description', // Include html tags such as <p>.
+            // 'description' => 'description', // Include html tags such as <p>.
             'priority' => 160, // Mixed with top-level-section hierarchy.
-         ]
-    );
-
-    $wp_customize->add_section(
-        'theme_colors_default',
-        [
-            'title'      => __('Default', 'flynt'),
-            'priority'   => 20,
-            'panel' => 'theme_colors_panel'
         ]
     );
-
-    $wp_customize->add_section(
-        'theme_colors_light',
-        [
-            'title'      => __('Theme Light', 'flynt'),
-            'priority'   => 20,
-            'panel' => 'theme_colors_panel'
-        ]
-    );
-
-    $wp_customize->add_section(
-        'theme_colors_dark',
-        [
-            'title'      => __('Theme Dark', 'flynt'),
-            'priority'   => 20,
-            'panel' => 'theme_colors_panel'
-        ]
-    );
-
-    $wp_customize->add_section(
-        'theme_colors_hero',
-        [
-            'title'      => __('Theme Hero', 'flynt'),
-            'priority'   => 20,
-            'panel' => 'theme_colors_panel'
-        ]
-    );
-
-    foreach ($colorsDefault as $name => $color) {
-        // Settings
-        $wp_customize->add_setting(
-            'theme_colors_' . $name,
+    foreach (($config['sections'] ?? []) as $key => $title) {
+        $wp_customize->add_section(
+            $key,
             [
-                'default'   => $color['default'],
-                'transport' => 'postMessage',
+                'title'      => $title,
+                'priority'   => 20,
+                'panel' => 'theme_colors_panel'
             ]
-        );
-
-        // Controls
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                'theme_colors_' . $name,
-                [
-                    'section'     => 'theme_colors_default',
-                    'label'       => __($color['label']),
-                    'description' => __($color['description']),
-                ]
-            )
         );
     }
-
-    foreach ($colorsLight as $name => $color) {
-        // Settings
-        $wp_customize->add_setting(
-            'theme_colors_' . $name,
-            [
-                'default'   => $color['default'],
-                'transport' => 'postMessage',
-            ]
-        );
-
-        // Controls
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                'theme_colors_' . $name,
+    foreach (($config['colors'] ?? []) as $theme => $colors) {
+        foreach ($colors as $colorName => $colorConfig) {
+            // Settings
+            $wp_customize->add_setting(
+                'theme_colors_' . $colorName,
                 [
-                    'section'     => 'theme_colors_light',
-                    'label'       => __($color['label']),
-                    'description' => __($color['description']),
+                    'default'   => $colorConfig['default'],
+                    'transport' => 'postMessage',
                 ]
-            )
-        );
-    }
-
-    foreach ($colorsDark as $name => $color) {
-        // Settings
-        $wp_customize->add_setting(
-            'theme_colors_' . $name,
-            [
-                'default'   => $color['default'],
-                'transport' => 'postMessage',
-            ]
-        );
-
-        // Controls
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                'theme_colors_' . $name,
-                [
-                    'section'     => 'theme_colors_dark',
-                    'label'       => __($color['label']),
-                    'description' => __($color['description']),
-                ]
-            )
-        );
-    }
-
-    foreach ($colorsHero as $name => $color) {
-        // Settings
-        $wp_customize->add_setting(
-            'theme_colors_' . $name,
-            [
-                'default'   => $color['default'],
-                'transport' => 'postMessage',
-            ]
-        );
-
-        // Controls
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                'theme_colors_' . $name,
-                [
-                    'section'     => 'theme_colors_hero',
-                    'label'       => __($color['label']),
-                    'description' => __($color['description']),
-                ]
-            )
-        );
+            );
+            // Controls
+            $wp_customize->add_control(
+                new WP_Customize_Color_Control(
+                    $wp_customize,
+                    'theme_colors_' . $colorName,
+                    [
+                        'section'     => 'theme_colors_' . $theme,
+                        'label'       => __($colorConfig['label']),
+                        'description' => __($colorConfig['description']),
+                    ]
+                )
+            );
+        }
     }
 });
 
@@ -269,6 +188,29 @@ add_action('customize_preview_init', function () {
         '',
         true
     );
+});
+
+add_action('wp_head', function () {
+    $config = getConfig();
+
+    $alphaColorAmount = 0.4;
+    $darkenColorAmount = -0.1; // -0.1 darken value approximately equals scss darken 5%
+    ?>
+    <style type="text/css">
+        :root.html {
+            <?php foreach (($config['colors'] ?? []) as $theme => $colors) {
+                foreach ($colors as $colorName => $colorConfig) {
+                    $colorValue = get_theme_mod("theme_colors_{$colorName}", $colorConfig['default']);
+                    echo "--theme-color-{$colorName}: {$colorValue};";
+                }
+                $accentColorValue = get_theme_mod("theme_colors_accent-{$theme}", $colors["accent-{$theme}"]);
+                $rgbaValue = hex2rgba($accentColorValue, $alphaColorAmount);
+                $darkenedValue = colorBrightness($accentColorValue, $darkenColorAmount);
+                echo "--theme-color-accent-alpha-{$theme}: {$rgbaValue};";
+                echo "--theme-color-accent-hover-{$theme}: {$darkenedValue};";
+            } ?>
+    </style>
+    <?php
 });
 
 function hex2rgba($color, $opacity = false)
@@ -351,63 +293,3 @@ function colorBrightness($hex, $percent)
     }
     return $hash . $hex;
 }
-
-add_action('wp_head', function () use ($colorsDefault, $colorsLight, $colorsDark, $colorsHero) {
-    $hasChangedDefault = array_filter($colorsDefault, function ($color, $name) {
-        $value = get_theme_mod('theme_colors_' . $name, $color['default']);
-        return $value !== $color['default'];
-    }, ARRAY_FILTER_USE_BOTH);
-
-    $hasChangedLight = array_filter($colorsLight, function ($color, $name) {
-        $value = get_theme_mod('theme_colors_' . $name, $color['default']);
-        return $value !== $color['default'];
-    }, ARRAY_FILTER_USE_BOTH);
-
-    $hasChangedDark = array_filter($colorsDark, function ($color, $name) {
-        $value = get_theme_mod('theme_colors_' . $name, $color['default']);
-        return $value !== $color['default'];
-    }, ARRAY_FILTER_USE_BOTH);
-
-    $hasChangedHero = array_filter($colorsHero, function ($color, $name) {
-        $value = get_theme_mod('theme_colors_' . $name, $color['default']);
-        return $value !== $color['default'];
-    }, ARRAY_FILTER_USE_BOTH);
-
-    $alphaColorAmount = 0.4;
-    $darkenColorAmount = -0.1; // -0.1 darken value approximately equals scss darken 5%
-
-    if ($hasChangedDefault or $hasChangedLight or $hasChangedDark or $hasChangedHero) { ?>
-        <style type="text/css">
-            :root.html {
-               <?php if ($hasChangedDefault) {
-                    foreach ($colorsDefault as $name => $color) {
-                        echo (get_theme_mod('theme_colors_' . $name)) ? '--theme-color-' . $name . ': ' . get_theme_mod('theme_colors_' . $name) . ';' : null;
-                    };
-                    echo (get_theme_mod('theme_colors_accent-default')) ? '--theme-color-accent-alpha-default: ' . hex2rgba(get_theme_mod('theme_colors_accent-default'), $alphaColorAmount) . ';' : null;
-                    echo (get_theme_mod('theme_colors_accent-default')) ? '--theme-color-accent-hover-default: ' . colorBrightness(get_theme_mod('theme_colors_accent-default'), $darkenColorAmount) . ';' : null;
-               } ?>
-               <?php if ($hasChangedLight) {
-                    foreach ($colorsLight as $name => $color) {
-                        echo (get_theme_mod('theme_colors_' . $name)) ? '--theme-color-' . $name . ': ' . get_theme_mod('theme_colors_' . $name) . ';' : null;
-                    };
-                    echo (get_theme_mod('theme_colors_accent-light')) ? '--theme-color-accent-alpha-light: ' . hex2rgba(get_theme_mod('theme_colors_accent-light'), $alphaColorAmount) . ';' : null;
-                    echo (get_theme_mod('theme_colors_accent-light')) ? '--theme-color-accent-hover-light: ' . colorBrightness(get_theme_mod('theme_colors_accent-light'), $darkenColorAmount) . ';' : null;
-               } ?>
-                <?php if ($hasChangedDark) {
-                    foreach ($colorsDark as $name => $color) {
-                        echo (get_theme_mod('theme_colors_' . $name)) ? '--theme-color-' . $name . ': ' . get_theme_mod('theme_colors_' . $name) . ';' : null;
-                    };
-                    echo (get_theme_mod('theme_colors_accent-dark')) ? '--theme-color-accent-alpha-dark: ' . hex2rgba(get_theme_mod('theme_colors_accent-dark'), $alphaColorAmount) . ';' : null;
-                    echo (get_theme_mod('theme_colors_accent-dark')) ? '--theme-color-accent-hover-dark: ' . colorBrightness(get_theme_mod('theme_colors_accent-dark'), $darkenColorAmount) . ';' : null;
-                } ?>
-                <?php if ($hasChangedHero) {
-                    foreach ($colorsHero as $name => $color) {
-                        echo (get_theme_mod('theme_colors_' . $name)) ? '--theme-color-' . $name . ': ' . get_theme_mod('theme_colors_' . $name) . ';' : null;
-                    };
-                    echo (get_theme_mod('theme_colors_accent-hero')) ? '--theme-color-accent-alpha-hero: ' . hex2rgba(get_theme_mod('theme_colors_accent-hero'), $alphaColorAmount) . ';' : null;
-                    echo (get_theme_mod('theme_colors_accent-hero')) ? '--theme-color-accent-hover-hero: ' . colorBrightness(get_theme_mod('theme_colors_accent-hero'), $darkenColorAmount) . ';' : null;
-                } ?>
-            }
-        </style>
-    <?php }
-});
