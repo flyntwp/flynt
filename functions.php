b110197c0511fbf7a34c603fbcf4139bb2d18de0
Timo Hubois <info@pixelsaft.de>
2020-08-14 00:15:29 +0200

    fix(EnvironmenVariable): use wp 5.5 core function
    details: https://make.wordpress.org/core/2020/07/24/new-wp_get_environment_type-function-in-wordpress-5-5/

--- functions.php
+++ functions.php
@@ -6,10 +6,6 @@ use Flynt\Utils\FileLoader;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-if (!defined('WP_ENV')) {
-    define('WP_ENV', 'production');
-}
-
 // Check if the required plugins are installed and activated.
 // If they aren't, this function redirects the template rendering to use
 // plugin-inactive.php instead and shows a warning in the admin backend.
