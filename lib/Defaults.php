cf87b9f06f13507057215393dda49b0855b76c21
Dominik Tränklein <dominik@bleech.de>
2020-08-13 12:35:41 +0200

    revert(Defaults): remove unused renderComponent filter"
    This reverts commit ff3c454852c5fa8edf3f5088bd9d3300bfe4aeae.

--- lib/Defaults.php
+++ lib/Defaults.php
@@ -8,9 +8,21 @@ class Defaults
 {
     public static function init()
     {
+        add_filter('Flynt/renderComponent', ['Flynt\Defaults', 'renderComponent'], 999, 3);
         add_action('Flynt/registerComponent', ['Flynt\Defaults', 'loadFunctionsFile']);
     }
 
+    public static function renderComponent($output, $componentName, $componentData)
+    {
+        if (is_null($output)) {
+            $componentManager = ComponentManager::getInstance();
+            $filePath = $componentManager->getComponentFilePath($componentName);
+            $output = self::renderFile($componentData, $filePath);
+        }
+        return $output;
+    }
+
+  // this action needs to be removed by the user if they want to overwrite this functionality
     public static function loadFunctionsFile($componentName)
     {
         $componentManager = ComponentManager::getInstance();
@@ -18,5 +30,26 @@ class Defaults
         if (false !== $functionsFilePath) {
             require_once $functionsFilePath;
         }
+    }
+
+    protected static function renderFile($componentData, $filePath)
+    {
+        if (!is_file($filePath)) {
+            trigger_error("Template not found: {$filePath}", E_USER_WARNING);
+            return '';
+        }
+
+        $data = function () use ($componentData) {
+            $args = func_get_args();
+            array_unshift($args, $componentData);
+            return Helpers::extractNestedDataFromArray($args);
+        };
+
+        ob_start();
+        require $filePath;
+        $output = ob_get_contents();
+        ob_get_clean();
+
+        return $output;
     }
 }
