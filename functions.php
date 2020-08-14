33c21f4c8d4de9e01c124115ba459e4a05b12832
Timo Hubois <info@pixelsaft.de>
2020-08-14 10:37:46 +0200

    fix(EnvironmentVariables): wp 5.5 core compatibilty

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@ use Flynt\Utils\FileLoader;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+if (!defined('WP_ENV')) {	
+    define('WP_ENV', function_exists('wp_get_environment_type') ? wp_get_environment_type() : 'production');	
+} elseif (!defined('WP_ENVIRONMENT_TYPE')) {
+   define('WP_ENVIRONMENT_TYPE', WP_ENV);
+}
+
 // Check if the required plugins are installed and activated.
 // If they aren't, this function redirects the template rendering to use
 // plugin-inactive.php instead and shows a warning in the admin backend.
