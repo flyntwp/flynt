9040f66d067365c9007be2a3c8dee0d222d855d1
Timo Hubois <46505685+pixelsaft@users.noreply.github.com>
2020-08-31 14:29:48 +0200

    feat(theme): use wp_get_environment_type for WP_ENV (#298)
    * fix(EnvironmenVariable): use wp 5.5 core function
    
    details: https://make.wordpress.org/core/2020/07/24/new-wp_get_environment_type-function-in-wordpress-5-5/
    
    * revert(componentLogServer)
    
    * fix(EnvironmentVariables): wp 5.5 core compatibilty
    
    * fix(EnvironmentVariables): removed lint errors

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@ use Flynt\Utils\FileLoader;
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (!defined('WP_ENV')) {
-    define('WP_ENV', 'production');
+    define('WP_ENV', function_exists('wp_get_environment_type') ? wp_get_environment_type() : 'production');
+} elseif (!defined('WP_ENVIRONMENT_TYPE')) {
+    define('WP_ENVIRONMENT_TYPE', WP_ENV);
 }
 
 // Check if the required plugins are installed and activated.
