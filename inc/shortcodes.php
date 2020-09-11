e9929fd2ced1652341deedf5330de39e911eaaa8
Timo Hubois <info@pixelsaft.de>
2020-09-11 21:23:03 +0200

    feat(Shorcodes): added reference section

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -29,3 +29,51 @@ add_shortcode('tagline', function () {
     $tagline = get_bloginfo(false, 'description');
     return $tagline;
 });
+
+/**
+ * Flynt Shortcode reference
+ */
+function getShortcodeReference()
+{
+    return [
+        'label' => __('Shortcode Reference', 'flynt'),
+        'name' => 'groupShortcodes',
+        'instructions' => __('A Shortcode can generally be used inside text fields. It’s best practice to switch to text mode before inserting a shortcode inside the visual editor.', 'flynt'),
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => __('Site Title (Website Name)', 'flynt'),
+                'name' => 'messageShortcodeSiteTitle',
+                'type' => 'message',
+                'message' => '<pre>[sitetitle]</pre>',
+                'new_lines' => 'wpautop',
+                'esc_html' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Tagline (Subtitle)', 'flynt'),
+                'name' => 'messageShortcodeTagline',
+                'type' => 'message',
+                'message' => '<pre>[tagline]</pre>',
+                'new_lines' => 'wpautop',
+                'esc_html' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Current Year', 'flynt'),
+                'name' => 'messageShortcodeCurrentYear',
+                'type' => 'message',
+                'message' => '<pre>[year]</pre>',
+                'new_lines' => 'wpautop',
+                'esc_html' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+        ]
+    ];
+}
