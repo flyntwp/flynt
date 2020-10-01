3e060f4572a85a46d312ade79eeb402d5eccb6fe
Christina Koukouliata <christina@bleech.de>
2020-10-01 09:41:30 +0200

    refactor(ReusableComponent): change cpt to plural, update instructions

--- Components/ReusableComponent/functions.php
+++ Components/ReusableComponent/functions.php
@@ -19,7 +19,7 @@ function getACFLayout()
         'label' => 'Reusable <i class="dashicons dashicons-controls-repeat"></i>',
         'sub_fields' => [
             [
-                'label' => __('Select Component Set', 'flynt'),
+                'label' => __('Select Reusable Components', 'flynt'),
                 'name' => 'reusableId',
                 'type' => 'post_object',
                 'post_type' => [
@@ -42,8 +42,8 @@ add_filter('acf/prepare_field/name=reusableId', function ($field) {
     $postTitle = get_the_title($field['value']);
     $postId = $field['value'] ? $field['value'] : get_the_ID();
 
-    $instructions = sprintf(_x('Add or edit %sreusable components%s.<br>', '%s: start and end of <a> tag', 'flynt'), "<a href=\"${reusableAdminLink}\" target=\"_blank\" rel=\"noopener noreferrer\">", "</a>");
-    $editLink = sprintf(_x('Edit %s.', '%s: Link and title of selected reusable-post', 'flynt'), "<a class=\"reusable-postLink\" data-postId=\"${postId}\" href=\"${postEditLink}\" target=\"_blank\" rel=\"noopener noreferrer\">${postTitle}</a>");
+    $instructions = sprintf(_x('Add %sreusable components%s.', '%s: start and end of <a> tag', 'flynt'), "<a href=\"${reusableAdminLink}\" target=\"_blank\" rel=\"noopener noreferrer\">", "</a>");
+    $editLink = sprintf(_x(' Edit %s.', '%s: Link and title of selected reusable-post', 'flynt'), "<a class=\"reusable-postLink\" data-postId=\"${postId}\" href=\"${postEditLink}\" target=\"_blank\" rel=\"noopener noreferrer\">${postTitle}</a>");
 
     if ($field['value']) {
         $instructions .= $editLink;
