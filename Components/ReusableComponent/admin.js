eacdafab81f816f346f725dad35bf4d88d84e157
Christina Koukouliata <christina@bleech.de>
2020-10-01 13:10:04 +0200

    refactor(ReusableComponent): admin js add checks

--- Components/ReusableComponent/admin.js
+++ Components/ReusableComponent/admin.js
@@ -1,22 +1,29 @@
-/* globals acf */
-import $ from 'jquery'
+/* globals acf jQuery */
 
-acf.addAction('select2_init', function ($select, args, settings, field) {
-  const $fieldEl = field.$el
+(function ($) {
+  const select2init = function () {
+    acf.addAction('select2_init', function ($select, args, settings, field) {
+      const $fieldEl = field.$el
 
-  if ($fieldEl.data('name') === 'reusablePost') {
-    $select.on('change', { $fieldEl: $fieldEl }, function () {
-      const postId = $(this).val()
-      const postTitle = $(this).find('option:selected').text()
-      const $postLink = $fieldEl.find('.reusable-postLink')
-      const oldPostId = $postLink.attr('data-postId')
-      const $hiddenEl = $fieldEl.find('[hidden]')
-      const href = $postLink.attr('href')
+      if ($fieldEl.data('name') === 'reusablePost') {
+        $select.on('change', { $fieldEl: $fieldEl }, function () {
+          const postId = $(this).val()
+          const postTitle = $(this).find('option:selected').text()
+          const $postLink = $fieldEl.find('.reusable-postLink')
+          const oldPostId = $postLink.attr('data-postId')
+          const $hiddenEl = $fieldEl.find('[hidden]')
+          const href = $postLink.attr('href')
 
-      $hiddenEl.removeAttr('hidden')
-      $postLink.text(postTitle)
-      $postLink.attr('data-postId', postId)
-      $postLink.attr('href', href.replace(oldPostId, postId))
+          $hiddenEl.removeAttr('hidden')
+          $postLink.text(postTitle)
+          $postLink.attr('data-postId', postId)
+          $postLink.attr('href', href.replace(oldPostId, postId))
+        })
+      }
     })
   }
-})
+
+  if (typeof acf !== 'undefined') {
+    select2init()
+  }
+})(jQuery)
