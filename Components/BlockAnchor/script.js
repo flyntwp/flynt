cd77e4bbec13186bae47d61e2f792279f81651a7
Christina Koukouliata <christina@bleech.de>
2020-10-13 10:52:45 +0200

    refactor(BlockAnchor): smooth scrolling

--- Components/BlockAnchor/script.js
+++ Components/BlockAnchor/script.js
@@ -1,3 +1,4 @@
+/* globals location */
 import $ from 'jquery'
 import 'jquery.easing'
 
@@ -17,35 +18,60 @@ class BlockAnchor extends window.HTMLDivElement {
 
   resolveElements () {
     this.$window = $(window)
+    this.$document = $(document)
+    this.$body = $('body')
   }
 
   bindFunctions () {
-    this.scrollToAnchor = this.scrollToAnchor.bind(this)
-    this.blockJump = this.blockJump.bind(this)
+    this.checkScrollToLink = this.checkScrollToLink.bind(this)
+    this.checkScrollToHash = this.checkScrollToHash.bind(this)
   }
 
   bindEvents () {
-    $('body').on('click', 'a[href*="#"]', this.scrollToAnchor)
+    $('body').on('click', 'a[href*="#"]', this.checkScrollToLink)
   }
 
   connectedCallback () {
     if (window.location.hash) {
-      this.$window.on('load', this.scrollToAnchor)
+      this.$window.on('load', this.checkScrollToHash)
     }
   }
 
-  blockJump () {
-    return setTimeout(() => window.scrollTo(0, 0), 1)
+  checkScrollToLink (e) {
+    const $el = $(e.currentTarget)
+    const href = $(e.currentTarget).attr('href')
+    /* detect local link */
+    if (location.hostname === $el.prop('hostname') || !$el.prop('hostname').length) {
+      /* detect hash in link */
+      if (href.indexOf('#') !== -1) {
+        if (e) {
+          e.preventDefault()
+        }
+
+        this.smoothScrollTo(href.substr(href.indexOf('#')))
+      }
+    }
   }
 
-  scrollToAnchor (e) {
-    const hrefHash = $(e.target).attr('href') ? $(e.target).attr('href').split('#')[1] : ''
-    const $target = window.location.hash ? $(window.location.hash) : $(`#${hrefHash}`)
-    if ($target.length > 0) {
-      this.blockJump()
+  checkScrollToHash () {
+    const hash = window.location.hash
+    if (hash) {
+      setTimeout(() => {
+        this.smoothScrollTo(hash)
+      }, 500)
+    }
+  }
+
+  smoothScrollTo (hash) {
+    const $target = $(hash)
+    if ($target.length) {
+      let offsetTop = $target.offset().top
+      const maxScrollOffsetTop = this.$document.height() - this.$window.height()
+      offsetTop = ($target.offset().top + $target.outerHeight() / 2) - (this.$window.height() / 2)
+      offsetTop = offsetTop >= maxScrollOffsetTop ? maxScrollOffsetTop : offsetTop
       $('html, body').animate({
-        scrollTop: $target.offset().top
-      }, 500, 'easeOutQuad')
+        scrollTop: offsetTop
+      }, 1800, 'easeInOutQuart')
     }
   }
 }
