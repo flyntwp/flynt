d133b5dd8542c2287fa090b229a3b229633adf2e
Harun Bašić <harun@bleech.de>
2020-10-16 16:57:57 +0200

    feat(ColorHelpers): remove function colorBrightness

--- lib/Utils/ColorHelpers.php
+++ lib/Utils/ColorHelpers.php
@@ -110,45 +110,4 @@ class ColorHelpers
         $rgba = self::hexToRgba($color, $opacity);
         return self::rgbaToHsla($rgba, $opacity, $returnType);
     }
-
-    public static function colorBrightness($hex, $percent)
-    {
-        // Work out if hash given
-        $hash = '';
-        if (stristr($hex, '#')) {
-            $hex = str_replace('#', '', $hex);
-            $hash = '#';
-        }
-        // HEX TO RGB
-        $rgb = [hexdec(substr($hex, 0, 2)), hexdec(substr($hex, 2, 2)), hexdec(substr($hex, 4, 2))];
-        // CALCULATE
-        for ($i = 0; $i < 3; $i++) {
-            // See if brighter or darker
-            if ($percent > 0) {
-                // Lighter
-                $rgb[$i] = round($rgb[$i] * $percent) + round(255 * (1 - $percent));
-            } else {
-                // Darker
-                $positivePercent = $percent - ($percent * 2);
-                $rgb[$i] = round($rgb[$i] * (1 - $positivePercent)); // round($rgb[$i] * (1-$positivePercent));
-            }
-            // In case rounding up causes us to go to 256
-            if ($rgb[$i] > 255) {
-                $rgb[$i] = 255;
-            }
-        }
-        // RBG to Hex
-        $hex = '';
-        for ($i = 0; $i < 3; $i++) {
-            // Convert the decimal digit to hex
-            $hexDigit = dechex($rgb[$i]);
-            // Add a leading zero if necessary
-            if (strlen($hexDigit) == 1) {
-                $hexDigit = "0" . $hexDigit;
-            }
-            // Append to the hex string
-            $hex .= $hexDigit;
-        }
-        return $hash . $hex;
-    }
 }
