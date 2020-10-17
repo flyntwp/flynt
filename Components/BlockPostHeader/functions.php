a7dd1075a56bb5f970322a27ca8ae8bbff8d7a77
Harun Bašić <harun@bleech.de>
2020-10-17 20:41:04 +0200

    feat(BlockPostHeader): remove meta dividers from translatable options

--- Components/BlockPostHeader/functions.php
+++ Components/BlockPostHeader/functions.php
@@ -51,36 +51,4 @@ Options::addTranslatable('BlockPostHeader', [
             ],
         ],
     ],
-    [
-        'label' => __('Dividers', 'flynt'),
-        'name' => 'dividersTab',
-        'type' => 'tab',
-        'placement' => 'top',
-        'endpoint' => 0
-    ],
-    [
-        'label' => '',
-        'name' => 'dividers',
-        'type' => 'group',
-        'sub_fields' => [
-            [
-                'label' => __('Datetime | Author', 'flynt'),
-                'name' => 'authorDivider',
-                'type' => 'text',
-                'default_value' => '-',
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('Author | Reading Time', 'flynt'),
-                'name' => 'readingtimeDivider',
-                'type' => 'text',
-                'default_value' => '|',
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-        ],
-    ],
 ]);
