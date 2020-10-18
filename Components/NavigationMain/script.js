b6185a45f995198bb36c44122408ada5238a3c37
Harun Bašić <harun@bleech.de>
2020-10-18 16:15:05 +0200

    feat(NavigationMain): add headroom.js

---
+++ Components/NavigationMain/script.js
@@ -0,0 +1,63 @@
+import $ from 'jquery'
+import Headroom from 'headroom.js'
+import debounce from 'lodash/debounce'
+
+class NavigationMain extends window.HTMLElement {
+  constructor (...args) {
+    const self = super(...args)
+    self.init()
+    return self
+  }
+
+  init () {
+    this.$ = $(this)
+    this.bindFunctions()
+    this.bindEvents()
+    this.resolveElements()
+    this.headroom = null
+  }
+
+  bindFunctions () {
+    this.onWindowResize = this.onWindowResize.bind(this)
+  }
+
+  bindEvents () {
+    $(window).on('resize', debounce(this.onWindowResize, 200))
+  }
+
+  resolveElements () {}
+
+  connectedCallback () {
+    this.initHeadroom()
+  }
+
+  initHeadroom () {
+    this.headroom = new Headroom(this, {
+      offset: 80,
+      tolerance: {
+        up: 5,
+        down: 0
+      }
+    })
+
+    if (!this.isMobile()) {
+      this.headroom.init()
+    }
+  }
+
+  onWindowResize () {
+    if (!this.isMobile()) {
+      if (!this.headroom.initialised) this.headroom.init()
+    } else {
+      if (this.headroom.initialised) this.headroom.destroy()
+    }
+  }
+
+  isMobile () {
+    return window.matchMedia('(max-width: 1023px)').matches
+  }
+}
+
+window.customElements.define('flynt-navigation-main', NavigationMain, {
+  extends: 'nav'
+})
