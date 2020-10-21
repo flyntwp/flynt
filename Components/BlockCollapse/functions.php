77cb703f1ecc4762f5c268269ff8a2ba98a3685b
Christina Koukouliata <christina@bleech.de>
2020-10-21 14:20:05 +0200

    refactor(BlockCollapse): update options

--- Components/BlockCollapse/functions.php
+++ Components/BlockCollapse/functions.php
@@ -9,23 +9,15 @@ function getACFLayout()
         'label' => 'Block: Collapse',
         'sub_fields' => [
             [
-                [
-                    'label' => __('Collapse Component Spacing', 'flynt'),
-                    'name' => 'message',
-                    'type' => 'message',
-                    'message' => __('<b>Full</b> option to removes space between same colored themed components. <br><b>Small</b> option reduces space to 64px between components and 32px on mobile regardless of the theme.', 'flynt'),
-                    'new_lines' => 'wpautop',
-                ]
-            ],
-            [
-                'label' => __('Collapse Level', 'flynt'),
+                'label' => __('Collapse Component Spacing', 'flynt'),
                 'name' => 'collapseLevel',
                 'type' => 'button_group',
                 'choices' => [
+                    'none' => __('None', 'flynt'),
                     'small' => __('Small', 'flynt'),
-                    'full' => __('Full', 'flynt'),
+                    'medium' => __('Medium', 'flynt'),
                 ],
-                'default_value' => 'half',
+                'default_value' => 'none',
             ],
         ]
     ];
