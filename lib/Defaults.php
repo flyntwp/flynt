1bb7aadbd6f35a67222202317da1bf306eb90b93
Dominik Tränklein <dominik@bleech.de>
2020-10-30 10:45:49 +0100

    feat: remove deprecationed code

--- lib/Defaults.php
+++ lib/Defaults.php
@@ -8,18 +8,7 @@ class Defaults
 {
     public static function init()
     {
-        add_filter('Flynt/renderComponent', ['Flynt\Defaults', 'renderComponent'], 999, 3);
         add_action('Flynt/registerComponent', ['Flynt\Defaults', 'loadFunctionsFile']);
-    }
-
-    public static function renderComponent($output, $componentName, $componentData)
-    {
-        if (is_null($output)) {
-            $componentManager = ComponentManager::getInstance();
-            $filePath = $componentManager->getComponentFilePath($componentName);
-            $output = self::renderFile($componentData, $filePath);
-        }
-        return $output;
     }
 
     public static function loadFunctionsFile($componentName)
@@ -29,21 +18,5 @@ class Defaults
         if (false !== $functionsFilePath) {
             require_once $functionsFilePath;
         }
-    }
-
-    protected static function renderFile($componentData, $filePath)
-    {
-        _deprecated_function(__METHOD__, '1.4.0');
-        if (!is_file($filePath)) {
-            trigger_error("Template not found: {$filePath}", E_USER_WARNING);
-            return '';
-        }
-
-        ob_start();
-        require $filePath;
-        $output = ob_get_contents();
-        ob_get_clean();
-
-        return $output;
     }
 }
