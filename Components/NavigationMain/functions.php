70f7e75cde48241a9ff966d550226ba4782fad18
Timo Hubois <46505685+timohubois@users.noreply.github.com>
2021-08-26 10:48:24 +0200

    refactor(NavigationMain): use Timber\Menu sub-namespace instead of Timber (#354)
    * refactor(NavigationMain): use Timber\Menu sub-namespace instead of Timber
    
    * fix(NavigationMain): fixed a linting error

--- Components/NavigationMain/functions.php
+++ Components/NavigationMain/functions.php
@@ -2,8 +2,8 @@
 
 namespace Flynt\Components\NavigationMain;
 
-use Timber;
 use Flynt\Utils\Asset;
+use Timber\Menu;
 
 add_action('init', function () {
     register_nav_menus([
@@ -12,7 +12,7 @@ add_action('init', function () {
 });
 
 add_filter('Flynt/addComponentData?name=NavigationMain', function ($data) {
-    $data['menu'] = new Timber\Menu('navigation_main');
+    $data['menu'] = new Menu('navigation_main');
     $data['logo'] = [
         'src' => get_theme_mod('custom_header_logo') ? get_theme_mod('custom_header_logo') : Asset::requireUrl('Components/NavigationMain/Assets/logo.svg'),
         'alt' => get_bloginfo('name')
