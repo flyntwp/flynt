3efd13e30dd4f663f9ef6904e2a0e924f6a90f17
Timo Hubois <timo.hubois@bleech.de>
2022-06-26 20:32:36 +0200

    chore(release): 1.4.1

---
+++ lib/Utils/ArrayHelpers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Flynt\Utils;
+
+class ArrayHelpers
+{
+    /**
+     * Checks if an array is associative.
+     *
+     * @since 0.1.0
+     *
+     * @param array $array The array to check.
+     *
+     * @return boolean
+     */
+    public static function isAssoc(array $array)
+    {
+        _deprecated_function(__METHOD__, '1.4.0');
+        // Keys of the array
+        $keys = array_keys($array);
+
+        // If the array keys of the keys match the keys, then the array must
+        // not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+        return array_keys($keys) !== $keys;
+    }
+
+    /**
+     * Converts indexed values to associative keys.
+     *
+     * @since 0.1.0
+     *
+     * @param array $array The array to convert.
+     *
+     * @return array
+     */
+    public static function indexedValuesToAssocKeys(array $array)
+    {
+        _deprecated_function(__METHOD__, '1.4.1');
+        $values = array_map(function ($value) {
+            return is_array($value) ? $value : [];
+        }, $array);
+
+        $keys = array_map(function ($key) use ($array) {
+            return is_int($key) ? $array[$key] : $key;
+        }, array_keys($array));
+
+        return array_combine($keys, $values);
+    }
+}
