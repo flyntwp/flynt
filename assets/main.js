73a59079fcf66a37222f2c8e85f662f698bc3f8f
Timo Hubois <timo.hubois@bleech.de>
2022-07-09 22:26:48 +0200

    refactor(feather-icons): load feather-icons only if required, add stroke-width inside _base.scss

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,13 @@
 import './scripts/publicPath'
 import './scripts/loadCustomElements'
+import { initFeatherIcons, addFeatherIconToListCheckCircle } from './scripts/helpersFeatherIcons'
 import 'normalize.css/normalize.css'
 import './main.scss'
-import feather from 'feather-icons'
 import 'lazysizes'
 
-// feather-icons
 document.addEventListener('DOMContentLoaded', function () {
-  // Prepare .iconList--checkCircle
-  const iconListCheckCircleElements = document.querySelectorAll('.iconList--checkCircle')
-  iconListCheckCircleElements.forEach(function (element) {
-    const children = [...element.children]
-    children.forEach(function (child) {
-      child.insertAdjacentHTML('afterbegin', '<i data-feather=check-circle></i>')
-    })
-  })
-
-  // Set feather-icons stroke width
-  const borderWidth = window.getComputedStyle(document.documentElement).getPropertyValue('--border-width')
-    ? parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--border-width'))
-    : 1.5
-  feather.replace({
-    'stroke-width': borderWidth
-  })
+  addFeatherIconToListCheckCircle()
+  initFeatherIcons()
 })
 
 function importAll (r) {
