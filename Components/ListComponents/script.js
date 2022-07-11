d6793bcc6b7408321e4dedc93adf563ad8fa945f
Timo Hubois <timo.hubois@bleech.de>
2022-07-11 12:12:02 +0200

    refactor(ListComponents): replace jquery with vanilla js

--- Components/ListComponents/script.js
+++ Components/ListComponents/script.js
@@ -1,5 +1,3 @@
-import $ from 'jquery'
-
 class ListComponents extends window.HTMLDivElement {
   constructor (...args) {
     const self = super(...args)
@@ -8,16 +6,14 @@ class ListComponents extends window.HTMLDivElement {
   }
 
   init () {
-    this.$ = $(this)
     this.resolveElements()
     this.bindFunctions()
   }
 
   resolveElements () {
-    this.$window = $(window)
-    this.$document = $(document)
-    this.$componentimageWrappers = $('.component-imageWrapper', this)
-    this.$componentScreenshotImages = $('.component-imageWrapper img', this)
+    this.window = window
+    this.document = document
+    this.componentScreenshotImages = this.querySelectorAll('.component-imageWrapper img')
   }
 
   bindFunctions () {
@@ -27,70 +23,88 @@ class ListComponents extends window.HTMLDivElement {
   }
 
   connectedCallback () {
-    if (this.$window.width() >= 1280) {
-      this.$.on('mouseenter mouseleave', '.component-link', this.toggleHoverScroll)
+    if (!this.isDesktop()) {
+      // Add delegated events.
+      this.addEventListener('mouseenter', (e) => {
+        const { target } = e
+        if (target.matches('.component-link')) {
+          this.toggleHoverScroll(e)
+        }
+      }, true)
+
+      this.addEventListener('mouseleave', (e) => {
+        const { target } = e
+        if (target.matches('.component-link')) {
+          this.toggleHoverScroll(e)
+        }
+      }, true)
     } else {
-      this.$window.on('resize', this.setParallaxConfig)
-      this.$document.on('lazyloaded', this.setParallaxConfig)
-      this.$window.on('scroll', this.startParallaxScroll)
+      this.window.addEventListener('resize', this.setParallaxConfig)
+      this.document.addEventListener('lazyloaded', this.setParallaxConfig)
+      this.window.addEventListener('scroll', this.startParallaxScroll, { passive: true })
     }
   }
 
   toggleHoverScroll (e) {
-    const $imageWrapper = $(e.currentTarget).find('.component-imageWrapper')
-    const $imageWrapperHeight = $imageWrapper.outerHeight()
-    const $image = $imageWrapper.find('img')
-    const $imageHeight = $image.height()
+    const imageWrapper = e.target.querySelector('.component-imageWrapper')
+    const imageWrapperHeight = imageWrapper.offsetHeight
+    const image = imageWrapper.querySelector('img')
+    const imageHeight = image.offsetHeight
 
-    if ($imageHeight > $imageWrapperHeight) {
+    if (imageHeight > imageWrapperHeight) {
       if (e.type === 'mouseenter') {
-        $image.css('transition', 'transform ' + Math.max((($imageHeight - $imageWrapperHeight) / $imageWrapperHeight), 0.3) + 's cubic-bezier(0.215, 0.61, 0.355, 1)')
-        $image.css('transform', 'translateY(-' + ($imageHeight - $imageWrapperHeight) + 'px)')
+        image.style.transition = 'transform ' + Math.max(((imageHeight - imageWrapperHeight) / imageWrapperHeight), 0.3) + 's cubic-bezier(0.215, 0.61, 0.355, 1)'
+        image.style.transform = 'translateY(-' + (imageHeight - imageWrapperHeight) + 'px)'
       } else {
-        $image.css('transition', 'transform ' + Math.max((($imageHeight - $imageWrapperHeight) / $imageWrapperHeight), 0.3) + 's cubic-bezier(0.23, 1, 0.32, 1)')
-        $image.css('transform', 'translateY(0)')
+        image.style.transition = 'transform ' + Math.max(((imageHeight - imageWrapperHeight) / imageWrapperHeight), 0.3) + 's cubic-bezier(0.23, 1, 0.32, 1)'
+        image.style.transform = 'translateY(0)'
       }
     }
   }
 
-  setParallaxConfig () {
-    const windowHeight = this.$window.height()
+  setParallaxConfig (e) {
+    const windowHeight = this.window.innerHeight
+    console.log(e.type)
 
-    this.$componentScreenshotImages.each((index, el) => {
-      const $image = $(el)
-      const $imageWrapper = $image.parent()
+    this.componentScreenshotImages.forEach((image) => {
+      const imageWrapper = image.parentElement
 
-      const imageOverflow = $image.height() - $imageWrapper.outerHeight()
-      const topOffset = $imageWrapper.offset().top
+      const imageOverflow = image.offsetHeight - imageWrapper.offsetHeight
+      const topOffset = imageWrapper.getBoundingClientRect().top
       const startOffset = topOffset - windowHeight * 0.4
       const endOffset = topOffset - 100
 
-      $image.data('parallaxConfig', {
-        imageOverflow: imageOverflow,
-        startOffset: startOffset,
-        endOffset: endOffset
-      })
+      if (!image.dataset.parallaxConfig) {
+        image.dataset.parallaxConfig = JSON.stringify({
+          imageOverflow: imageOverflow,
+          startOffset: startOffset,
+          endOffset: endOffset
+        })
+      }
     })
 
     this.startParallaxScroll()
   }
 
   startParallaxScroll (e) {
-    const scrollTop = this.$window.scrollTop()
+    const scrollTop = window.scrollY
 
-    this.$componentScreenshotImages.each((index, el) => {
-      const $image = $(el)
-      const parallaxConfig = $image.data('parallaxConfig')
+    this.componentScreenshotImages.forEach((image) => {
+      const parallaxConfig = JSON.parse(image.dataset.parallaxConfig)
 
       if (parallaxConfig && parallaxConfig.imageOverflow > 0) {
         let scrollPercentage = (scrollTop - parallaxConfig.startOffset) / (parallaxConfig.endOffset - parallaxConfig.startOffset)
         scrollPercentage = Math.min(Math.max(scrollPercentage, 0), 1)
         const move = (scrollPercentage * parallaxConfig.imageOverflow) * -1
         if (!e || (scrollTop >= parallaxConfig.startOffset && scrollTop < parallaxConfig.endOffset)) {
-          $image.css('transform', 'translateY(' + move + 'px)')
+          image.style.transform = 'translateY(' + move + 'px)'
         }
       }
     })
+  }
+
+  isDesktop () {
+    return window.matchMedia('(max-width: 1280px)').matches
   }
 }
 
