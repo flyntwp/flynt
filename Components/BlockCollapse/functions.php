1b6b0310f2787028a7e5c997d79ae2bbd526fc3b
Timo Hubois <timo.hubois@bleech.de>
2022-07-27 13:58:11 +0200

    feat(BlockCollapse): add range slider and examples

--- Components/BlockCollapse/functions.php
+++ Components/BlockCollapse/functions.php
@@ -2,6 +2,11 @@
 
 namespace Flynt\Components\BlockCollapse;
 
+add_filter('Flynt/addComponentData?name=BlockCollapse', function ($data) {
+    $data['status'] = $data['percentageDistance'] >= 100 ? 'expand' : 'collapse';
+    return $data;
+});
+
 function getACFLayout()
 {
     return [
@@ -9,15 +14,34 @@ function getACFLayout()
         'label' => 'Block: Collapse',
         'sub_fields' => [
             [
-                'label' => __('Collapse Spacing', 'flynt'),
-                'name' => 'collapseSpacing',
-                'type' => 'button_group',
-                'choices' => [
-                    'noSpacing' => __('No Spacing', 'flynt'),
-                    'preContent' => __('Pre Content', 'flynt'),
-                    'paragraph' => __('Paragraph', 'flynt'),
+                'label' => __('Vertical space', 'flynt'),
+                'instructions' => __('Distance adjusts the vertical space between two components.', 'flynt'),
+                'name' => 'percentageDistance',
+                'type' => 'range',
+                'prepend' => __('Distance', 'flynt'),
+                'append' => __('%', 'flynt'),
+                'default_value' => 0,
+                'min' => 0,
+                'max' => 200,
+                'wrapper' =>  [
+                    'width' => '50',
                 ],
-                'default_value' => 'none',
+            ],
+            [
+                'label' => __('Examples', 'flynt'),
+                'name' => '',
+                'type' => 'message',
+                'message' => sprintf(
+                    '%1$s' . PHP_EOL . '%2$s' . PHP_EOL . '%3$s',
+                    __('0% (Default) no spacing between components', 'flynt'),
+                    __('50% reduces vertical space (by half)', 'flynt'),
+                    __('150% extends vertical space (by 50%)', 'flynt')
+                ),
+                'new_lines' => 'wpautop',
+                'esc_html' => 1,
+                'wrapper' =>  [
+                    'width' => '50',
+                ],
             ],
         ]
     ];
