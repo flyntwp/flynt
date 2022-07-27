e29f3f489715db7f87eca2745e701a6a2bf42d45
Timo Hubois <timo.hubois@bleech.de>
2022-07-27 13:58:46 +0200

    fix(BlockPostHeader): remove typo

--- Components/BlockPostHeader/functions.php
+++ Components/BlockPostHeader/functions.php
@@ -42,7 +42,7 @@ Options::addTranslatable('BlockPostHeader', [
             ],
             [
                 'label' => __('Reading Time - (20) min read', 'flynt'),
-                'instructions' => __('%d is placehoder for number of minutes', 'flynt'),
+                'instructions' => __('%d is placeholder for number of minutes', 'flynt'),
                 'name' => 'readingTime',
                 'type' => 'text',
                 'default_value' => '%d min read',
