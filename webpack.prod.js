3f9227129b933480cca0f049132a0bdb8c50b818
Timo Hubois <timo.hubois@bleech.de>
2022-08-05 00:40:47 +0200

    refactor(webpack.prod.js): remove unused code

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
-// const { host, domain, watchFiles } = require('./build-config.js')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
