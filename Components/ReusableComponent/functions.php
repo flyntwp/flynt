5561a5bd99029e7c7c4752820598cd160c8d47ed
Timo Hubois <timo.hubois@bleech.de>
2022-08-18 22:35:06 +0200

    refactor(ReusableComponent): make label translateable, change singular/plural

--- Components/ReusableComponent/functions.php
+++ Components/ReusableComponent/functions.php
@@ -6,10 +6,10 @@ function getACFLayout()
 {
     return [
         'name' => 'ReusableComponent',
-        'label' => 'Reusable <i class="dashicons dashicons-controls-repeat"></i>',
+        'label' => sprintf('%1$s <i class="dashicons dashicons-controls-repeat"></i>', __('Reusable', 'flynt')),
         'sub_fields' => [
             [
-                'label' => __('Select Reusable Components', 'flynt'),
+                'label' => __('Select Reusable Component', 'flynt'),
                 'name' => 'reusablePost',
                 'type' => 'post_object',
                 'post_type' => [
@@ -27,12 +27,12 @@ function getACFLayout()
 
 add_filter('acf/prepare_field/name=reusablePost', function ($field) {
     // Set initial instructions and update with selected post link
-    $reusableAdminLink = admin_url('edit.php?post_type=reusable-component');
+    $reusableAdminLink = admin_url('edit.php?post_type=reusable-components');
     $postEditLink = get_edit_post_link($field['value']);
     $postTitle = get_the_title($field['value']);
     $postId = $field['value'] ? $field['value'] : get_the_ID();
 
-    $instructions = sprintf(_x('Add %sreusable components%s.', '%s: start and end of <a> tag', 'flynt'), "<a href=\"${reusableAdminLink}\" target=\"_blank\" rel=\"noopener noreferrer\">", "</a>");
+    $instructions = sprintf(_x('Add %sreusable component%s.', '%s: start and end of <a> tag', 'flynt'), "<a href=\"${reusableAdminLink}\" target=\"_blank\" rel=\"noopener noreferrer\">", "</a>");
     $editLink = sprintf(_x(' Edit %s.', '%s: Link and title of selected reusable-post', 'flynt'), "<a class=\"reusable-postLink\" data-postId=\"${postId}\" href=\"${postEditLink}\" target=\"_blank\" rel=\"noopener noreferrer\">${postTitle}</a>");
 
     if ($field['value']) {
