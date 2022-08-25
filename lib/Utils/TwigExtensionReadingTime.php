7d9030aac302ea5a0e3fa3ed4ca34030703a923f
Timo Hubois <timo.hubois@bleech.de>
2022-08-25 22:33:08 +0200

    refactor(TwigExtensionReadingTime): remove deprecated function, use consistent naming of functions

--- lib/Utils/TwigExtensionReadingTime.php
+++ lib/Utils/TwigExtensionReadingTime.php
@@ -7,24 +7,19 @@ use Twig\TwigFilter;
 
 class TwigExtensionReadingTime extends AbstractExtension
 {
-    public function getName()
-    {
-        return 'ReadingTime';
-    }
-
     public function getFilters()
     {
         return [
-            new TwigFilter('readingtime', [$this, 'readingtimeFilter'])
+            new TwigFilter('readingtime', [$this, 'renderMinutesToRead']),
         ];
     }
 
-    public function readingtimeFilter($content)
+    public function renderMinutesToRead($content)
     {
         $wordsPerMinute = 200;
         $words = str_word_count(strip_tags($content));
         $minutesToRead = floor($words / $wordsPerMinute);
-        $min = ($minutesToRead < 1 ? '1' : $minutesToRead);
+        $min = $minutesToRead < 1 ? '1' : $minutesToRead;
 
         return $min;
     }
