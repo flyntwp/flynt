9c0419f216933e1ffb9f869fd3766bcccc121b17
Timo Hubois <t.hubois@pixelsaft.de>
2022-10-05 09:53:14 +0200

    feat(timberLoader): convert ACF Field of type taxonomy to a Timber\Term

--- inc/timberLoader.php
+++ inc/timberLoader.php
@@ -4,6 +4,7 @@
  * - Enables rendering with Timber and Twig.
  * - Converts ACF Images to Timber Images if ACF is enabled.
  * - Convert ACF Field of type post_object to a Timber\Post and add all ACF Fields of that Post
+ * - Convert ACF Field of type taxonomy to a Timber\Term
  */
 
 namespace Flynt\TimberLoader;
@@ -23,6 +24,9 @@ add_filter('acf/format_value/type=post_object', NS . 'formatPostObject', 100);
 
 // Convert ACF Field of type relationship to a Timber\Post and add all ACF Fields of that Post
 add_filter('acf/format_value/type=relationship', NS . 'formatPostObject', 100);
+
+// Convert ACF Field of type taxonomy to a Timber\Term
+add_filter('acf/format_value/type=taxonomy', NS . 'formatTaxonomy', 100);
 
 function formatImage($value)
 {
@@ -56,6 +60,24 @@ function convertToTimberPost($value)
 {
     if (!empty($value) && is_object($value) && get_class($value) === 'WP_Post') {
         $value = Timber::get_Post($value);
+    }
+    return $value;
+}
+
+function formatTaxonomy($value)
+{
+    if (is_array($value)) {
+        $value = array_map(NS . 'convertToTimberTaxonomy', $value);
+    } else {
+        $value = Timber::get_term($value);
+    }
+    return $value;
+}
+
+function convertToTimberTaxonomy($value)
+{
+    if (!empty($value) && is_object($value) && get_class($value) === 'WP_Term') {
+        $value = Timber::get_term($value->term_id);
     }
     return $value;
 }
