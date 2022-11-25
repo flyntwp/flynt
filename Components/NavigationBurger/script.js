2b1c306a55deb2193a1c0512ab9a9771369aeb60
Dominik Tränklein <dominik@bleech.de>
2022-11-25 14:19:07 +0100

    feat(NavigationBurger): remove custom element

--- Components/NavigationBurger/script.js
+++ Components/NavigationBurger/script.js
@@ -1,85 +1,55 @@
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import Headroom from 'headroom.js'
-import debounce from 'lodash/debounce'
+import delegate from 'delegate-event-listener'
+import { buildRefs } from '@/assets/scripts/helpers.js'
 
-class NavigationBurger extends window.HTMLElement {
-  constructor (...args) {
-    const self = super(...args)
-    self.init()
-    return self
-  }
+export default function (el) {
+  let headroom, isMenuOpen
 
-  init () {
-    this.resolveElements()
-    this.bindFunctions()
-    this.bindEvents()
-    this.headroom = null
-    this.isMenuOpen = false
-  }
+  const refs = buildRefs(el)
 
-  bindFunctions () {
-    this.triggerMenu = this.triggerMenu.bind(this)
-    this.onWindowResize = this.onWindowResize.bind(this)
-  }
+  const isDesktopMediaQuery = window.matchMedia('(min-width: 1024px)')
+  isDesktopMediaQuery.addEventListener('change', onBreakpointChange)
 
-  bindEvents () {
-    this.menuButton.addEventListener('click', this.triggerMenu)
-    this.window.addEventListener('resize', debounce(this.onWindowResize, 200))
-  }
+  el.addEventListener('click', delegate('[data-ref="menuButton"]', onMenuButtonClick))
 
-  resolveElements () {
-    this.window = window
-    this.menu = this.querySelector('.menu')
-    this.menuButton = this.querySelector('.hamburger')
-    this.navigationHeight = parseInt(window.getComputedStyle(this).getPropertyValue('--navigation-height')) || 0
-  }
+  onBreakpointChange()
 
-  connectedCallback () {
-    this.initHeadroom()
-  }
+  function onMenuButtonClick (e) {
+    isMenuOpen = !isMenuOpen
+    refs.menuButton.setAttribute('aria-expanded', isMenuOpen)
 
-  triggerMenu (e) {
-    this.isMenuOpen = !this.isMenuOpen
-    this.menuButton.setAttribute('aria-expanded', this.isMenuOpen)
-
-    if (this.isMenuOpen) {
-      this.setAttribute('data-status', 'menuIsOpen')
-      disableBodyScroll(this.menu)
+    if (isMenuOpen) {
+      el.setAttribute('data-status', 'menuIsOpen')
+      disableBodyScroll(refs.menu)
     } else {
-      this.removeAttribute('data-status')
-      enableBodyScroll(this.menu)
+      el.removeAttribute('data-status')
+      enableBodyScroll(refs.menu)
     }
   }
 
-  initHeadroom () {
-    const navigationHeight = parseInt(window.getComputedStyle(this).getPropertyValue('--navigation-height')) || 0
+  function initHeadroom () {
+    if (headroom) {
+      return
+    }
+    const navigationHeight = parseInt(window.getComputedStyle(el).getPropertyValue('--navigation-height')) || 0
 
-    this.headroom = new Headroom(this, {
+    headroom = new Headroom(el, {
       offset: navigationHeight,
       tolerance: {
         up: 5,
         down: 0
       }
     })
-
-    if (this.isMobile()) {
-      this.headroom.init()
-    }
   }
 
-  onWindowResize () {
-    if (this.isMobile()) {
-      if (!this.headroom.initialised) this.headroom.init()
+
+  function onBreakpointChange() {
+    if (isDesktopMediaQuery.matches) {
+      if (headroom?.initialised) headroom.destroy()
     } else {
-      if (this.headroom.initialised) this.headroom.destroy()
+      initHeadroom()
+      if (!headroom.initialised) headroom.init()
     }
-  }
-
-  isMobile () {
-    return window.matchMedia('(max-width: 1023px)').matches
   }
 }
-
-window.customElements.define('flynt-navigation-burger', NavigationBurger, {
-  extends: 'nav'
-})
