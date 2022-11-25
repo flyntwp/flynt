792375f286d09608fe6215a07937f1169ec33cc6
Dominik Tränklein <dominik@bleech.de>
2022-11-25 16:46:48 +0100

    feat(BlockAnchor): remove custom element

--- Components/BlockAnchor/script.js
+++ Components/BlockAnchor/script.js
@@ -1,68 +1,30 @@
 /* globals location */
+import delegate from 'delegate-event-listener'
 
-class BlockAnchor extends window.HTMLDivElement {
-  constructor (...args) {
-    const self = super(...args)
-    self.init()
-    return self
-  }
+window.document.body.addEventListener('click', delegate('a[href*="#"]', onLinkClick))
 
-  init () {
-    this.resolveElements()
-    this.bindFunctions()
-    this.bindEvents()
-  }
+if (location.hash) {
+  smoothScrollTo(location.hash)
+}
 
-  resolveElements () {
-    this.window = window
-    this.document = document
-    this.body = document.querySelector('body')
-  }
-
-  bindFunctions () {
-    this.checkScrollToHash = this.checkScrollToHash.bind(this)
-  }
-
-  bindEvents () {
-    // Delegated click event.
-    this.body.addEventListener('click', (e) => {
-      const { target } = e
-      const href = target.getAttribute('href')
-
-      /* detect local link */
-      if (href && (location.hostname === target.hostname || !target.hostname)) {
-        /* detect hash in link */
-        if (href.indexOf('#') !== -1) {
-          const hash = href.substr(href.indexOf('#'))
-          e.preventDefault()
-          this.smoothScrollTo(hash)
-        }
-      }
-    })
-  }
-
-  connectedCallback () {
-    if (this.window.location.hash && !(this.body.classList.contains('scrolledToHash'))) {
-      this.body.classList.add('scrolledToHash')
-      this.document.addEventListener('DOMContentLoaded', this.checkScrollToHash, { passive: true })
-    }
-  }
-
-  checkScrollToHash () {
-    const hash = this.window.location.hash
-    if (hash) {
-      setTimeout(() => {
-        this.smoothScrollTo(hash)
-      }, 750)
-    }
-  }
-
-  smoothScrollTo (hash) {
-    const target = this.document.querySelector(hash)
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' })
-    }
+function onLinkClick(e) {
+  const delegate = e.delegateTarget
+  const currentURL = new URL(location)
+  const linkURL = new URL(delegate.href)
+  if (hrefWithoutHash(currentURL) === hrefWithoutHash(linkURL)) {
+    e.preventDefault()
+    smoothScrollTo(linkURL.hash)
+    history.pushState({}, '', linkURL.href)
   }
 }
 
-window.customElements.define('flynt-block-anchor', BlockAnchor, { extends: 'div' })
+function hrefWithoutHash(url) {
+  return url.origin + url.pathname + url.search
+}
+
+function smoothScrollTo (selector) {
+  const target = document.querySelector(selector)
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' })
+  }
+}
