de7f0bf8491c99155b6c00b2943e38ce42222bd1
Dominik Tränklein <dominik@bleech.de>
2022-12-01 17:32:05 +0100

    feat(BlockCollapse): remove custom element

--- Components/BlockCollapse/script.js
+++ Components/BlockCollapse/script.js
@@ -1,10 +1,7 @@
-class BlockCollapse extends window.HTMLDivElement {
-  connectedCallback () {
-    const previousElementTheme = this.parentElement.previousElementSibling.getAttribute('data-theme')
-    if (previousElementTheme) {
-      this.setAttribute('data-theme', previousElementTheme)
-    }
+export default function(el) {
+  console.log(el.previousElementSibling)
+  const previousElementTheme = el.previousElementSibling.getAttribute('data-theme')
+  if (previousElementTheme) {
+    el.setAttribute('data-theme', previousElementTheme)
   }
 }
-
-window.customElements.define('flynt-block-collapse', BlockCollapse, { extends: 'div' })
