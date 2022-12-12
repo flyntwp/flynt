a312ca0a6fb3d2da81227cef7f60b15c23799886
Dominik Tränklein <dominik@bleech.de>
2022-12-12 13:55:49 +0100

    feat(lazysizes): remove native loading and preload script

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,12 @@
 import 'vite/modulepreload-polyfill'
 import './scripts/loadCustomElements'
-import { prepareAboveTheFoldLazyLoadedElements } from './scripts/prepareAboveTheFold'
 import FlyntComponent from './scripts/FlyntComponent'
 
-import lazySizes from 'lazysizes'
-import 'lazysizes/plugins/native-loading/ls.native-loading'
+import 'lazysizes'
 
 if (import.meta.env.DEV) {
   import('@vite/client')
 }
-
-lazySizes.cfg.nativeLoading = { setLoadingAttribute: true, disableListeners: { scroll: true } }
-prepareAboveTheFoldLazyLoadedElements()
 
 window.customElements.define(
   'flynt-component',
