727ab1935930537aa11be3807aaf862ce25ab4bb
Dominik Tränklein <dominik@bleech.de>
2022-12-13 11:36:24 +0100

    feat(ListComponents): remove custom element

--- Components/ListComponents/script.js
+++ Components/ListComponents/script.js
@@ -1,79 +1,59 @@
-class ListComponents extends window.HTMLDivElement {
-  constructor (...args) {
-    const self = super(...args)
-    self.init()
-    return self
-  }
+import { debounce } from 'lodash-es'
+import { buildRefs } from '../../assets/scripts/helpers'
 
-  init () {
-    this.resolveElements()
-    this.bindFunctions()
-  }
+const parallaxConfigs = new Map()
 
-  resolveElements () {
-    this.window = window
-    this.document = document
-    this.componentScreenshotImages = this.querySelectorAll('.component-imageWrapper img')
-  }
+/**
+ *
+ * @param { Element } el
+ */
+export default function (el) {
+  const multiRefs = buildRefs(el, true)
+  const delegatedToggleHoverScroll = e =>
+    e.target.matches('.component-link') ? toggleHoverScroll(e) : null
 
-  bindFunctions () {
-    this.toggleHoverScroll = this.toggleHoverScroll.bind(this)
-    this.setParallaxConfig = this.setParallaxConfig.bind(this)
-    this.startParallaxScroll = this.startParallaxScroll.bind(this)
-  }
+  connect()
+  isDesktopQueryList.addEventListener('change', () => {
+    disconnect()
+    connect()
+  })
 
-  connectedCallback () {
-    if (!this.isDesktop()) {
-      // Add delegated events.
-      this.addEventListener('mouseenter', (e) => {
-        const { target } = e
-        if (target.matches('.component-link')) {
-          this.toggleHoverScroll(e)
-        }
-      }, true)
-
-      this.addEventListener('mouseleave', (e) => {
-        const { target } = e
-        if (target.matches('.component-link')) {
-          this.toggleHoverScroll(e)
-        }
-      }, true)
+  function connect () {
+    if (isDesktop()) {
+      el.addEventListener('mouseenter', delegatedToggleHoverScroll, true)
+      el.addEventListener('mouseleave', delegatedToggleHoverScroll, true)
     } else {
-      this.window.addEventListener('resize', this.setParallaxConfig)
-      this.document.addEventListener('lazyloaded', this.setParallaxConfig)
-      this.window.addEventListener('scroll', this.startParallaxScroll, { passive: true })
+      window.addEventListener('resize', debounce(setParallaxConfig, 250))
+      document.addEventListener('lazyloaded', setParallaxConfig)
+      window.addEventListener('scroll', startParallaxScroll, { passive: true })
     }
   }
-
-  toggleHoverScroll (e) {
-    const imageWrapper = e.target.querySelector('.component-imageWrapper')
-    const imageWrapperHeight = imageWrapper.offsetHeight
-    const image = imageWrapper.querySelector('img')
-    const imageHeight = image.offsetHeight
-
-    if (imageHeight > imageWrapperHeight) {
-      if (e.type === 'mouseenter') {
-        image.style.transition = 'transform ' + Math.max(((imageHeight - imageWrapperHeight) / imageWrapperHeight), 0.3) + 's cubic-bezier(0.215, 0.61, 0.355, 1)'
-        image.style.transform = 'translateY(-' + (imageHeight - imageWrapperHeight) + 'px)'
-      } else {
-        image.style.transition = 'transform ' + Math.max(((imageHeight - imageWrapperHeight) / imageWrapperHeight), 0.3) + 's cubic-bezier(0.23, 1, 0.32, 1)'
-        image.style.transform = 'translateY(0)'
-      }
-    }
+  function disconnect () {
+    el.removeEventListener('mouseenter', delegatedToggleHoverScroll, true)
+    el.removeEventListener('mouseleave', delegatedToggleHoverScroll, true)
+    window.removeEventListener('resize', setParallaxConfig)
+    document.removeEventListener('lazyloaded', setParallaxConfig)
+    window.removeEventListener('scroll', startParallaxScroll, { passive: true })
   }
 
-  setParallaxConfig (e) {
-    const windowHeight = this.window.innerHeight
-    this.componentScreenshotImages.forEach((image) => {
+  function setParallaxConfig (e) {
+    const { scrollY, innerHeight } = window
+    const { clientTop } = document.documentElement
+
+    parallaxConfigs.clear()
+    multiRefs.images.forEach(image => {
       const imageWrapper = image.parentElement
 
       const imageOverflow = image.offsetHeight - imageWrapper.offsetHeight
-      const topOffset = imageWrapper.getBoundingClientRect().top
-      const startOffset = topOffset - windowHeight * 0.4
+      const topOffset =
+        imageWrapper.getBoundingClientRect().top +
+        scrollY -
+        clientTop
+      const startOffset = topOffset - innerHeight * 0.4
       const endOffset = topOffset - 100
 
-      if (!image.dataset.parallaxConfig) {
-        image.dataset.parallaxConfig = JSON.stringify({
+      if (imageOverflow > 0) {
+        parallaxConfigs.set(image, {
           imageOverflow,
           startOffset,
           endOffset
@@ -81,29 +61,57 @@ class ListComponents extends window.HTMLDivElement {
       }
     })
 
-    this.startParallaxScroll()
+    startParallaxScroll()
   }
 
-  startParallaxScroll (e) {
-    const scrollTop = window.scrollY
+  function startParallaxScroll (e) {
+    const { scrollY } = window
 
-    this.componentScreenshotImages.forEach((image) => {
-      const parallaxConfig = JSON.parse(image.dataset.parallaxConfig)
-
-      if (parallaxConfig && parallaxConfig.imageOverflow > 0) {
-        let scrollPercentage = (scrollTop - parallaxConfig.startOffset) / (parallaxConfig.endOffset - parallaxConfig.startOffset)
-        scrollPercentage = Math.min(Math.max(scrollPercentage, 0), 1)
-        const move = (scrollPercentage * parallaxConfig.imageOverflow) * -1
-        if (!e || (scrollTop >= parallaxConfig.startOffset && scrollTop < parallaxConfig.endOffset)) {
-          image.style.transform = 'translateY(' + move + 'px)'
-        }
+    parallaxConfigs.forEach((parallaxConfig, image) => {
+      let scrollPercentage =
+        (scrollY - parallaxConfig.startOffset) /
+        (parallaxConfig.endOffset - parallaxConfig.startOffset)
+      scrollPercentage = Math.min(Math.max(scrollPercentage, 0), 1)
+      const move = scrollPercentage * parallaxConfig.imageOverflow * -1
+      if (
+        !e ||
+        (scrollY >= parallaxConfig.startOffset &&
+          scrollY < parallaxConfig.endOffset)
+      ) {
+        image.style.transform = 'translateY(' + move + 'px)'
       }
     })
   }
-
-  isDesktop () {
-    return window.matchMedia('(max-width: 1280px)').matches
-  }
 }
 
-window.customElements.define('flynt-list-components', ListComponents, { extends: 'div' })
+const isDesktopQueryList = window.matchMedia('(min-width: 1280px)')
+const isDesktop = () => isDesktopQueryList.matches
+
+/**
+ *
+ * @param {Event} e
+ */
+function toggleHoverScroll (e) {
+  const target = e.delegateTarget || e.target
+  const imageWrapper = target.querySelector('.component-imageWrapper')
+  const imageWrapperHeight = imageWrapper.offsetHeight
+  const image = imageWrapper.querySelector('img')
+  const imageHeight = image.offsetHeight
+
+  if (imageHeight > imageWrapperHeight) {
+    if (e.type === 'mouseenter') {
+      image.style.transition =
+        'transform ' +
+        Math.max((imageHeight - imageWrapperHeight) / imageWrapperHeight, 0.3) +
+        's cubic-bezier(0.215, 0.61, 0.355, 1)'
+      image.style.transform =
+        'translateY(-' + (imageHeight - imageWrapperHeight) + 'px)'
+    } else {
+      image.style.transition =
+        'transform ' +
+        Math.max((imageHeight - imageWrapperHeight) / imageWrapperHeight, 0.3) +
+        's cubic-bezier(0.23, 1, 0.32, 1)'
+      image.style.transform = 'translateY(0)'
+    }
+  }
+}
