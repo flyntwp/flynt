789f4defcab443fb87d551ed928f7df4c03fbd71
Timo Hubois <info@pixelsaft.de>
2022-12-19 09:53:07 +0100

    refactor(BlockCollapse): remove script, add option to define the theme

--- Components/BlockCollapse/functions.php
+++ Components/BlockCollapse/functions.php
@@ -2,6 +2,8 @@
 
 namespace Flynt\Components\BlockCollapse;
 
+use Flynt\FieldVariables;
+
 add_filter('Flynt/addComponentData?name=BlockCollapse', function ($data) {
     $data['status'] = $data['percentageDistance'] >= 101 ? 'expand' : 'collapse';
     return $data;
@@ -13,6 +15,13 @@ function getACFLayout()
         'name' => 'blockCollapse',
         'label' => __('Block: Collapse', 'flynt'),
         'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
             [
                 'label' => __('Vertical space', 'flynt'),
                 'instructions' => __('Distance adjusts the vertical space between two components.', 'flynt'),
@@ -44,6 +53,22 @@ function getACFLayout()
                     'width' => '50',
                 ],
             ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme(),
+                ]
+            ]
         ]
     ];
 }
--- Components/BlockCollapse/script.js
+++
@@ -1,6 +0,0 @@
-export default function (el) {
-  const previousElementTheme = el.previousElementSibling.getAttribute('data-theme')
-  if (previousElementTheme) {
-    el.setAttribute('data-theme', previousElementTheme)
-  }
-}
