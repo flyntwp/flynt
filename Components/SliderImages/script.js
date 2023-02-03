587fb581a7cfa67b0b77d72362068a73ea1ebe9d
Timo Hubois <info@pixelsaft.de>
2023-02-03 14:38:51 +0100

    build(packages): bump swiper to version 9
    - remove swiper lazy module (it was removed in swiper 9)

--- Components/SliderImages/script.js
+++ Components/SliderImages/script.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation, A11y, Autoplay, Lazy } from 'swiper'
+import Swiper, { Navigation, A11y, Autoplay } from 'swiper'
 import 'swiper/css/bundle'
 import { buildRefs, getJSON } from '@/assets/scripts/helpers.js'
 
@@ -12,15 +12,12 @@ export default function (sliderImages) {
 function initSlider (refs, data) {
   const { options } = data
   const config = {
-    modules: [Navigation, A11y, Lazy, Autoplay],
+    modules: [Navigation, A11y, Autoplay],
     a11y: options.a11y,
     roundLengths: true,
     navigation: {
       nextEl: refs.next,
       prevEl: refs.prev
-    },
-    lazy: {
-      loadPrevNext: true
     }
   }
   if (options.autoplay && options.autoplaySpeed) {
