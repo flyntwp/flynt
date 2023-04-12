f07d807941a3ddae46b8571a5b176d40c06f5ae7
Timo Hubois <info@pixelsaft.de>
2023-04-12 21:34:08 +0200

    feat(vite.config.js): add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import { dest, entries, host, watchFiles } from './build-config.js'
+import autoprefixer from 'autoprefixer'
 import flynt from './vite-plugin-flynt'
 import globImporter from 'node-sass-glob-importer'
 import FullReload from 'vite-plugin-full-reload'
@@ -16,6 +17,9 @@ export default defineConfig(({ mode }) => {
       preprocessorOptions: {
         scss: {
           importer: globImporter()
+        },
+        postcss: {
+          plugins: [autoprefixer()]
         }
       }
     },
