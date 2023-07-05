5f72da87c4c6310139b14065cc148a676b3afc51
Dominik Tränklein <dominik@bleech.de>
2023-07-05 20:14:39 +0200

    style(js): fix linting issues

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import globImporter from 'node-sass-glob-importer'
 import FullReload from 'vite-plugin-full-reload'
 import fs from 'fs'
 
-const wordpressHost = `http://localhost:3000`
+const wordpressHost = 'http://localhost:3000'
 
 const dest = './dist'
 const entries = [
