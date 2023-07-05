e2206ae22341a89428d90e4ac39873a32ca87966
Dominik Tränklein <dominik@bleech.de>
2023-07-05 15:30:56 +0200

    feat(BlockEditor): combine all customisation into blockEditor.php

--- inc/assets.php
+++ inc/assets.php
@@ -23,14 +23,6 @@ add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_style('Flynt/assets/main', Asset::requireUrl('assets/main.scss'), [], null);
     wp_enqueue_style('Flynt/assets/print', Asset::requireUrl('assets/print.scss'), [], null, 'print');
-
-    // Remove Gutenberg block related styles on front-end, when a post has no blocks.
-    if (!has_blocks()) {
-        wp_dequeue_style('core-block-supports');
-        wp_dequeue_style('wp-block-library');
-        wp_dequeue_style('wp-block-library-theme');
-        wp_dequeue_style('global-styles');
-    }
 });
 
 add_action('admin_enqueue_scripts', function () {
---
+++ inc/blockEditor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Flynt\BlockEditor;
+
+/**
+ * Disable Full Site Editing
+ */
+define('DISABLE_FSE', '__return_true');
+
+/**
+ * Disable Templates and Template Parts in Block Editor
+ */
+add_filter('block_editor_settings_all', function ($settings) {
+    $settings['supportsTemplateMode'] = false;
+    return $settings;
+}, 10);
+
+/**
+ * Removes editor from Wordpress pages, since Flynt uses ACF.
+ */
+add_action('init', function () {
+    remove_post_type_support('page', 'editor');
+    remove_action('wp_enqueue_scripts', 'wp_enqueue_classic_theme_styles');
+});
+
+/**
+ * Removes Gutenberg block related styles on front-end, when a post has no blocks.
+ */
+add_action('wp_enqueue_scripts', function () {
+    if (!has_blocks()) {
+        wp_dequeue_style('core-block-supports');
+        wp_dequeue_style('wp-block-library');
+        wp_dequeue_style('wp-block-library-theme');
+        wp_dequeue_style('global-styles');
+    }
+});
