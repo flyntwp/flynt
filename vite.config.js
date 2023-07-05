2efe775eb5e82fa7b61e006bb2e44ecb3dee01d9
Dominik Tränklein <dominik@bleech.de>
2023-07-05 19:59:24 +0200

    feat(vite): merge build-config.js into vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig, loadEnv } from 'vite'
-import { dest, entries, host as configHost, watchFiles } from './build-config.js'
 import autoprefixer from 'autoprefixer'
 import flynt from './vite-plugin-flynt'
 import globImporter from 'node-sass-glob-importer'
 import FullReload from 'vite-plugin-full-reload'
 import fs from 'fs'
 
+const wordpressHost = `http://localhost:3000`
+
+const dest = './dist'
+const entries = [
+  './assets/admin.js',
+  './assets/admin.scss',
+  './assets/main.js',
+  './assets/main.scss',
+  './assets/print.scss',
+  './assets/editor-style.scss'
+]
+const watchFiles = [
+  '*.php',
+  'templates/**/*',
+  'lib/**/*',
+  'inc/**/*',
+  './Components/**/*.{php,twig}'
+]
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const host = env.VITE_DEV_SERVER_HOST || configHost
+  const host = env.VITE_DEV_SERVER_HOST || wordpressHost
   const isSecure = host.indexOf('https://') === 0 && (env.VITE_DEV_SERVER_KEY || env.VITE_DEV_SERVER_CERT)
 
   return {
