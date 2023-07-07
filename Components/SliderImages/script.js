5c9197ff2c3002ba2ec922f409b656bd8b37c53a
Timo Hubois <info@pixelsaft.de>
2023-07-07 10:46:04 +0200

    feat(SliderImages): only import required css

--- Components/SliderImages/script.js
+++ Components/SliderImages/script.js
@@ -1,7 +1,10 @@
-import Swiper from 'swiper'
-import { Navigation, A11y, Autoplay } from 'swiper/modules'
-import 'swiper/css/bundle'
 import { buildRefs, getJSON } from '@/assets/scripts/helpers.js'
+import Swiper from 'swiper'
+import { Autoplay, A11y, Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/a11y'
+import 'swiper/css/navigation'
 
 export default function (el) {
   const refs = buildRefs(el)
@@ -13,7 +16,7 @@ export default function (el) {
 function initSlider (refs, data) {
   const { options } = data
   const config = {
-    modules: [Navigation, A11y, Autoplay],
+    modules: [Autoplay, A11y, Navigation],
     a11y: options.a11y,
     roundLengths: true,
     navigation: {
