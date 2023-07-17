5f78df5f72fe02bc43eb3d4dc24ccb00a05b9d1f
Timo Hubois <info@pixelsaft.de>
2023-07-17 13:30:49 +0200

    fix(ReusableComponent): remove string interpolation deprecated warning when using PHP 8.2

--- Components/ReusableComponent/functions.php
+++ Components/ReusableComponent/functions.php
@@ -34,19 +34,19 @@ add_filter('acf/prepare_field/name=reusableComponent', function ($field) {
     $instructions = sprintf(
         // translators: 1: <a> element 2: </a> element
         __('Add %1$sreusable component%2$s.', 'flynt'),
-        "<a href='${reusableAdminLink}' target='_blank' rel='noopener noreferrer'>",
+        "<a href='{$reusableAdminLink}' target='_blank' rel='noopener noreferrer'>",
         "</a>"
     );
     $editLink = sprintf(
         // translators: %s: Link and title of selected reusable-post
         __(' Edit %s.', 'flynt'),
-        "<a class='reusable-postLink' data-postId='${postId}' href='${postEditLink}' target='_blank' rel='noopener noreferrer'>${postTitle}</a>"
+        "<a class='reusable-postLink' data-postId='{$postId}' href='{$postEditLink}' target='_blank' rel='noopener noreferrer'>{$postTitle}</a>"
     );
 
     if ($field['value']) {
         $instructions .= $editLink;
     } else {
-        $instructions .= "<span hidden>${editLink}</span>";
+        $instructions .= "<span hidden>{$editLink}</span>";
     }
 
     $field['instructions'] = $instructions;
