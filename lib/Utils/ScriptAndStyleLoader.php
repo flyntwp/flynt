31977a92c94b34b204406f46ad20387a4e889981
Timo Hubois <info@pixelsaft.de>
2023-08-19 23:02:37 +0200

    feat(assets): use native loading strategy
    - available since WordPress 6.3 see: https://core.trac.wordpress.org/ticket/12009

--- lib/Utils/ScriptAndStyleLoader.php
+++ lib/Utils/ScriptAndStyleLoader.php
@@ -3,12 +3,12 @@
 namespace Flynt\Utils;
 
 /**
- * Provides functions to add async/defer attributes to enqueued / registered scripts or add a preload link.
+ * Provides functions to modify the script and style loader.
  */
 class ScriptAndStyleLoader
 {
     /**
-     * Adds async/defer attributes to enqueued / registered scripts or add a preload link.
+     * Filters the script loader tag.
      *
      * @param string $tag The script tag.
      * @param string $handle The script handle.
@@ -17,23 +17,6 @@ class ScriptAndStyleLoader
      */
     public function filterScriptLoaderTag(string $tag, string $handle, string $src)
     {
-        /*
-         * If #12009 lands in WordPress, this loop can no-op since it would be handled in core.
-         *
-         * @link https://core.trac.wordpress.org/ticket/12009
-         */
-        foreach (['async', 'defer'] as $attr) {
-            if (!wp_scripts()->get_data($handle, $attr)) {
-                continue;
-            }
-            // Prevent adding attribute when already added in #12009.
-            if (!preg_match(":\s$attr(=|>|\s):", $tag)) {
-                $tag = preg_replace(':(?=></script>):', " $attr", $tag, 1);
-            }
-            // Only allow async or defer, not both.
-            break;
-        }
-
         if (wp_scripts()->get_data($handle, 'module')) {
             $tag = preg_replace(':(?=></script>):', " type=\"module\"", $tag, 1);
         }
