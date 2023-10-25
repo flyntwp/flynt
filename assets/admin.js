4f35678723c1cc0e3db465c8bec68634541192da
Dominik Tränklein <dominik@bleech.de>
2023-10-26 00:14:24 +0200

    tmp

--- assets/admin.js
+++ assets/admin.js
@@ -5,6 +5,20 @@ if (import.meta.env.DEV) {
 import.meta.glob('../Components/**/admin.js', { eager: true })
 
 
+const componentList = document.getElementById('component-list')
+if (componentList) {
+  const anchors = componentList.querySelectorAll('a')
+  for (const anchor of anchors) {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault()
+      const componentName = anchor.dataset.componentName
+      const url = new URL(window.location)
+      url.searchParams.set('component', componentName)
+      window.location = url
+    })
+  }
+}
+
 const flyntRenderer = document.getElementById('flynt-renderer')
 if (flyntRenderer) {
   if (
@@ -36,63 +50,20 @@ function init() {
     })
 
   })
-  // const handshakes = []
-  // import('postmate').then(({ default: Postmate }) => {
-  //   handshakes.push(new Postmate({
-  //     container: document.getElementById('postmate'),
-  //     name: 'postmate-mobile',
-  //     url: '/renderer',
-  //     classListArray: ['mobile']
-  //   }))
-  //   handshakes.push(new Postmate({
-  //     container: document.getElementById('postmate'),
-  //     name: 'postmate-tablet',
-  //     url: '/renderer',
-  //     classListArray: ['tablet']
-  //   }))
-  //   handshakes.push(new Postmate({
-  //     container: document.getElementById('postmate'),
-  //     name: 'postmate-desktop',
-  //     url: '/renderer',
-  //     classListArray: ['desktop']
-  //   }))
-  // })
-  // jQuery(flyntRenderer).on('change', function () {
-  //   console.log('jq change')
-  // })
-  // jQuery(flyntRenderer).on('changed', function () {
-  //   console.log('jq changed')
-  // })
   acf.addAction('ready', (e) => {submit()})
-  // acf.addAction('prepare_field', (e) => {console.log('prepare_field', e)})
-  // acf.addAction('ready_field', (e) => {console.log('ready_field', e)})
-  // flyntRenderer.querySelectorAll('input').forEach(input => {
-  //   const value = localStorage.getItem('input_' + input.name)
-  //   if (value) {
-  //     // const matches = input.name.match(/\[(field_\w+)\]/g)
-  //     // console.log(input.name, matches)
-  //     // input.value = value
-  //     acf.val(jQuery(input), value, true)
-  //   }
-  // })
   jQuery(flyntRenderer).on('change', (e) => {
-    console.log('flynt-renderer changed')
-    localStorage.setItem('input_' + e.target.name, e.target.value)
-    const url = new URL(window.location)
-    const formData = new FormData(flyntRenderer)
-    formData.delete('_acf_form')
-    url.searchParams.set('customData', JSON.stringify(Object.fromEntries(formData)))
-    history.pushState(null, null, url)
+    // console.log('flynt-renderer changed')
+    // localStorage.setItem('input_' + e.target.name, e.target.value)
+    // const url = new URL(window.location)
+    // const formData = new FormData(flyntRenderer)
+    // formData.delete('_acf_form')
+    // url.searchParams.set('customData', JSON.stringify(Object.fromEntries(formData)))
+    // history.pushState(null, null, url)
     submit()
   })
   flyntRenderer.addEventListener('submit', async (e) => {
     console.log('flynt-renderer submitted')
     e.preventDefault()
-    // wp.apiFetch({
-    //   path: 'flynt/v1/render',
-    //   method: 'POST',
-    //   data: Object.fromEntries(new FormData(flyntRenderer))
-    // })
     submit()
   })
 
--- inc/renderer.php
+++ inc/renderer.php
@@ -1,16 +1,13 @@
 <?php
 
 use ACFComposer\ACFComposer;
-use Flynt\Components;
+use Flynt\ComponentManager;
 use Timber\Timber;
-use Timber\Twig;
-
-use function Flynt\ComponentLogServer\consoleDebug;
 
 function my_admin_menu() {
     add_menu_page(
-        __( 'Sample page', 'my-textdomain' ),
-        __( 'Sample menu', 'my-textdomain' ),
+        __( 'Flynt Studio', 'flynt' ),
+        __( 'Flynt Studio', 'flynt' ),
         'manage_options',
         'sample-page',
         'my_admin_page_contents',
@@ -20,23 +17,8 @@ function my_admin_menu() {
 }
 add_action( 'admin_menu', 'my_admin_menu' );
 
-// function my_admin_page_contents() {
-//     acf_enqueue_scripts();
-//     ACFComposer::registerFieldGroup([
-//         'name' => 'tmp',
-//         'title' => '',
-//         'fields' => Components\BlockImageText\getACFLayout()['sub_fields'],
-//         'location' => []
-//     ]);
-//     acf_form([
-//         'id' => 'flynt-renderer',
-//         'field_groups' => [
-//             'group_tmp'
-//         ],
-//     ]);
-// }
 
-function registerField() {
+function registerField($componentName) {
     ACFComposer::registerFieldGroup([
         'name' => 'tmp',
         'title' => '',
@@ -50,10 +32,10 @@ function registerField() {
                 'endpoint' => 0
             ],
             [
-                'label' => 'Something',
-                'name' => 'something',
+                'label' => 'Current Component',
+                'name' => 'component',
                 'type' => 'group',
-                'sub_fields' => Components\BlockImageText\getACFLayout()['sub_fields'],
+                'sub_fields' => "Flynt\\Components\\$componentName\\getACFLayout"()['sub_fields'],
             ],
         ],
         'location' => []
@@ -67,48 +49,70 @@ function getFieldInputName($field) {
             return getFieldInputName($parent) . '[' . $field['key'] . ']';
         }
     }
-    return 'acf[' . $field['key'] . ']';
+    return $field['key'];
 }
 
 function my_admin_page_contents() {
-    registerField();
-    if (isset($_GET['customData'])) {
-        $customData = json_decode(stripslashes_deep($_GET['customData']), true);
-        // var_dump($customData);
-        add_filter('acf/load_value', function ($value, $postId, $field) use ($customData) {
-            $inputName = getFieldInputName($field);
-            $val = $customData[$inputName] ?? null;
+    $componentManager = ComponentManager::getInstance();
+    $componentsNames = array_keys($componentManager->getAll());
+    echo '<ul id="component-list">';
+    foreach ($componentsNames as $componentName) {
+        if (function_exists("\\Flynt\\Components\\$componentName\\getACFLayout"))
+        echo '<li><a href="#" data-component-name="' . $componentName . '">' . $componentName . '</a></li>';
+    }
+    echo '</ul>';
+    $currentComponent = $_GET['component'] ?? null;
+    if ($currentComponent) {
+        registerField($currentComponent);
+        prepareLoadValues();
+        acf_form_head();
+        acf_form([
+            'id' => 'flynt-renderer',
+            'field_groups' => [
+                'group_tmp'
+            ],
+            'html_before_fields' => <<<EOL
+                <input type="hidden" name="action" value="renderAcfComponent" />
+                <input type="hidden" name="currentComponent" value="$currentComponent" />
+            EOL,
+        ]);
+        // echo '<iframe width="800" height="800" id="rendererIframe" src="/renderer"></iframe>';
+        echo '<label for="iframe-scale">Scale</label>';
+        echo '<input type="range" id="iframe-scale" min=0 max=1 step="0.01" value="0.5" />';
+        echo '<div id="postmate"></div>';
+    }
+}
 
+function prepareLoadValues() {
+    $config = get_option('flynt_render_config', []);
+    $componentConfig = $config[$_GET['component']] ?? null;
+    if (isset($componentConfig)) {
+        add_filter('acf/load_value', function ($value, $postId, $field) use ($componentConfig) {
+            $val = getValueForField($field, $componentConfig);
             return $val;
         }, 1, 3);
-        // add_filter('acf/prepare_field', function ($field) use ($customData) {
-        //     var_dump($field);die();
-        //     $inputName = getFieldInputName($field);
-        //     $value = $customData[$inputName];
-        //     if ($value) {
-        //         // var_dump($inputName, $value);
-        //         $field['default_value'] = $value;
-        //     }
-        //     return $field;
-        // });
     }
-    // acf_enqueue_scripts();
+}
 
-    // echo '<form id="flynt-renderer">';
-    // render_field(get_field_object('field_tmp_something'));
-    // echo '</form>';
-    acf_form_head();
-    acf_form([
-        'id' => 'flynt-renderer',
-        'field_groups' => [
-            'group_tmp'
-        ],
-        'html_before_fields' => '<input type="hidden" name="action" value="renderAcfComponent" />',
-    ]);
-    // echo '<iframe width="800" height="800" id="rendererIframe" src="/renderer"></iframe>';
-    echo '<label for="iframe-scale">Scale</label>';
-    echo '<input type="range" id="iframe-scale" min=0 max=1 step="0.01" value="0.5" />';
-    echo '<div id="postmate"></div>';
+function getParents($field, &$parents = []) {
+    if ($field['parent']) {
+        $parent = acf_get_field($field['parent']);
+        array_unshift($parents, $parent);
+        if ($parent['key'] ?? null) {
+            return getParents($parent, $parents);
+        }
+    }
+    return $parents;
+}
+
+function getValueForField($field, $values) {
+    $parents = getParents($field);
+    foreach ($parents as $parent) {
+        if ($parent['key'] ?? null) {
+            $values = $values[$parent['key']] ?? null;
+        }
+    }
+    return $values[$field['key']] ?? null;
 }
 
 function getFieldValues($keysAndValues) {
@@ -124,53 +128,25 @@ function getFieldValues($keysAndValues) {
     return $values;
 }
 
-add_filter('acf/pre_submit_form', function ($form) {
-    registerField();
-    $values = [];
-    // var_dump($_POST, $GLOBALS['acf_form']);
-    // foreach ( $_POST['acf'] as $key => $value ) {
-
-	// 	// Get field.
-	// 	$field = acf_get_field( $key );
-    //     // var_dump($field['name']);
-	// 	// Update value.
-	// 	if ( $field ) {
-    //         $values[$field['name']] = getFieldValues($key, $value);
-    //         // if ($field['sub_fields'] ) {
-    //         //     $values[ $field['name'] ] = [];
-    //         //     foreach($value as $fieldKey => $fieldValue) {
-    //         //         $fieldField = acf_get_field( $fieldKey );
-    //         //         // $values[ $field['name'] ][$fieldField['name']] = acf_format_value(apply_filters( 'acf/update_value', $fieldValue, 0, $field, $fieldValue ), 0, $field);
-    //         //         var_dump($fieldValue);
-    //         //         $values[ $field['name'] ][$fieldField['name']] = acf_format_value($fieldValue, 0, $fieldField);
-    //         //     }
-    //         // } else {
-    //         //     // $values[ $field['name'] ] = acf_format_value(apply_filters( 'acf/update_value', $value, 0, $field, $value ), 0, $field);
-    //         //     var_dump($value);
-    //         //     $values[ $field['name'] ] = acf_format_value($value, 0, $field);
-    //         // }
-	// 	}
-	// }
-    \Flynt\TimberLoader\addFilters();
-    $values = getFieldValues($_POST['acf']);
-    var_dump($values);
-    echo Timber::compile_string("{{ renderComponent('BlockImageText', data) }}", [
-        'data' => $values['something'],
-    ]);
-    // die();
-});
-
 function acfRenderComponent() {
-    registerField();
+    $componentName = $_POST['currentComponent'];
+    registerField($componentName);
     checkSubmit();
+    saveConfig();
     $values = [];
     \Flynt\TimberLoader\addFilters();
     $values = getFieldValues($_POST['acf']);
-    // var_dump($values);
-    echo Timber::compile_string("{{ renderComponent('BlockImageText', data) }}", [
-        'data' => $values['something'],
+    echo Timber::compile_string("{{ renderComponent('$componentName', data) }}", [
+        'data' => $values['component'],
     ]);
     wp_die();
+}
+
+function saveConfig() {
+    $config = get_option('flynt_render_config', []);
+    $componentName = $_POST['currentComponent'];
+    $config[$componentName] = $_REQUEST['acf'];
+    update_option('flynt_render_config', $config);
 }
 
 function checkSubmit() {
@@ -207,8 +183,3 @@ function checkSubmit() {
 
 add_action('wp_ajax_renderAcfComponent', 'acfRenderComponent');
 add_action('wp_ajax_nopriv_renderAcfComponent', 'acfRenderComponent');
-
-// add_action('post_action_acf_renderer', function () {
-//     var_dump(func_get_args());
-//     die();
-// }, 10, 10);
