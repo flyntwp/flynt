fab97e2d919f6c32cf5e439053ddfb5b877b474d
Timo Hubois <info@pixelsaft.de>
2024-02-03 16:01:44 +0100

    refactor(GridPostsLatest): use implode instead of join

--- Components/GridPostsLatest/functions.php
+++ Components/GridPostsLatest/functions.php
@@ -17,7 +17,7 @@ add_filter('Flynt/addComponentData?name=GridPostsLatest', function ($data) {
     $posts = Timber::get_posts([
         'post_status' => 'publish',
         'post_type' => POST_TYPE,
-        'cat' => join(',', array_map(function ($taxonomy) {
+        'cat' => implode(',', array_map(function ($taxonomy) {
             return $taxonomy->term_id;
         }, $data['taxonomies'])),
         'posts_per_page' => $postsPerPage + 1,
