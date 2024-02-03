f3f7e549bb2e6b2b3bf03d1a9195f98ad5e22305
Timo Hubois <info@pixelsaft.de>
2024-02-03 14:47:45 +0100

    refactor(ReusableComponent): use shorthand ternary operator

--- Components/ReusableComponent/functions.php
+++ Components/ReusableComponent/functions.php
@@ -29,7 +29,7 @@ add_filter('acf/prepare_field/name=reusableComponent', function ($field) {
     $reusableAdminLink = admin_url('edit.php?post_type=reusable-components');
     $postEditLink = get_edit_post_link($field['value']);
     $postTitle = get_the_title($field['value']);
-    $postId = $field['value'] ? $field['value'] : get_the_ID();
+    $postId = $field['value'] ?: get_the_ID();
 
     $instructions = sprintf(
         // translators: 1: <a> element 2: </a> element
