201a89e8c4ce610405ac7f4ae4bf03046e4ef9d9
Timo Hubois <info@pixelsaft.de>
2024-02-23 15:28:05 +0100

    refactor(FormPasswordProtection): add return type

--- Components/FormPasswordProtection/functions.php
+++ Components/FormPasswordProtection/functions.php
@@ -5,7 +5,7 @@ namespace Flynt\Components\FormPasswordProtection;
 use Flynt\Utils\Options;
 use Timber\Timber;
 
-add_filter('the_password_form', function () {
+add_filter('the_password_form', function (): string {
     $context = Timber::context();
     $context['form'] = [
         'url' => site_url('/wp-login.php?action=postpass', 'login_post')
