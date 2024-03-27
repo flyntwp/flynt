b94461a49eb3ff0293778d4dac94a2e3823542c8
Timo Hubois <info@pixelsaft.de>
2024-03-27 20:37:19 +0100

    refactor(theContentFix.php): add type declarations, make code more readable

--- inc/theContentFix.php
+++ inc/theContentFix.php
@@ -4,59 +4,56 @@ namespace Flynt\TheContentFix;
 
 use Timber\Timber;
 
-add_filter('wp_insert_post_data', function (array $data, array $postArr) {
-    if (
-        in_array(
-            $postArr['post_type'],
-            [
-                'revision',
-                'nav_menu_item',
-                'attachment',
-                'customize_changeset',
-                'custom_css',
-                'acf-taxonomy',
-                'acf-post-type',
-                'acf-ui-options-page',
-                'acf-field-group',
-                'acf-field'
-            ]
-        )
-    ) {
+add_filter('wp_insert_post_data', function (array $data, array $postArr): array {
+    $excludedPostTypes = [
+        'attachment',
+        'revision',
+        'custom_css',
+        'customize_changeset',
+        'acf-taxonomy',
+        'acf-post-type',
+        'acf-ui-options-page',
+        'acf-field-group',
+        'acf-field'
+    ];
+
+    if (in_array($postArr['post_type'], $excludedPostTypes)) {
         return $data;
     }
 
-    $isPostTypeSupportsGutenberg = post_type_supports($data['post_type'], 'editor');
-    // Check if no content was saved before, or if there is a flyntTheContent shortcode but the id does not match the post id.
-    if (
-        !$isPostTypeSupportsGutenberg &&
-        (!isset($data['post_content']) || $data['post_content'] === '' || !isPostIdInShortcode($data['post_content'], $postArr['ID']))
-    ) {
-        $data['post_content'] = "[flyntTheContent id=\"{$postArr['ID']}\"]";
+    $doesPostTypeSupportEditor = isset($data['post_type']) && post_type_supports($data['post_type'], 'editor');
+    if ($doesPostTypeSupportEditor) {
+        return $data;
+    }
+
+    $postContent = (string) $data['post_content'] ?? '';
+    $postId =  (int) $postArr['ID'] ?? 0;
+    $shouldAddShortcode = $postContent === '' || !doesShortcodeMatchPostId($postContent, $postId);
+
+    if ($shouldAddShortcode) {
+        $data['post_content'] = "[flyntTheContent id=\"{$postId}\"]";
     }
 
     return $data;
 }, 99, 2);
 
-add_shortcode('flyntTheContent', function (array $attrs) {
-    if (is_admin()) {
-        return;
+add_shortcode('flyntTheContent', function (array $attrs = []) {
+    if (is_admin() || empty($attrs['id'])) {
+        return '';
     }
 
     $postId = $attrs['id'];
-    // in case the post id was not set correctly and is 0
-    if (!empty($postId)) {
-        $context = Timber::context();
-        $context['post'] = Timber::get_Post($postId);
-        $context['post']->setup();
-        return Timber::compile('templates/theContentFix.twig', $context);
-    }
+    $context = Timber::context();
+    $context['post'] = Timber::get_Post($postId);
+    $context['post']->setup();
+    return Timber::compile('templates/theContentFix.twig', $context);
 });
 
-function isPostIdInShortcode($postContent, $postId): bool
+function doesShortcodeMatchPostId(string $postContent, int $postId): bool
 {
     preg_match('/^\[flyntTheContent id=\\\"(\d*)\\\"\]$/', $postContent, $matches);
 
-    if (!isset($matches) || count($matches) === 0) {
+    if (!isset($matches) || empty($matches)) {
         return false;
     }
 
