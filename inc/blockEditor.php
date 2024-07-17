370a7417de6b9b7db20bad3bdc018710cf385ef7
Timo Hubois <info@pixelsaft.de>
2024-07-17 11:01:03 +0200

    chore(blockEditor.php): optimize dequeueing of Gutenberg block styles

--- inc/blockEditor.php
+++ inc/blockEditor.php
@@ -31,10 +31,13 @@ add_action('init', function (): void {
  * Remove Gutenberg block related styles on front-end, when a post has no blocks.
  */
 add_action('wp_enqueue_scripts', function (): void {
-    if (!has_blocks()) {
-        wp_dequeue_style('core-block-supports');
-        wp_dequeue_style('wp-block-library');
-        wp_dequeue_style('wp-block-library-theme');
-        wp_dequeue_style('global-styles');
+    if (has_blocks()) {
+        return;
     }
+
+    wp_dequeue_style('core-block-supports');
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wp-global-styles');
+    wp_dequeue_style('block-style-variation-styles');
 });
