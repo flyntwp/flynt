2be9292aeeb213aca883385be0c2ec7046035dcb
Timo Hubois <info@pixelsaft.de>
2025-08-20 12:08:23 +0200

    fix(FeatureFlexibleContentExtension): add order parameter to layout title filter

--- Components/FeatureFlexibleContentExtension/functions.php
+++ Components/FeatureFlexibleContentExtension/functions.php
@@ -21,7 +21,11 @@ add_action('admin_enqueue_scripts', function (): void {
 });
 
 // add image to the flexible content component name
-add_filter('acf/fields/flexible_content/layout_title', function (string $title, array $field, array $layout): string {
+add_filter('acf/fields/flexible_content/layout_title', function (string $title, array $field, array $layout, $order): string {
+    if (!is_numeric($order)) {
+        return $title;
+    }
+
     $componentManager = ComponentManager::getInstance();
     $componentName = ucfirst($layout['name']);
     $componentPathFull = $componentManager->getComponentDirPath($componentName);
