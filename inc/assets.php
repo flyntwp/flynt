5c2727157b866e2f1fb322d1400ce43851c4218e
mahmoud <awad25.ma@gmail.com>
2026-04-07 09:52:38 +0200

    feat(assets): enqueue theme stylesheet

--- inc/assets.php
+++ inc/assets.php
@@ -22,6 +22,7 @@ add_action('wp_enqueue_scripts', function (): void {
 
     wp_enqueue_style('Flynt/assets/main', Asset::requireUrl('assets/main.scss'), [], null);
     wp_enqueue_style('Flynt/assets/print', Asset::requireUrl('assets/print.scss'), [], null, 'print');
+    wp_enqueue_style('Flynt/assets/theme', Asset::requireUrl('assets/style.css'), [], null);
 });
 
 add_action('admin_enqueue_scripts', function (): void {
